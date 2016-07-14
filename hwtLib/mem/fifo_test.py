45e24ba3e791acfaf526db5b80990a290f7ab669
Nic30 <nic30@seznam.cz>
2016-07-14 19:04:02 +0200

    better fifo tests

---
+++ hwtLib/mem/fifo_test.py
@@ -0,0 +1,22 @@
+from hwtLib.mem.fifo import Fifo
+from hdl_toolkit.simulator.shortcuts import simUnitVcd
+from hdl_toolkit.simulator.hdlSimulator import HdlSimulator
+from hdl_toolkit.synthetisator.shortcuts import toRtl
+from hdl_toolkit.simulator.agentConnector import autoAddAgents
+
+if __name__ == "__main__":
+    
+    u = Fifo()
+    u.DATA_WIDTH.set(8)
+    u.DEPTH.set(4)
+    ns = HdlSimulator.ns
+    toRtl(u)
+
+    procs = autoAddAgents(u)
+    u.dIn._ag.data = [1, 2, 3]
+
+
+    simUnitVcd(u, procs,
+                "tmp/fifo.vcd", 
+                time=80 * HdlSimulator.ns)
+    print("done")
