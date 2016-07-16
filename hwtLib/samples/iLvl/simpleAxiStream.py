729181ad4f02ee2d1e5668c5bdf0a2210ef3aa49
nic30 <nic30@seznam.cz>
2016-07-16 21:30:19 +0200

    better names for examples

--- hwtLib/samples/iLvl/simpleAxiStream.py
+++ hwtLib/samples/iLvl/simpleAxiStream.py
@@ -2,18 +2,19 @@ from hdl_toolkit.intfLvl import connect, Param, Unit
 from hdl_toolkit.interfaces.amba import AxiStream
 
 
-class SimpleUnit2(Unit):
-    def _config(self):
-        self.DATA_WIDTH = Param(8)
+class SimpleUnitAxiStream(Unit):
+    #def _config(self):
+    #    self.DATA_WIDTH = Param(8)
 
     def _declr(self):
-        self.a = AxiStream(isExtern=True)
-        self.b = AxiStream(isExtern=True)
-        self._shareAllParams()
+        with self._asExtern():
+            self.a = AxiStream()
+            self.b = AxiStream()
+        #self._shareAllParams()
         
     def _impl(self):
         connect(self.a, self.b)
 
 if __name__ == "__main__":
     from hdl_toolkit.synthetisator.shortcuts import toRtl
-    print(toRtl(SimpleUnit2))
+    print(toRtl(SimpleUnitAxiStream))
