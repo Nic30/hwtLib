ea1d66758ef1e2c330c04c7220c592c15aff38a3
Nic30 <nic30@seznam.cz>
2016-07-20 11:30:00 +0200

    clean port

--- hwtLib/handshaked/bramReader.py
+++ hwtLib/handshaked/bramReader.py
@@ -17,33 +17,79 @@ class HsBramPortReader(Unit):
     def _declr(self):
         with self._asExtern():
             addClkRstn(self)
+            # start reading data over dataOut
             self.en = HandshakeSync()
+            
+            # delete active memoryset
+            self.clean = HandshakeSync()
             
             with self._paramsShared():
                 self.dataIn = BramPort_withoutClk()
                 self.dataOut = Handshaked() 
+    
+    def fsm(self, st, data_flag, data_inReg, addr):
+        st_t = st._dtype
+        Out = self.dataOut
+        
+        def onLastGoIddle():
+            ADDR_HIGH = self.ADDR_LOW + self.SIZE - 1
+            lastAddr = addr._eq(ADDR_HIGH)
+            return  If(lastAddr,
+                        c(st_t.iddle, st)
+                        ,
+                        st._same()
+                    )
+        
+        Switch(st,
+            (st_t.iddle,
+                If(self.en.vld,
+                    c(st_t.sendingData, st)
+                    ,
+                    If(self.clean.vld,
+                       c(st_t.inCleaning, st)
+                       ,
+                       st._same()
+                    )
+                )
+            ),
+            (st_t.sendingData,
+                If(data_inReg | (data_flag & ~Out.rd),
+                    # if some data is loaded and it can not be send out
+                    st._same()
+                    ,
+                    # if is possible to send data in this clk
+                    onLastGoIddle()
+                )
+            ),
+            (st_t.inCleaning,
+                onLastGoIddle()
+            )
+        )
                 
     def _impl(self):
         In = self.dataIn
         Out = self.dataOut
         
-        st_t = Enum("st_t", ["iddle", "sendingData"])
+        st_t = Enum("st_t", ["iddle", "sendingData", "inCleaning"])
         self.st = st = self._reg("st_reg", st_t, st_t.iddle)
-        self.addr = addr = self._reg("addr_reg", In.addr._dtype)
-        self.data_reg = data_reg = self._reg("data_reg", In.dout._dtype)
-        self.data_flag = data_flag = self._reg("data_flag_reg", defVal=0)
-        self.data_inReg = data_inReg = self._reg("data_inReg_flag_reg", defVal=0)
+        addr = self.addr = self._reg("addr_reg", In.addr._dtype)
+        data_reg = self.data_reg = self._reg("data_reg", In.dout._dtype)
+        data_flag = self.data_flag = self._reg("data_flag_reg", defVal=0)
+        data_inReg = self.data_inReg = self._reg("data_inReg_flag_reg", defVal=0)
         
         
+        self.fsm(st, data_flag, data_inReg, addr)
+
         c(st._eq(st_t.iddle), self.en.rd)
+        c(st._eq(st_t.iddle), self.clean.rd)
         
         c(data_inReg | data_flag, Out.vld)
         
-
         c(0, In.din)
         c(1, In.en)
-        c(0, In.we)
+        c(st._eq(st_t.inCleaning), In.we)
         c(addr, In.addr)
+
         
         If(data_flag,
            c(In.dout, data_reg)
@@ -56,38 +102,42 @@ class HsBramPortReader(Unit):
            c(In.dout, Out.data)
         )
         
+        # addr incrementig logic
         Switch(st,
             (st_t.iddle,
-                c(self.ADDR_LOW, addr) + 
-                c(0, data_flag) + 
-                c(0, data_inReg) + 
-                If(self.en.vld,
-                   c(st_t.sendingData, st)
-                   ,
-                   st._same()
-                )
+                c(self.ADDR_LOW, addr)
             ),
             (st_t.sendingData,
-                c(1, data_flag) +
                 If(data_inReg | (data_flag & ~Out.rd),
                     # if some data is loaded and it can not be send out
-                    addr._same()+
-                    If(data_inReg,
-                       c(~Out.rd, data_inReg)
-                       ,
-                       data_inReg._same()
-                    )
+                    addr._same()
                     ,
                     # if is possible to send data in this clk
-                    c(~self.dataOut.rd, data_inReg) +
-                    If(addr._eq(self.ADDR_LOW + self.SIZE),
-                        c(self.ADDR_LOW, addr)
-                        ,
-                        c(addr + 1, addr)
-                    )
+                    c(addr + 1, addr)
                 )
-            
+            ),
+            (st_t.inCleaning,
+                c(addr + 1, addr)
             )
+        )
+        
+        # dataRegs logic
+        If(st._eq(st_t.sendingData),
+            c(1, data_flag) + 
+            If(data_inReg | (data_flag & ~Out.rd),
+                # if some data is loaded and it can not be send out
+                If(data_inReg,
+                   c(~Out.rd, data_inReg)
+                   ,
+                   data_inReg._same()
+                )
+                ,
+                # if is possible to send data in this clk
+                c(~self.dataOut.rd, data_inReg)
+            )
+            ,
+            c(0, data_flag) + 
+            c(0, data_inReg) 
         )
         
 if __name__ == "__main__":
