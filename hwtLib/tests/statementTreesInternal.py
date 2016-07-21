03a57d2dadd3021ff483d8269ddb93611f6a6cd3
Nic30 <nic30@seznam.cz>
2016-07-21 17:29:11 +0200

    new test case

---
+++ hwtLib/tests/statementTreesInternal.py
@@ -0,0 +1,40 @@
+import unittest
+from hdl_toolkit.synthetisator.rtlLevel.netlist import RtlNetlist
+from hdl_toolkit.synthetisator.assigRenderer import getBaseCond
+
+
+
+
+class StatementTreesTC(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.n = RtlNetlist("test")
+    
+    def test_getBaseCond(self):
+        a = self.n.sig('a')
+        _a = getBaseCond(~a)
+        self.assertIs(a, _a[0])
+        self.assertIs(True, _a[1])
+        
+        _a = getBaseCond(a)
+        self.assertIs(a, _a[0])
+        self.assertIs(False, _a[1])
+        
+        
+        b = a < self.n.sig('b')
+        _b = getBaseCond(~b)
+        
+        self.assertIs(b, _b[0])
+        self.assertIs(True, _b[1])
+        
+        _b = getBaseCond(b)
+        self.assertIs(b, _b[0])
+        self.assertIs(False, _b[1])
+        
+        
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    #suite.addTest(StatementTreesTC('test_basicSwitch'))
+    suite.addTest(unittest.makeSuite(StatementTreesTC))
+    runner = unittest.TextTestRunner(verbosity=3)
+    runner.run(suite)
