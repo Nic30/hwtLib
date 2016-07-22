e5b57b399f7695346fe4dda7cc2b1fb3011eb65e
Nic30 <nic30@seznam.cz>
2016-07-22 16:54:55 +0200

    new: c like types

---
+++ hwtLib/types/ctypes.py
@@ -0,0 +1,13 @@
+from hdl_toolkit.hdlObjects.typeShortcuts import vecT
+
+
+char = vecT(8, False)
+
+uint16_t = vecT(16, False)
+int16_t = vecT(16, True)
+
+uint32_t = vecT(32, False)
+int32_t = vecT(32, True)
+
+uint64_t = vecT(64, False)
+int32_t = vecT(64, True)
