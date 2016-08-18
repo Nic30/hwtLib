d7f0c7b22f665564a21d216731f1485c6ff321a6
Michal Orsak <xorsak01@fit.vutbr.cz>
2016-08-18 15:25:52 +0200

    new: aluTypes

---
+++ hwtLib/proc/light8051/aluTypes.py
@@ -0,0 +1,10 @@
+from hdl_toolkit.hdlObjects.typeShortcuts import vecT
+t_alu_op_sel = vecT(4, False)
+t_alu_fns = vecT(6, False)
+
+class AI():
+    "alu instruction"
+    A_T = 0b0101
+    T_0 = 0
+    V_T = 0b1001
+    A_0 = 0b0100
