2503cc67019159bbad4e8d7e418f0330b83a5687
Michal Orsak <xorsak01@fit.vutbr.cz>
2016-08-18 22:28:41 +0200

    address space map for bus converter

--- hwtLib/abstract/busConverter.py
+++ hwtLib/abstract/busConverter.py
@@ -2,6 +2,8 @@ from hdl_toolkit.synthesizer.interfaceLevel.unit import Unit
 from hdl_toolkit.interfaces.std import BramPort_withoutClk, RegCntrl
 from hdl_toolkit.interfaces.utils import log2ceil
 from hdl_toolkit.hdlObjects.types.typeCast import toHVal
+from hwtLib.abstract.addrSpace import AddrSpaceItem
+from hdl_toolkit.synthesizer.param import evalParam
 
 def unpackAddrMap(am):
     try:
@@ -31,28 +33,38 @@ class BusConverter(Unit):
     def getMaxAddr(self):
         m = self.ADRESS_MAP[-1]
         if len(m) == 2:
-            return m[0]
+            return toHVal(m[0])
         elif len(m) == 3:
             # base + size -1
             return toHVal(m[0]) + (m[2] - 1)
+    
+    def suggestedAddrWidth(self):
+        maxAddr = evalParam(self.getMaxAddr()).val
+        return maxAddr.bit_length()
         
     def decorateWithConvertedInterfaces(self):
         assert len(self.ADRESS_MAP) > 0
  
-        
         self._directlyMapped = []
         self._bramPortMapped = []
+        self._addrSpace = []
+        
         for am in self.ADRESS_MAP:
             addr, name, size = unpackAddrMap(am)
             if size is None:
-                    p = RegCntrl()
-                    p._replaceParam("DATA_WIDTH", self.DATA_WIDTH)
-                    self._directlyMapped.append((addr, p))
+                size = 1
+                p = RegCntrl()
+                p._replaceParam("DATA_WIDTH", self.DATA_WIDTH)
+                self._directlyMapped.append((addr, p))
             else:
                 p = BramPort_withoutClk()
                 p._replaceParam("DATA_WIDTH", self.DATA_WIDTH)
                 p.ADDR_WIDTH.set(log2ceil(size - 1))
                 self._bramPortMapped.append((addr, p, size))
 
+            ai = AddrSpaceItem(addr, name, size)
+            self._addrSpace.append(ai)
+            p._addrSpaceItem = ai
+            
             setattr(self, name, p)
     
