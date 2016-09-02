aa5aaec1b120e75f49089c84453bbda24ac191ea
Michal Orsak <xorsak01@fit.vutbr.cz>
2016-09-02 22:20:40 +0200

    new: register array

---
+++ hwtLib/mem/regArray.py
@@ -0,0 +1,37 @@
+from hdl_toolkit.synthesizer.interfaceLevel.unit import Unit
+from hdl_toolkit.synthesizer.param import Param, evalParam
+from hdl_toolkit.interfaces.utils import addClkRstn
+from hdl_toolkit.interfaces.std import RegCntrl
+from hdl_toolkit.hdlObjects.typeShortcuts import vecT
+from hdl_toolkit.synthesizer.codeOps import If, c
+
+
+class RegArray(Unit):
+    def _config(self):
+        self.ITEMS = Param(4)
+        self.DATA_WIDTH = Param(8)
+        
+    def _declr(self):
+        with self._asExtern(), self._paramsShared():
+            addClkRstn(self)
+            self.data = RegCntrl(multipliedBy=self.ITEMS)
+    
+    def _impl(self):
+        regs = self.regs = [self._reg("reg%d" % (i), vecT(self.DATA_WIDTH)  ) 
+                            for i in range(evalParam(self.ITEMS).val)]
+        
+        for reg, intf in zip(regs, self.data):
+            If(intf.dout.vld,
+                c(intf.dout.data, reg)
+            ).Else(
+                reg._same()
+            )
+            
+            c(reg, intf.din)
+        
+        
+
+if __name__ == "__main__":
+    from hdl_toolkit.synthesizer.shortcuts import toRtl
+    u = RegArray()
+    print(toRtl(u))
