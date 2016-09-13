a447af61af67b88d6ac27a4b14ca7c5d344280ae
Michal Orsak <xorsak01@fit.vutbr.cz>
2016-09-13 09:43:08 +0200

    cleanup

--- hwtLib/samples/iLvl/simple_test.py
+++ hwtLib/samples/iLvl/simple_test.py
@@ -3,9 +3,8 @@ import unittest
 
 from hdl_toolkit.hdlObjects.specialValues import Time
 from hdl_toolkit.simulator.agentConnector import autoAddAgents, agInts
-from hdl_toolkit.simulator.shortcuts import simUnitVcd 
-from hdl_toolkit.synthesizer.shortcuts import synthesised
-from hwtLib.samples.iLvl.hierarchy.simpleSubunit import SimpleSubunit
+from hdl_toolkit.simulator.shortcuts import simUnitVcd, simPrepare
+from hwtLib.samples.iLvl.simple import SimpleUnit
 
 
 # unittest.TestCase is class of unit test framework integrated in python
@@ -14,15 +13,15 @@ class SimpleTC(unittest.TestCase):
     # if method name starts with "test" unittest framework know that this method is test 
     def testSimple(self):
         # create a unit instance
-        u = SimpleSubunit()
+        u = SimpleUnit()
         # convert it to rtl level
-        synthesised(u)
+        u, model = simPrepare(u)
         
         # decorate interface with agents (._ag property) which will drive or monitor values on the interface
         # there drivers and monitors are returned and stored in procs, we will need them for simulation
         procs = autoAddAgents(u)
         
-        # there we have our test data, because SimpleSubunit has only connection inside 
+        # there we have our test data, because SimpleUnit has only connection inside 
         # None represents invalid value (like universal "x" in vhdl)
         inputData = [0, 1, 0, 1, None, 0, None, 1, None, 0]
         
@@ -37,7 +36,7 @@ class SimpleTC(unittest.TestCase):
         # now we run simulation, we use our unit "u", our monitors and drivers of interfaces stored in "procs",
         # we save dum of value changes into file "tmp/simple.vcd"
         # and we let simulation run for 100 ns
-        simUnitVcd(u, procs, "tmp/simple.vcd", time=100 * Time.ns)
+        simUnitVcd(model, procs, "tmp/simple.vcd", time=100 * Time.ns)
         
         # now we use part of unittest framework to check results
         self.assertSequenceEqual(expected, agInts(u.b))
@@ -50,7 +49,7 @@ if __name__ == "__main__":
     # this is how you can select specific test
     # suite.addTest(TwoCntrsTC('test_withStops'))
     
-    #this is how you add all test from testcase
+    # this is how you add all test from testcase
     suite.addTest(unittest.makeSuite(SimpleTC))
 
     runner = unittest.TextTestRunner(verbosity=3)
