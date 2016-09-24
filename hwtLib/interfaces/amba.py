99cbbd70df8d4c91458d9ef32691881f021d3b91
nic30 <nic30@seznam.cz>
2016-09-24 22:49:59 +0200

    codestyle (rm parameter which already has this def. value)

--- hwtLib/interfaces/amba.py
+++ hwtLib/interfaces/amba.py
@@ -30,10 +30,10 @@ class AxiStream_withoutSTRB(Interface):
         self.DATA_WIDTH = Param(64)
 
     def _declr(self):
-        self.last = s(masterDir=D.OUT, alternativeNames=['tlast' ])
-        self.data = s(masterDir=D.OUT, dtype=vecT(self.DATA_WIDTH), alternativeNames=['tdata' ])
+        self.last = s(alternativeNames=['tlast' ])
+        self.data = s(dtype=vecT(self.DATA_WIDTH), alternativeNames=['tdata' ])
         self.ready = s(masterDir=D.IN, alternativeNames=['tready' ])
-        self.valid = s(masterDir=D.OUT, alternativeNames=['tvalid' ])
+        self.valid = s(alternativeNames=['tvalid' ])
     
     def _getIpCoreIntfClass(self):
         return IP_AXIStream
@@ -41,18 +41,16 @@ class AxiStream_withoutSTRB(Interface):
 class AxiStream(AxiStream_withoutSTRB):
     def _declr(self):
         super(AxiStream, self)._declr()
-        self.strb = s(masterDir=D.OUT,
-                 dtype=vecT(self.DATA_WIDTH // 8),
-                 alternativeNames=['tstrb', 'keep', 'tkeep' ])
+        self.strb = s(dtype=vecT(self.DATA_WIDTH // 8),
+                      alternativeNames=['tstrb', 'keep', 'tkeep' ])
         
 class Axi_user(Interface):
     def _config(self):
-        self.USER_WIDTH = Param(0)
+        self.USER_WIDTH = Param(1)
         
     def _declr(self):
-        self.user = s(masterDir=D.OUT,
-                 dtype=vecT(self.USER_WIDTH),
-                 alternativeNames=['tuser'])
+        self.user = s(dtype=vecT(self.USER_WIDTH),
+                      alternativeNames=['tuser'])
 
 class AxiStream_withUserAndNoStrb(AxiStream_withoutSTRB, Axi_user):
     def _config(self):
@@ -79,9 +77,9 @@ class AxiLite_addr(Interface):
         self.ADDR_WIDTH = Param(32)
         
     def _declr(self):
-        self.addr = s(masterDir=D.OUT, dtype=vecT(self.ADDR_WIDTH), alternativeNames=['addr_v'])
+        self.addr = s(dtype=vecT(self.ADDR_WIDTH), alternativeNames=['addr_v'])
         self.ready = s(masterDir=D.IN)
-        self.valid = s(masterDir=D.OUT)
+        self.valid = s()
 
 class AxiLite_r(Interface):
     def _config(self):
@@ -90,7 +88,7 @@ class AxiLite_r(Interface):
     def _declr(self):
         self.data = s(masterDir=D.IN, dtype=vecT(self.DATA_WIDTH), alternativeNames=['data_v'])
         self.resp = s(masterDir=D.IN, dtype=vecT(2), alternativeNames=['resp_v'])
-        self.ready = s(masterDir=D.OUT)
+        self.ready = s()
         self.valid = s(masterDir=D.IN)
 
 class AxiLite_w(Interface):
@@ -98,17 +96,16 @@ class AxiLite_w(Interface):
         self.DATA_WIDTH = Param(64)
         
     def _declr(self):
-        self.data = s(masterDir=D.OUT, dtype=vecT(self.DATA_WIDTH), alternativeNames=['data_v'])
-        self.strb = s(masterDir=D.OUT,
-                      dtype=vecT(self.DATA_WIDTH // 8), alternativeNames=['strb_v'])
+        self.data = s(dtype=vecT(self.DATA_WIDTH), alternativeNames=['data_v'])
+        self.strb = s(dtype=vecT(self.DATA_WIDTH // 8), alternativeNames=['strb_v'])
         self.ready = s(masterDir=D.IN)
-        self.valid = s(masterDir=D.OUT)
+        self.valid = s()
         
     
 class AxiLite_b(Interface):
     def _declr(self):
         self.resp = s(masterDir=D.IN, dtype=vecT(2), alternativeNames=['resp_v'])
-        self.ready = s(masterDir=D.OUT)
+        self.ready = s()
         self.valid = s(masterDir=D.IN)
 
 class AxiLite(Interface):
@@ -153,14 +150,14 @@ class Axi4_addr(AxiLite_addr):
     
     def _declr(self):
         super(Axi4_addr, self)._declr()
-        self.id = s(masterDir=D.OUT, dtype=vecT(self.ID_WIDTH), alternativeNames=['id_v'])
-        self.burst = s(masterDir=D.OUT, dtype=vecT(2), alternativeNames=['burst_v'])
-        self.cache = s(masterDir=D.OUT, dtype=vecT(4), alternativeNames=['cache_v'])
-        self.len = s(masterDir=D.OUT, dtype=vecT(8), alternativeNames=['len_v'])
-        self.lock = s(masterDir=D.OUT, dtype=vecT(1), alternativeNames=['lock_v'])
-        self.prot = s(masterDir=D.OUT, dtype=vecT(3), alternativeNames=['prot_v'])
-        self.size = s(masterDir=D.OUT, dtype=vecT(3), alternativeNames=['size_v'])
-        self.qos = s(masterDir=D.OUT, dtype=vecT(4), alternativeNames=['qos_v'])
+        self.id = s(dtype=vecT(self.ID_WIDTH), alternativeNames=['id_v'])
+        self.burst = s(dtype=vecT(2), alternativeNames=['burst_v'])
+        self.cache = s(dtype=vecT(4), alternativeNames=['cache_v'])
+        self.len = s(dtype=vecT(8), alternativeNames=['len_v'])
+        self.lock = s(dtype=vecT(1), alternativeNames=['lock_v'])
+        self.prot = s(dtype=vecT(3), alternativeNames=['prot_v'])
+        self.size = s(dtype=vecT(3), alternativeNames=['size_v'])
+        self.qos = s(dtype=vecT(4), alternativeNames=['qos_v'])
 
 
 class Axi4_r(AxiLite_r):
@@ -180,8 +177,8 @@ class Axi4_w(AxiLite_w):
     
     def _declr(self):
         super(Axi4_w, self)._declr()
-        self.id = s(masterDir=D.OUT, dtype=vecT(self.ID_WIDTH), alternativeNames=['id_v'])
-        self.last = s(masterDir=D.OUT)
+        self.id = s(dtype=vecT(self.ID_WIDTH), alternativeNames=['id_v'])
+        self.last = s()
     
 class Axi4_b(AxiLite_b):
     def _config(self):
