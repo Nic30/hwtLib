8efdbb8b2ed2a0fbb53ecd2a56397a99cbf33283
nic30 <nic30@seznam.cz>
2016-10-03 13:57:02 +0200

    rework transaction info passing

--- hwtLib/axi/axi4_wDatapump.py
+++ hwtLib/axi/axi4_wDatapump.py
@@ -2,26 +2,46 @@
 # -*- coding: utf-8 -*-
 
 from hdl_toolkit.bitmask import mask
-from hdl_toolkit.interfaces.std import Signal
+from hdl_toolkit.interfaces.std import Signal, Handshaked, VectSignal, \
+    HandshakeSync
 from hdl_toolkit.interfaces.utils import addClkRstn, propagateClkRstn, log2ceil
-from hdl_toolkit.synthesizer.codeOps import connect, If
+from hdl_toolkit.synthesizer.codeOps import connect, If, Concat
 from hdl_toolkit.synthesizer.interfaceLevel.unit import Unit
 from hdl_toolkit.synthesizer.param import Param, evalParam
 from hwtLib.axi.axi4_rDatapump import AddrSizeHs
 from hwtLib.interfaces.amba import Axi4_w, Axi4_addr, AxiStream, Axi4_b
 from hwtLib.interfaces.amba_constants import BURST_INCR, CACHE_DEFAULT, LOCK_DEFAULT, PROT_DEFAULT, QOS_DEFAULT, \
     BYTES_IN_TRANS, RESP_OKAY
+from hwtLib.handshaked.fifo import HandshakedFifo
+from hwtLib.handshaked.builder import HsBuilder
 
 
 def axiHsAck(m, s):
     return m.valid & s.ready
 
+class WFifoIntf(Handshaked):
+    def _config(self):
+        self.ID_WIDTH = Param(4)
+        
+    def _declr(self):
+        self.id = VectSignal(self.ID_WIDTH)
+        HandshakeSync._declr(self)
+
+class BFifoIntf(Handshaked):
+    def _config(self):
+        pass
+    
+    def _declr(self):
+        self.isLast = Signal()
+        HandshakeSync._declr(self)
+
+        
 class Axi4_wDatapump(Unit):
     """
-    @ivar param MAX_TRANS_OVERLAP: max number of concurent transactios
+    @ivar param MAX_TRANS_OVERLAP: max number of concurrent transactions
+ 
     """
     def _config(self):
-        self.DEFAULT_ID = Param(0)
         self.MAX_TRANS_OVERLAP = Param(16)
         self.MAX_LEN = Param(4096 // 8 - 1)
         
@@ -39,29 +59,39 @@ class Axi4_wDatapump(Unit):
                 self.b = Axi4_b()
                 
                 self.req = AddrSizeHs()
+
                 self.wIn = AxiStream()
                 
                 self.wErrFlag = Signal()
-                
-        
+            self.reqAck = Handshaked()
+            self.reqAck.DATA_WIDTH.set(self.ID_WIDTH)
+        with self._paramsShared():
+            # fifo for id propagation and frame splitting on axi.w channel 
+            wf = self.writeInfoFifo = HandshakedFifo(WFifoIntf)
+            wf.ID_WIDTH.set(self.ID_WIDTH)
+            wf.DEPTH.set(self.MAX_TRANS_OVERLAP)
+            
+            # fifo for propagation of end of frame from axi.b channel
+            bf = self.bInfoFifo = HandshakedFifo(BFifoIntf)
+            bf.DEPTH.set(self.MAX_TRANS_OVERLAP)
+            
+    
     def useTransSplitting(self):
-        return self.req.len._dtype.bit_length() > self.ar.len._dtype.bit_length()
+        return evalParam(self.MAX_LEN).val > self.aw.len._dtype.bit_length()
     
     def getSizeAlignBits(self):
         return log2ceil(self.DATA_WIDTH // 8).val
     
     def getAxiLenMax(self):
-        return mask(self.ar.len._dtype.bit_length())
+        return mask(self.aw.len._dtype.bit_length())
     
     def getBurstAddrOffset(self):
         return (self.getAxiLenMax() + 1) << self.getSizeAlignBits()
     
-     
-    def axiAwHandler(self, enable):
+    def axiAwHandler(self):
         req = self.req
         aw = self.aw
-        
-        aw.id ** self.DEFAULT_ID
+
         aw.burst ** BURST_INCR
         aw.cache ** CACHE_DEFAULT
         aw.lock ** LOCK_DEFAULT
@@ -71,56 +101,79 @@ class Axi4_wDatapump(Unit):
 
         if self.useTransSplitting():
             LEN_MAX = mask(aw.len._dtype.bit_length())
+            wInfo = self.writeInfoFifo.dataIn
             
             lastReqDispatched = self._reg("lastReqDispatched", defVal=1)
-            
             lenDebth = self._reg("lenDebth", req.len._dtype)
-            rAddr = self._reg("r_addr", req.addr._dtype)
+            addrBackup = self._reg("addrBackup", req.addr._dtype)
+            req_idBackup = self._reg("req_idBackup", self.req.id._dtype)
+            _id = self._sig("id", aw.id._dtype)
+                    
+            en = wInfo.rd
+            
+
             
             If(lastReqDispatched,
-               aw.addr ** req.addr,
-               connect(req.len, aw.len, fit=True),
-               aw.valid ** (enable & req.vld),
-               rAddr ** (req.addr + self.getBurstAddrOffset()),
-               lenDebth ** (req.len - LEN_MAX),
-               If(enable & (req.len > LEN_MAX) & aw.ready & req.vld,
-                  lastReqDispatched ** 1 
-               )
+                _id ** req.id,
+                aw.addr ** req.addr,
+                connect(req.len, aw.len, fit=True),
+                aw.valid ** (en & req.vld),
+
+                wInfo.vld ** (req.vld & aw.ready),
+                
+                req.rd ** (wInfo.rd & aw.ready),
+                
+                req_idBackup ** req.id,
+                addrBackup ** (req.addr + self.getBurstAddrOffset()),
+                lenDebth ** (req.len - (LEN_MAX + 1)),
+                If(en & (req.len > LEN_MAX) & aw.ready & req.vld,
+                   lastReqDispatched ** 1 
+                )
             ).Else(
-               aw.addr ** rAddr,
-               connect(lenDebth, aw.len, fit=True),
-               aw.valid ** enable,
-               If(enable & aw.ready,
-                  rAddr ** (rAddr + self.getBurstAddrOffset()),
-                  lenDebth ** (lenDebth - LEN_MAX),
-                  If(lenDebth <= LEN_MAX,
-                     lastReqDispatched ** 1
-                  )
-               )
+                _id ** req_idBackup,
+                aw.addr ** addrBackup,
+                connect(lenDebth, aw.len, fit=True),
+                aw.valid ** en,
+                
+                wInfo.vld ** aw.ready,
+                req.rd ** 0,
+                
+                If(en & aw.ready,
+                   addrBackup ** (addrBackup + self.getBurstAddrOffset()),
+                   lenDebth ** (lenDebth - LEN_MAX),
+                   If(lenDebth <= LEN_MAX,
+                      lastReqDispatched ** 1
+                   )
+                )
             )
-            
-            ack = aw.ready & ((lastReqDispatched & req.vld) | ~lastReqDispatched)
+            aw.id ** _id
+            wInfo.id ** _id
             
         else:
+            aw.id ** req.id
             aw.addr ** req.addr
             connect(req.len, aw.len, fit=True)
             aw.valid ** req.vld 
             req.rd ** aw.ready
             
-            ack = aw.ready & req.vld
-            
-        return ack
+
     
-    def axiWHandler(self, enable):
+    def axiWHandler(self):
         w = self.w
         wIn = self.wIn
-
-        w.id ** self.DEFAULT_ID
+        wInfo = HsBuilder(self, self.writeInfoFifo.dataOut).reg().end
+        bInfo = self.bInfoFifo.dataIn
+        
+        enable = wInfo.vld & bInfo.rd
+        
+        w.id ** wInfo.id
         w.data ** wIn.data
         w.strb ** wIn.strb
         
         if self.useTransSplitting():
             wordCntr = self._reg("wWordCntr", self.aw.len._dtype, 0)
+            doSplit = wordCntr._eq(self.getAxiLenMax())
+            
             If(enable & axiHsAck(wIn, w),
                If(wIn.last,
                    wordCntr ** 0
@@ -128,8 +181,11 @@ class Axi4_wDatapump(Unit):
                    wordCntr ** (wordCntr + 1)
                )
             )
+            wInfo.rd ** (axiHsAck(wIn, w) & (doSplit | wIn.last) & bInfo.rd)
+            bInfo.isLast ** wIn.last
+            bInfo.vld ** (axiHsAck(wIn, w) & (doSplit | wIn.last) & wInfo.vld)
             
-            w.last ** (wordCntr._eq(self.getAxiLenMax()) | wIn.last)
+            w.last ** (doSplit | wIn.last)
             w.valid ** (enable & wIn.valid)
             wIn.ready ** (enable & w.ready)
 
@@ -138,20 +194,34 @@ class Axi4_wDatapump(Unit):
             w.valid ** (enable & wIn.valid)
             wIn.ready ** w.ready
             
-    def axiBHandler(self, enable):
+    def axiBHandler(self):
         wErrFlag = self._reg("wErrFlag", defVal=0)
         b = self.b
-        If(enable & b.valid & (b.resp != RESP_OKAY),
+        reqAck = self.reqAck
+        
+        lastFlags = HsBuilder(self, self.bInfoFifo.dataOut).reg().end
+        
+        If(lastFlags.vld & reqAck.rd & b.valid & (b.resp != RESP_OKAY),
            wErrFlag ** 1
         )
 
-        self.wErrFlag * wErrFlag 
-        b.ready ** enable
+        self.wErrFlag ** wErrFlag 
+        b.ready ** (lastFlags.vld & reqAck.rd)
+        lastFlags.rd ** (reqAck.rd & b.valid) 
         
-        ack = b.valid
-        return ack
+        reqAck.vld ** (lastFlags.vld & lastFlags.isLast & b.valid)
+        reqAck.data ** b.id
+        
     
     def _impl(self):
         propagateClkRstn(self)
         
+        self.axiAwHandler()
         self.axiWHandler()
+        self.axiBHandler()
+        
+        
+if __name__ == "__main__":
+    from hdl_toolkit.synthesizer.shortcuts import toRtl
+    u = Axi4_wDatapump()
+    print(toRtl(u))
