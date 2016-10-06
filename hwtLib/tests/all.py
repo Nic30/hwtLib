ccb65ac432f0e567ccceaa52ac020228d6d78736
nic30 <nic30@seznam.cz>
2016-10-06 23:18:19 +0200

    import sort

--- hwtLib/tests/all.py
+++ hwtLib/tests/all.py
@@ -4,6 +4,7 @@
 from unittest import TestLoader, TextTestRunner, TestSuite
 
 from hwtLib.axi.axi4_rDatapump_test import Axi4_rDatapumpTC
+from hwtLib.axi.axi4_wDatapump_test import Axi4_wDatapumpTC
 from hwtLib.handshaked.fork_test import ForkTC
 from hwtLib.handshaked.reg2_test import HsReg2TC
 from hwtLib.handshaked.reg_test import HsRegTC
@@ -36,7 +37,6 @@ from hwtLib.tests.synthesizer.interfaceLevel.subunitsSynthesisTC import Subunits
 from hwtLib.tests.synthesizer.rtlLevel.optimalizator import Expr2CondTC
 from hwtLib.tests.synthesizer.rtlLevel.synthesis import TestCaseSynthesis
 from hwtLib.tests.synthesizer.value import ValueTC
-from hwtLib.axi.axi4_wDatapump_test import Axi4_wDatapumpTC
 
 
 if __name__ == "__main__":
