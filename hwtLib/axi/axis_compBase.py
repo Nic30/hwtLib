337c9b8dcddf1c33035f73f1de5256692131fc76
nic30 <nic30@seznam.cz>
2016-10-11 23:58:38 +0200

    better error message

--- hwtLib/axi/axis_compBase.py
+++ hwtLib/axi/axis_compBase.py
@@ -11,7 +11,7 @@ class AxiSCompBase(HandshakedCompBase):
         """
         @param hsIntfCls: class of interface which should be used as interface of this unit
         """
-        assert(issubclass(hsIntfCls, AxiStream_withoutSTRB))
+        assert(issubclass(hsIntfCls, AxiStream_withoutSTRB)), hsIntfCls
         self.intfCls = hsIntfCls
         Unit.__init__(self)
         
