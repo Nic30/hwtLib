06e1fcf542a966592b5e3b13e5cfeecea1779c38
nic30 <nic30@seznam.cz>
2016-10-12 19:04:12 +0200

    more tests for measuring Fifo

--- hwtLib/axi/axis_measuringFifo_test.py
+++ hwtLib/axi/axis_measuringFifo_test.py
@@ -7,7 +7,6 @@ from hdl_toolkit.bitmask import mask
 from hdl_toolkit.hdlObjects.specialValues import Time
 from hdl_toolkit.simulator.agentConnector import valuesToInts
 from hdl_toolkit.simulator.shortcuts import simUnitVcd, simPrepare
-from hwtLib.axi.axi4_rDatapump import Axi4_rDataPump
 from hwtLib.axi.axis_measuringFifo import AxiS_measuringFifo
 
 
@@ -37,17 +36,71 @@ class AxiS_measuringFifoTC(unittest.TestCase):
     def test_singleWordPacket(self):
         u = self.u
         
-        
-        u.dataIn._ag.data.extend(((1,255,0), 
-                                  (2,255,1)))
+        u.dataIn._ag.data.extend([(2, 255, 1),
+                                  ])
 
         self.doSim(200 * Time.ns)
         self.assertEqual(len(u.sizes._ag.data), 1)
-        self.assertEqual(len(u.dataOut._ag.data), 2)
-        
+        self.assertEqual(len(u.dataOut._ag.data), 1)
 
+
+    def test_multiplePackets(self):
+        u = self.u
+        sizes = u.sizes._ag.data
+        data = u.dataOut._ag.data
+        
+        u.dataIn._ag.data.extend([(1, 255, 1),
+                                  (2, 255, 1),
+                                  (3, 255, 1)
+                                  ])
+
+        self.doSim(200 * Time.ns)
+        self.assertEqual(len(sizes), 3)
+        self.assertEqual(len(data), 3)
+        self.assertSequenceEqual(valuesToInts(sizes), (8, 8, 8))
+
+        
+    def test_doubleWordPacket(self):
+        u = self.u
+        sizes = u.sizes._ag.data
+        data = u.dataOut._ag.data
+        
+        u.dataIn._ag.data.extend([(1, 255, 0),
+                                  (2, 255, 1)
+                                 ])
+
+        self.doSim(200 * Time.ns)
+        self.assertEqual(len(sizes), 1)
+        self.assertEqual(len(data), 2)
+        self.assertSequenceEqual(valuesToInts(sizes), (16,))
     
+    def test_unalignedPacket(self):    
+        u = self.u
+        sizes = u.sizes._ag.data
+        data = u.dataOut._ag.data
+        
+        u.dataIn._ag.data.extend([(1, 255, 0),
+                                  (2, 1, 1)
+                                 ])
+        self.doSim(200 * Time.ns)
+        self.assertEqual(len(sizes), 1)
+        self.assertEqual(len(data), 2)
+        self.assertSequenceEqual(valuesToInts(sizes), (9,))
     
+    def test_unalignedPacket1Word(self):    
+        u = self.u
+        sizes = u.sizes._ag.data
+        data = u.dataOut._ag.data
+        
+        u.dataIn._ag.data.extend([
+                                  (2, 1, 1),
+                                 ])
+        self.doSim(200 * Time.ns)
+        self.assertEqual(len(sizes), 1)
+        self.assertEqual(len(data), 1)
+        self.assertSequenceEqual(valuesToInts(sizes), (1,))
+    
+        
 if __name__ == "__main__":
     suite = unittest.TestSuite()
     # suite.addTest(AxiS_measuringFifoTC('test_nop'))
