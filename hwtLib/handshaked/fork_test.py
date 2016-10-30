0a57414c02742ad30ff182dabe9b764c830027fa
nic30 <nic30@seznam.cz>
2016-10-30 11:09:18 +0100

    fixed example because now we have agents for array interfaces

--- hwtLib/handshaked/fork_test.py
+++ hwtLib/handshaked/fork_test.py
@@ -3,18 +3,18 @@
 
 import unittest
 
+from hdl_toolkit.hdlObjects.specialValues import Time
 from hdl_toolkit.interfaces.std import Handshaked
 from hdl_toolkit.simulator.agentConnector import agInts
 from hdl_toolkit.simulator.shortcuts import simUnitVcd, simPrepare
 from hwtLib.handshaked.fork import HandshakedFork
-from hdl_toolkit.hdlObjects.specialValues import Time
 
 
 class ForkTC(unittest.TestCase):
     def setUp(self):
-        u = HandshakedFork(Handshaked)
-        u.DATA_WIDTH.set(4)
-        self.u, self.model, self.procs = simPrepare(u)
+        self.u = HandshakedFork(Handshaked)
+        self.u.DATA_WIDTH.set(4)
+        _, self.model, self.procs = simPrepare(self.u)
     
     def doSim(self, name, time=80 * Time.ns):
         simUnitVcd(self.model, self.procs,
@@ -27,9 +27,13 @@ class ForkTC(unittest.TestCase):
 
         self.doSim("passdata", 120 * Time.ns)
 
-        collected = agInts(u.dataOut)
-        self.assertSequenceEqual([17], collected)  # 1 was in reset
-        self.assertSequenceEqual([2, 3, 4, 5, 6], u.dataIn._ag.data)
+        collected0 = agInts(u.dataOut[0])
+        collected1 = agInts(u.dataOut[1])
+        
+        self.assertSequenceEqual([1, 2, 3, 4, 5, 6], collected0)
+        self.assertSequenceEqual([1, 2, 3, 4, 5, 6], collected1)
+        
+        self.assertSequenceEqual([], u.dataIn._ag.data)
 
 
 if __name__ == "__main__":
