556600e3d5d1f9fb6a566bae31fa0ab08601998b
nic30 <nic30@seznam.cz>
2016-11-01 09:49:42 +0100

    axi4 r datapump test_multipleSplited

--- hwtLib/axi/axi4_rDatapump_test.py
+++ hwtLib/axi/axi4_rDatapump_test.py
@@ -10,7 +10,7 @@ from hdl_toolkit.simulator.shortcuts import simUnitVcd, simPrepare
 from hwtLib.axi.axi4_rDatapump import Axi_rDatapump
 from hwtLib.interfaces.amba import Axi3_addr_withUser
 from hwtLib.interfaces.amba_constants import BURST_INCR, CACHE_DEFAULT, \
-    LOCK_DEFAULT, PROT_DEFAULT, BYTES_IN_TRANS, QOS_DEFAULT
+    LOCK_DEFAULT, PROT_DEFAULT, BYTES_IN_TRANS, QOS_DEFAULT, RESP_OKAY
 
 
 class Axi4_rDatapumpTC(unittest.TestCase):
@@ -153,7 +153,78 @@ class Axi4_rDatapumpTC(unittest.TestCase):
             _len = 0
             self.assertSequenceEqual(valuesToInts(arreq),
                                      self.mkDefaultAddrReq(_id, addr, _len))
+    
+    def test_multipleShortest(self):
+        u = self.u
+        _id = 0
         
+        req = u.req._ag
+        r = u.r._ag
+        ar = u.a._ag.data
+        rout = u.rOut._ag.data
+        
+        for i in range(64):
+            req.data.append(req.mkReq(i, 0))
+            rdata = (_id, i + 1, RESP_OKAY, True)
+            r.data.append(rdata)
+        
+        self.doSim(((64 + 4) * 10) * Time.ns)
+        
+        self.assertEqual(len(req.data), 0)
+        self.assertEqual(len(ar), 64)
+        self.assertEqual(len(rout), 64)
+        
+        for i, arreq in enumerate(ar):
+            addr = i
+            _len = 0
+            self.assertSequenceEqual(valuesToInts(arreq),
+                                     self.mkDefaultAddrReq(_id, addr, _len))
+    
+    def test_multipleSplited(self):
+        _id = 0
+        FRAMES = 64
+        l = self.LEN_MAX + 1
+
+        u = self.u
+
+        req = u.req._ag
+        r = u.r._ag
+        ar = u.a._ag.data
+        rout = u.rOut._ag.data
+        
+        for i in range(FRAMES):
+            req.data.append(req.mkReq(i, l, _id))
+            for i2 in range(l + 1):
+                isLast = (i2 > 0 and ((i2 % self.LEN_MAX) == 0) or (i2 == l))
+                rdata = (_id, i2, RESP_OKAY, isLast)
+                r.data.append(rdata)
+        
+        self.doSim(((len(r.data) + 4) * 10) * Time.ns)
+        
+        self.assertEqual(len(req.data), 0)
+        self.assertEqual(len(ar), FRAMES * 2)
+        self.assertEqual(len(rout), FRAMES * (l + 1))
+        
+        for i, arreq in enumerate(ar):
+            if i % 2 == 0:
+                addr = i // 2
+                _len = self.LEN_MAX
+            else:
+                addr = (i // 2) + 8 * (self.LEN_MAX + 1)
+                _len = 0
+                
+            self.assertSequenceEqual(valuesToInts(arreq),
+                                     self.mkDefaultAddrReq(_id, addr, _len))
+            
+        _rout = iter(rout)
+        
+        for i in range(FRAMES):
+            for i2 in range(l + 1):
+                isLast = (i2 == l)
+                rdata = (_id, i2, mask(8), isLast)
+                d = next(_rout)
+                self.assertSequenceEqual(valuesToInts(d), rdata)
+
         
 class Axi3_rDatapumpTC(Axi4_rDatapumpTC):
     LEN_MAX = 15
