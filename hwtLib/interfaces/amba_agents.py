cc4ec1f4106047ca313ab4cb697715e87c875fc9
nic30 <nic30@seznam.cz>
2016-11-26 14:21:21 +0100

    fix default sizes for axi agents

--- hwtLib/interfaces/amba_agents.py
+++ hwtLib/interfaces/amba_agents.py
@@ -34,7 +34,7 @@ class Axi4_addrAgent(BaseAxiAgent):
                                        cache=CACHE_DEFAULT,
                                        lock=LOCK_DEFAULT,
                                        prot=PROT_DEFAULT,
-                                       size=BYTES_IN_TRANS(64),
+                                       size=BYTES_IN_TRANS(8),
                                        qos=QOS_DEFAULT):
         
         return (_id, addr, burst, cache, _len, lock, prot, size, qos)
@@ -80,7 +80,7 @@ class Axi4_addr_withUserAgent(BaseAxiAgent):
                                        cache=CACHE_DEFAULT,
                                        lock=LOCK_DEFAULT,
                                        prot=PROT_DEFAULT,
-                                       size=BYTES_IN_TRANS(64),
+                                       size=BYTES_IN_TRANS(8),
                                        qos=QOS_DEFAULT,
                                        user=0):
         
