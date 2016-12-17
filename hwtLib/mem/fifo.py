686731cd42f11854cc4525ec37c01e9fe2b04ffc
nic30 <nic30@seznam.cz>
2016-12-17 16:32:50 +0100

    refactoring

--- hwtLib/mem/fifo.py
+++ hwtLib/mem/fifo.py
@@ -18,6 +18,7 @@ class Fifo(Unit):
         self.DATA_WIDTH = Param(64)
         self.DEPTH = Param(200)
         self.EXPORT_SIZE = Param(False)
+        self.EXPORT_SPACE = Param(False)
     
     def _declr(self):
         addClkRstn(self)
@@ -26,25 +27,29 @@ class Fifo(Unit):
             self.dataOut = FifoReader()
         
         if evalParam(self.EXPORT_SIZE).val:
-            self.size = VectSignal(log2ceil(self.DEPTH + 1), signed=False) 
+            self.size = VectSignal(log2ceil(self.DEPTH + 1), signed=False)
+        if evalParam(self.EXPORT_SPACE).val:
+            self.space = VectSignal(log2ceil(self.DEPTH + 1), signed=False)
     
     def _impl(self):
         DEPTH = self.DEPTH
         assert evalParam(DEPTH).val > 0
         
         index_t = vecT(log2ceil(DEPTH), False)
+        s = self._sig
+        r = self._reg
         
-        mem = self.mem = self._sig("memory", Array(vecT(self.DATA_WIDTH), self.DEPTH))
-        wr_ptr = self._reg("wr_ptr", index_t, 0)
-        rd_ptr = self._reg("rd_ptr", index_t, 0)
-        EXPORT_SIZE = evalParam(self.EXPORT_SIZE).val
+        mem = self.mem = s("memory", Array(vecT(self.DATA_WIDTH), self.DEPTH))
+        wr_ptr = r("wr_ptr", index_t, 0)
+        rd_ptr = r("rd_ptr", index_t, 0)
         MAX_DEPTH = DEPTH - 1
         
         dout = self.dataOut
         din = self.dataIn
         
-        fifo_write = self._sig("fifo_write")
-        fifo_read = self._sig("fifo_read")
+        # we are storing signals to properties because someone else may use them 
+        self.__fifo_write = fifo_write = s("fifo_write")
+        self.__fifo_read =fifo_read = s("fifo_read")
 
         # Update Tail pointer as needed
         If(fifo_read,
@@ -73,7 +78,7 @@ class Fifo(Unit):
         
         # assert isPow2(evalParam(DEPTH).val), DEPTH
         
-        looped = self._reg("looped", defVal=False)
+        looped = r("looped", defVal=False)
         
         fifo_read ** (dout.en & (looped | (wr_ptr != rd_ptr)))
         If(self.clk._onRisingEdge(),
@@ -106,8 +111,8 @@ class Fifo(Unit):
             din.wait ** 0,
             dout.wait ** 0 
         )
-        if EXPORT_SIZE:
-            size = self._reg("size_reg", self.size._dtype, 0)
+        if evalParam(self.EXPORT_SIZE).val:
+            size = r("size_reg", self.size._dtype, 0)
             If(fifo_read,
                 If(~fifo_write,
                    size ** (size - 1)
@@ -118,7 +123,19 @@ class Fifo(Unit):
                 )
             )
             self.size ** size
-
+        if evalParam(self.EXPORT_SPACE).val:
+            space = r("space_reg", self.size._dtype, self.DEPTH)
+            If(fifo_read,
+                If(~fifo_write,
+                   size ** (space + 1)
+                )
+            ).Else(
+                If(fifo_write,
+                   size ** (space - 1)
+                )
+            )
+            self.space ** space
+        
 if __name__ == "__main__":
     from hdl_toolkit.synthesizer.shortcuts import toRtl
     u = Fifo()
