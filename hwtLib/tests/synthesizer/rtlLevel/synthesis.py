ead4e6d8a486b5bb0b0eb8624ffe51319e07ed90
nic30 <nic30@seznam.cz>
2016-12-18 20:40:10 +0100

    fix references

--- hwtLib/tests/synthesizer/rtlLevel/synthesis.py
+++ hwtLib/tests/synthesizer/rtlLevel/synthesis.py
@@ -5,7 +5,7 @@ import unittest
 
 from hdl_toolkit.hdlObjects.assignment import Assignment
 from hdl_toolkit.hdlObjects.operatorDefs import AllOps
-from hdl_toolkit.serializer.vhdlSerializer import VhdlSerializer
+from hdl_toolkit.serializer.vhdl.serializer import VhdlSerializer
 from hdl_toolkit.synthesizer.rtlLevel.netlist import RtlNetlist
 from hwtLib.samples.rtlLvl.indexOps import IndexOps
 
