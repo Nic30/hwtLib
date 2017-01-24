940860da7ef2a34910a3e4291f5fb7e4f85591bf
nic30 <nic30@seznam.cz>
2017-01-24 13:25:58 +0100

    more ctypes

--- hwtLib/types/ctypes.py
+++ hwtLib/types/ctypes.py
@@ -2,6 +2,8 @@ from hwt.hdlObjects.typeShortcuts import vecT
 
 
 char = vecT(8, False)
+uint8_t = vecT(8, False)
+int8_t = vecT(8, True)
 
 uint16_t = vecT(16, False)
 int16_t = vecT(16, True)
@@ -10,4 +12,5 @@ uint32_t = vecT(32, False)
 int32_t = vecT(32, True)
 
 uint64_t = vecT(64, False)
-int32_t = vecT(64, True)
+int64_t = vecT(64, True)
+
