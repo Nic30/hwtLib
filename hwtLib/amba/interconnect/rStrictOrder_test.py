d6b3e49f39edf7db761ea706ceb378403b9ddacf
nic30 <nic30@seznam.cz>
2017-02-20 13:42:59 +0100

    add RStrictOrderInterconnectTC.test_passWithData

--- hwtLib/amba/interconnect/rStrictOrder_test.py
+++ hwtLib/amba/interconnect/rStrictOrder_test.py
@@ -10,6 +10,7 @@ from hwt.simulator.utils import agent_randomize
 from hwt.synthesizer.param import evalParam
 from hwtLib.abstract.denseMemory import DenseMemory
 from hwtLib.amba.interconnect.rStricOrder import RStrictOrderInterconnect
+from hwt.bitmask import mask
 
 
 class RStrictOrderInterconnectTC(SimTestCase):
@@ -45,7 +46,24 @@ class RStrictOrderInterconnectTC(SimTestCase):
             self.assertValSequenceEqual(req,
                                         (i + 1, i + 1, i + 1, 0))
     def test_passWithData(self):
-        pass
+        u = self.u
+        
+        for i, driver in enumerate(u.drivers):
+            _id = i + 1
+            _len = i + 1
+            driver.req._ag.data.append((_id, i + 1, _len, 0))
+            for i2 in range(_len + 1):
+                u.rDatapump.r._ag.data.append((_id, i + 1, mask(self.DATA_WIDTH), i2 == _len))
+                
+        self.doSim(200 * Time.ns)
+        
+        for i, d in enumerate(u.drivers):
+            self.assertEqual(len(d.r._ag.data), i + 1 + 1)
+        
+        self.assertEqual(len(u.rDatapump.req._ag.data), 2)
+        for i, req in enumerate(u.rDatapump.req._ag.data):
+            self.assertValSequenceEqual(req,
+                                        (i + 1, i + 1, i + 1, 0))
    
 
     
