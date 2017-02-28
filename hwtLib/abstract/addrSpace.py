53bc6c641556fc63f79cc8aa7867f737a2e060bf
nic30 <nic30@seznam.cz>
2017-02-28 13:25:37 +0100

    pep8

--- hwtLib/abstract/addrSpace.py
+++ hwtLib/abstract/addrSpace.py
@@ -20,12 +20,12 @@ class AddrSpaceItem(object):
         self.alignOffsetBits = alignOffsetBits
         self.port = None
         self.children = {}
-    
+
     def assertNoOverlap(self, nextItem):
         left0 = self.addr + self.size
         right1 = nextItem.addr
         assert left0 <= right1, (self, nextItem)
-         
+
     @staticmethod
     def checkOverlapping(addrSpace):
         """
@@ -37,9 +37,9 @@ class AddrSpaceItem(object):
                 pass
             else:
                 last.assertNoOverlap(item)
-                
+
             last = item
-    
+
     def getMyAddrPrefix(self):
         """
         @return: None if base addr is not aligned to size and prefix can not be used
@@ -49,22 +49,21 @@ class AddrSpaceItem(object):
             return self.addr, 0
         else:
             subAddrBits = (self.size - 1).bit_length()
-        
+
         if self.addr & mask(subAddrBits):
             # is addr is not aligned to size
             return None
         return self.addr >> subAddrBits, subAddrBits
 
-            
     def __repr__(self):
         if self.children:
             children = ",\n" + pprint.pformat(self.children, 2) + "\n "
         else:
             children = ""
-        
+
         if self.size is None:
             size = 1
         else:
             size = self.size
-        
+
         return "<AddrSpaceItem %s, %d, size=%d%s>" % (self.name, self.addr, size, children)
