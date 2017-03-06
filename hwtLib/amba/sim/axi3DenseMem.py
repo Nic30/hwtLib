894bfc112e2dc851a4ff24ab65528da03e88819d
nic30 <nic30@seznam.cz>
2017-03-06 02:32:07 +0100

    rest of implementation of Axi3DenseMem

--- hwtLib/amba/sim/axi3DenseMem.py
+++ hwtLib/amba/sim/axi3DenseMem.py
@@ -1,13 +1,11 @@
 from hwtLib.abstract.denseMemory import DenseMemory
 from hwt.synthesizer.param import evalParam
 from hwt.bitmask import mask
+from hwtLib.amba.constants import RESP_OKAY
 
 
 class Axi3DenseMem(DenseMemory):
-    def __init__(self, clk, axi, parent=None):
-        DW = evalParam(axi.DATA_WIDTH).val
-        self.cellSize = DW // 8
-        self.allMask = mask(self.cellSize)
+    def __init__(self, clk, axi=None, axiAR=None, axiR=None, axiAW=None, axiW=None, axiB=None, parent=None):
 
         self.parent = parent
         if parent is None:
@@ -15,14 +13,72 @@ class Axi3DenseMem(DenseMemory):
         else:
             self.data = parent.data
 
-        self.arAg = axi._ag.ar
-        self.rAg = axi._ag.r
+        if axi is not None:
+            assert axiAR is None
+            assert axiR is None
+            self.arAg = axi._ag.ar
+            self.rAg = axi._ag.r
+            DW = evalParam(axi.DATA_WIDTH).val
+            self.awAg = axi._ag.aw
+            self.wAg = axi._ag.w
+            self.wAckAg = axi._ag.b
+        else:
+            assert axi is None
+            if axiAR is not None:
+                self.arAg = axiAR._ag
+                self.rAg = axiR._ag
+                DW = evalParam(axiR.DATA_WIDTH).val
+            else:
+                assert axiR is None
+                self.arAg = None
+                self.rAg = None
 
+            if axiAW is not None:
+                self.awAg = axiAW._ag
+                self.wAg = axiW._ag
+                self.wAckAg = axiB._ag
+                DW = evalParam(axiW.DATA_WIDTH).val
+            else:
+                assert axiW is None
+                assert axiB is None
+                self.awAg = None
+                self.wAg = None
+                self.wAckAg = None
+
+        self.cellSize = DW // 8
+        self.allMask = mask(self.cellSize)
         self.rPending = []
 
-        self.awAg = axi._ag.aw
-        self.wAg = axi._ag.w
-        self.wAckAg = axi._ag.b
         self.wPending = []
 
         self._registerOnClock(clk)
+
+    def parseReq(self, req):
+        for i, v in enumerate(req):
+            assert v._isFullVld(), (i, v)
+
+        _id = req[0].val
+        addr = req[1].val
+        size = req[4].val + 1
+
+        return (_id, addr, size, self.allMask)
+
+    def doRead(self):
+        _id, addr, size, lastWordBitmask = self.rPending.pop(0)
+
+        baseIndex = addr // self.cellSize
+        if baseIndex * self.cellSize != addr:
+            raise NotImplementedError("unaligned transaction not implemented (0x%x)" % addr)
+
+        for i in range(size):
+            isLast = i == size - 1
+            try:
+                data = self.data[baseIndex + i]
+            except KeyError:
+                data = None
+
+            if data is None:
+                raise AssertionError("Invalid read of uninitialized value on addr 0x%x" % 
+                                     (addr + i * self.cellSize))
+
+            self.rAg.data.append((_id, data, RESP_OKAY, isLast))
