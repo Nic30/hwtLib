c17f44ded105e2a25018e24c6504447e1a12b59b
nic30 <nic30@seznam.cz>
2017-03-07 16:56:02 +0100

    whitespaces

--- hwtLib/amba/axi4_wDatapump.py
+++ hwtLib/amba/axi4_wDatapump.py
@@ -19,20 +19,21 @@ from hwtLib.handshaked.streamNode import streamSync, streamAck
 class WFifoIntf(Handshaked):
     def _config(self):
         self.ID_WIDTH = Param(4)
-        
+
     def _declr(self):
         self.id = VectSignal(self.ID_WIDTH)
         HandshakeSync._declr(self)
 
+
 class BFifoIntf(Handshaked):
     def _config(self):
         pass
-    
+
     def _declr(self):
         self.isLast = Signal()
         HandshakeSync._declr(self)
 
-        
+
 class Axi_wDatapump(Axi_datapumpBase):
     """
     Axi3/4 to axi write datapump,
@@ -48,42 +49,39 @@ class Axi_wDatapump(Axi_datapumpBase):
 
             self.errorWrite = Signal()
             self.driver = AxiWDatapumpIntf()
-            
-           
+
         with self._paramsShared():
             # fifo for id propagation and frame splitting on axi.w channel 
             wf = self.writeInfoFifo = HandshakedFifo(WFifoIntf)
             wf.ID_WIDTH.set(self.ID_WIDTH)
             wf.DEPTH.set(self.MAX_TRANS_OVERLAP)
-            
+
             # fifo for propagation of end of frame from axi.b channel
             bf = self.bInfoFifo = HandshakedFifo(BFifoIntf)
             bf.DEPTH.set(self.MAX_TRANS_OVERLAP)
-            
 
     def axiAwHandler(self, wErrFlag):
         req = self.driver.req
         aw = self.a
-        r = self._reg 
-        
+        r = self._reg
+
         self.axiAddrDefaults()
 
         wInfo = self.writeInfoFifo.dataIn
         if self.useTransSplitting():
             LEN_MAX = mask(aw.len._dtype.bit_length())
-            
+
             lastReqDispatched = r("lastReqDispatched", defVal=1)
             lenDebth = r("lenDebth", req.len._dtype)
             addrBackup = r("addrBackup", req.addr._dtype)
             req_idBackup = r("req_idBackup", req.id._dtype)
             _id = self._sig("id", aw.id._dtype)
-                    
-            
+
             If(lastReqDispatched,
                 _id ** req.id,
                 aw.addr ** req.addr,
                 connect(req.len, aw.len, fit=True),
-                
+
                 req_idBackup ** req.id,
                 addrBackup ** (req.addr + self.getBurstAddrOffset()),
                 lenDebth ** (req.len - (LEN_MAX + 1)),
@@ -96,16 +94,16 @@ class Axi_wDatapump(Axi_datapumpBase):
                 ),
                 streamSync(masters=[req],
                            slaves=[aw, wInfo],
-                           extraConds={aw:[~wErrFlag]}),
+                           extraConds={aw: [~wErrFlag]}),
             ).Else(
                 _id ** req_idBackup,
                 aw.addr ** addrBackup,
                 connect(lenDebth, aw.len, fit=True),
-                
+
                 streamSync(slaves=[aw, wInfo], extraConds={aw:[~wErrFlag]}),
-                
+
                 req.rd ** 0,
-                
+
                 If(wInfo.rd & aw.ready,
                    addrBackup ** (addrBackup + self.getBurstAddrOffset()),
                    lenDebth ** (lenDebth - LEN_MAX),
@@ -116,30 +114,29 @@ class Axi_wDatapump(Axi_datapumpBase):
             )
             aw.id ** _id
             wInfo.id ** _id
-            
+
         else:
             aw.id ** req.id
             wInfo.id ** req.id
             aw.addr ** req.addr
             connect(req.len, aw.len, fit=True)
             streamSync(masters=[req], slaves=[aw, wInfo])
-           
+
     def axiWHandler(self, wErrFlag):
         w = self.w
         wIn = self.driver.w
-        
+
         wInfo = HsBuilder(self, self.writeInfoFifo.dataOut).reg().end
         bInfo = self.bInfoFifo.dataIn
 
-        
         w.id ** wInfo.id
         w.data ** wIn.data
         w.strb ** wIn.strb
-        
+
         if self.useTransSplitting():
             wordCntr = self._reg("wWordCntr", self.a.len._dtype, 0)
             doSplit = wordCntr._eq(self.getAxiLenMax()) | wIn.last
-            
+
             If(streamAck([wInfo, wIn], [bInfo, w]),
                If(wIn.last,
                    wordCntr ** 0
@@ -147,31 +144,30 @@ class Axi_wDatapump(Axi_datapumpBase):
                    wordCntr ** (wordCntr + 1)
                )
             )
-            extraConds = {wInfo:[doSplit],
-                          bInfo:[doSplit],
-                          w:[~wErrFlag]}
+            extraConds = {wInfo: [doSplit],
+                          bInfo: [doSplit],
+                          w: [~wErrFlag]}
             w.last ** (doSplit | wIn.last)
-            
+
         else:
             w.last ** wIn.last
-            extraConds = {wInfo:[wIn.last],
-                          bInfo:[wIn.last],
-                          w:[~wErrFlag]}
-        
+            extraConds = {wInfo: [wIn.last],
+                          bInfo: [wIn.last],
+                          w: [~wErrFlag]}
+
         bInfo.isLast ** wIn.last
         streamSync(masters=[wIn, wInfo],
                    slaves=[bInfo, w],
                    extraConds=extraConds
                    )
-            
-            
+
     def axiBHandler(self):
         wErrFlag = self._reg("wErrFlag", defVal=0)
         b = self.b
         ack = self.driver.ack
-        
-        lastFlags = HsBuilder(self, self.bInfoFifo.dataOut).reg().end
-        
+
+        lastFlags = self.bInfoFifo.dataOut
+
         If(lastFlags.vld & ack.rd & b.valid & (b.resp != RESP_OKAY),
            wErrFlag ** 1
         )
@@ -181,19 +177,18 @@ class Axi_wDatapump(Axi_datapumpBase):
         streamSync(masters=[b, lastFlags],
                    slaves=[ack],
                    extraConds={
-                               ack : [lastFlags.isLast]
+                               ack: [lastFlags.isLast]
                                })
-        
+
         return wErrFlag
-    
+
     def _impl(self):
         propagateClkRstn(self)
-        
+
         wErrFlag = self.axiBHandler()
         self.axiAwHandler(wErrFlag)
         self.axiWHandler(wErrFlag)
-        
-        
+
 if __name__ == "__main__":
     from hwt.synthesizer.shortcuts import toRtl
     u = Axi_wDatapump()
