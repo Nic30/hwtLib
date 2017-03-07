4ebe8566230551d396259347a13ce3246b47f513
nic30 <nic30@seznam.cz>
2017-03-07 12:02:32 +0100

    HsJoinFairShare: selectedOneHot as vldSynced

--- hwtLib/handshaked/joinFair.py
+++ hwtLib/handshaked/joinFair.py
@@ -6,64 +6,67 @@ from hwt.hdlObjects.typeShortcuts import vecT
 from hwtLib.handshaked.join import HandshakedJoin
 from hwt.interfaces.utils import addClkRstn
 from hwt.synthesizer.param import Param, evalParam
-from hwt.interfaces.std import VectSignal
+from hwt.interfaces.std import VectSignal, VldSynced
+
 
 def priorityAck(priorityReg, vldSignals, index):
     priorityOverdrives = []
     vldWithHigherPriority = list(vldSignals[:index])
-    
-    for i, (p, vld) in  enumerate(zip(iterBits(priorityReg), vldSignals)):
+
+    for i, (p, vld) in enumerate(zip(iterBits(priorityReg), vldSignals)):
         if i > index:
             priorityOverdrives.append(p & vld)
-    
+
 
     ack = ~Or(*priorityOverdrives, *vldWithHigherPriority) | priorityReg[index]
-    
+
     return ack
+
 
 class HsJoinFairShare(HandshakedJoin):
     """
     Join input stream to single output stream
     inputs with lower number has higher priority
-    
+
     Priority is changing every clock 
     If prioritized input is not sending valid data, 
     input with lowest index and valid is used
-    
+
     combinational
     """
     def _config(self):
         HandshakedJoin._config(self)
         self.EXPORT_SELECTED = Param(True)
-    
+
     def _declr(self):
         HandshakedJoin._declr(self)
         addClkRstn(self)
         if evalParam(self.EXPORT_SELECTED).val:
-            self.selectedOneHot = VectSignal(self.INPUTS)
-        
+            self.selectedOneHot = VldSynced()
+            self.selectedOneHot._replaceParam("DATA_WIDTH", self.INPUTS)
+
     def _impl(self):
         rd = self.getRd
         vld = self.getVld
         data = self.getData
         dout = self.dataOut
         EXPORT_SELECTED = evalParam(self.EXPORT_SELECTED).val
-        
+
         priority = self._reg("priority", vecT(self.INPUTS), defVal=1)
-        
-        vldSignals = list(map(vld, self.dataIn))  
-        
+
+        vldSignals = list(map(vld, self.dataIn))
+
         isSelected_tmp = []
         for i, din in enumerate(self.dataIn):
             isSelected = self._sig("isSelected_%d" % i)
             isSelected ** priorityAck(priority, vldSignals, i)
             isSelected_tmp.append(isSelected)
-            
+
             rd(din) ** (isSelected & rd(dout))
 
             if EXPORT_SELECTED:
-                self.selectedOneHot[i] ** (isSelected & vld(din))
-            
+                self.selectedOneHot.data[i] ** (isSelected & vld(din))
+
         # data out mux
         outMuxTop = []
         for d in data(dout):
@@ -72,17 +75,18 @@ class HsJoinFairShare(HandshakedJoin):
             dataConnectExpr = []
             for _din, _dout in zip(data(din), data(dout)):
                 dataConnectExpr.extend(_dout ** _din)
-           
+
             outMuxTop = If(vld(din) & isSelected,
                 dataConnectExpr
             ).Else(
                 outMuxTop
             )
-            
+
         priority ** rol(priority, 1)
         vld(dout) ** Or(*vldSignals)
-                
-        
+        if EXPORT_SELECTED:
+            self.selectedOneHot.vld ** (Or(*vldSignals) & rd(dout))
+
 if __name__ == "__main__":
     from hwt.interfaces.std import Handshaked
     from hwt.synthesizer.shortcuts import toRtl
--- hwtLib/handshaked/joinFair_test.py
+++ hwtLib/handshaked/joinFair_test.py
@@ -7,82 +7,104 @@ from hwt.hdlObjects.constants import Time
 from hwt.interfaces.std import Handshaked
 from hwt.simulator.shortcuts import simPrepare
 from hwt.simulator.simTestCase import SimTestCase
-from hwt.simulator.utils import agent_randomize
 from hwtLib.handshaked.joinFair import HsJoinFairShare
 from hwt.simulator.agentConnector import valuesToInts
 
 
-dataFn = lambda d: d._ag.data
+def dataFn(d):
+    return d._ag.data
+
+
 class HsJoinFair_2inputs_TC(SimTestCase):
     def setUp(self):
         u = self.u = HsJoinFairShare(Handshaked)
         self.INPUTS = 2
         u.INPUTS.set(self.INPUTS)
         u.DATA_WIDTH.set(8)
+        u.EXPORT_SELECTED.set(True)
         _, self.model, self.procs = simPrepare(self.u)
-    
+
     def addToAllInputs(self, n):
         u = self.u
         for i, d in enumerate(u.dataIn):
-            d._ag.data = [ _i + (n * i) for _i in range(n)]
+            d._ag.data = [_i + (n * i) for _i in range(n)]
 
         expected = []
         for d in zip(*map(dataFn, u.dataIn)):
             expected.extend(d)
-        
+
         return expected
-            
+
     def test_passdata(self):
         u = self.u
         expected = self.addToAllInputs(6)
-            
+
         self.doSim(self.INPUTS * 6 * 20 * Time.ns)
 
         self.assertValSequenceEqual(u.dataOut._ag.data, expected)
-        
+
         for d in u.dataIn:
             self.assertSequenceEqual([], d._ag.data)
+
+        expSelected = [1 << (i % self.INPUTS) for i in range(self.INPUTS * 6)]
+        self.assertValSequenceEqual(u.selectedOneHot._ag.data,
+                                    expSelected)
 
     def test_passdata_oneHasMore(self):
         u = self.u
 
         expected = self.addToAllInputs(6)
-        
+
         d = [7, 8, 9, 10, 11, 12]
         u.dataIn[self.INPUTS - 1]._ag.data.extend(d)
         expected.extend(d)
-        
+
         self.doSim(self.INPUTS * 6 * 20 * Time.ns)
 
         self.assertValSequenceEqual(u.dataOut._ag.data, expected)
-        
+
         for d in u.dataIn:
             self.assertSequenceEqual([], d._ag.data)
+
+        id0 = 1
+        id1 = (1 << (self.INPUTS - 1))
+        expSelected = [1 << (i % self.INPUTS) for i in range(self.INPUTS * 6)] + 6 * [id1, ]
+        self.assertValSequenceEqual(u.selectedOneHot._ag.data,
+                                    expSelected)
 
     def test_passData_onLowPriority(self):
         u = self.u
         lowPriority = u.dataIn[self.INPUTS - 1]
-        expected = [ _i for _i in range(6)]
-        
-        lowPriority._ag.data.extend(expected) 
+        expected = [_i for _i in range(6)]
+
+        lowPriority._ag.data.extend(expected)
 
         self.doSim(120 * Time.ns)
 
         self.assertValSequenceEqual(u.dataOut._ag.data, expected)
-        
+
+        expSelected = [1 << (self.INPUTS - 1) for _ in range(6)]
+        self.assertValSequenceEqual(u.selectedOneHot._ag.data,
+                                    expSelected)
+
     def test_randomized(self):
         u = self.u
         N = 8
         expected = []
         for i, inp in enumerate(u.dataIn):
-            d = [i*N + i2 + 1 for i2 in range(N)]
-            
+            self.randomize(inp)
+            d = [i * N + i2 + 1 for i2 in range(N)]
+
             inp._ag.data.extend(d)
             expected.extend(d)
-            
-        self.doSim(self.INPUTS * N * 50 * Time.ns)   
-        
-        self.assertEqual(set(valuesToInts(u.dataOut._ag.data)), set(expected)) 
+
+        self.randomize(u.dataOut)
+
+        self.doSim(self.INPUTS * N * 50 * Time.ns)
+
+        self.assertEqual(set(valuesToInts(u.dataOut._ag.data)),
+                         set(expected))
+
 
 class HsJoinFair_3inputs_TC(HsJoinFair_2inputs_TC):
     def setUp(self):
@@ -91,19 +113,11 @@ class HsJoinFair_3inputs_TC(HsJoinFair_2inputs_TC):
         u.INPUTS.set(self.INPUTS)
         u.DATA_WIDTH.set(8)
         _, self.model, self.procs = simPrepare(self.u)
-   
 
-# class HsJoin_randomized_TC(HsJoinTC):
-#    def setUp(self):
-#        super(HsJoin_randomized_TC, self).setUp()
-#        #self.procs.append(agent_randomize(self.u.dataIn[0]._ag))
-#        #self.procs.append(agent_randomize(self.u.dataIn[1]._ag))
-#        self.procs.append(agent_randomize(self.u.dataOut._ag))
-#        
-        
+
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    # suite.addTest(FifoTC('test_normalOp'))
+    # suite.addTest(HsJoinFair_2inputs_TC('test_passdata'))
     suite.addTest(unittest.makeSuite(HsJoinFair_2inputs_TC))
     suite.addTest(unittest.makeSuite(HsJoinFair_3inputs_TC))
     runner = unittest.TextTestRunner(verbosity=3)
