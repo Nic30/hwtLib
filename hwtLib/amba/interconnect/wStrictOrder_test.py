f7cfc54e59c3075b081d2d34819c37a89c723841
nic30 <nic30@seznam.cz>
2017-03-07 12:03:03 +0100

    more strict order interconnect tests

--- hwtLib/amba/interconnect/wStrictOrder_test.py
+++ hwtLib/amba/interconnect/wStrictOrder_test.py
@@ -14,6 +14,7 @@ from hwtLib.amba.interconnect.wStrictOrder import WStrictOrderInterconnect
 
 class WStrictOrderInterconnectTC(SimTestCase):
     def setUp(self):
+        super(WStrictOrderInterconnectTC, self).setUp()
         self.u = WStrictOrderInterconnect()
         self.MAX_TRANS_OVERLAP = 4
         self.u.MAX_TRANS_OVERLAP.set(self.MAX_TRANS_OVERLAP)
@@ -79,55 +80,8 @@ class WStrictOrderInterconnectTC(SimTestCase):
 
     def test_randomized(self):
         u = self.u
-        # m = DenseMemory(self.DATA_WIDTH, u.clk, wDatapumpIntf=u.wDatapump)
-
-        for d in u.drivers:
-            self.randomize(d.req)
-            self.randomize(d.w)
-            self.randomize(d.ack)
-
-        self.randomize(u.wDatapump.req)
-        self.randomize(u.wDatapump.w)
-        self.randomize(u.wDatapump.ack)
-
-        def prepare(driverIndex, addr, size, valBase=1, _id=1):
-            driver = u.drivers[driverIndex]
-            driver.req._ag.data.append((_id, addr, size - 1, 0))
-            expected = []
-            _mask = mask(self.DATA_WIDTH // 8)
-            # index = addr // (self.DATA_WIDTH // 8)
-
-            for i in range(size):
-                v = valBase + i
-                d = (v, _mask, int(i == size - 1))
-
-                driver.w._ag.data.append(d)
-                u.wDatapump.ack._ag.data.append(driverIndex)
-
-                expected.append(d)
-
-            return expected
-
-        def check(expected):
-            driverWData = u.wDatapump.w._ag.data
-            self.assertEqual(len(driverWData), len(expected))
-
-            for d, e in zip(driverWData, expected):
-                self.assertValSequenceEqual(d, e)
-
-        d0 = prepare(0, 0x1000, 3, 99, _id=0) + prepare(0, 0x2000, 1, 100, _id=0) + prepare(0, 0x3000, 16, 101)
-        d1 = prepare(1, 0x4000, 3, 200, _id=1) + prepare(1, 0x5000, 1, 201, _id=1)  # + prepare(1, 0x6000, 16, 202) #+ prepare(1, 0x7000, 16, 203)
-
-        self.doSim(1000 * Time.ns)
-
-        check(d0 + d1)
-        
-    def test_randomized2(self):
-        u = self.u
         m = DenseMemory(self.DATA_WIDTH, u.clk, wDatapumpIntf=u.wDatapump)
-        N = 50
-        _mask = mask(self.DATA_WIDTH // 8)
-         
+
         for d in u.drivers:
             self.randomize(d.req)
             self.randomize(d.w)
@@ -138,29 +92,78 @@ class WStrictOrderInterconnectTC(SimTestCase):
         self.randomize(u.wDatapump.ack)
 
         sectors = []
+
+        def prepare(driverIndex, addr, size, valBase=1, _id=1):
+            driver = u.drivers[driverIndex]
+            driver.req._ag.data.append((_id, addr, size - 1, 0))
+            _mask = mask(self.DATA_WIDTH // 8)
+
+            for i in range(size):
+                d = (valBase + i, _mask, int(i == size - 1))
+
+                driver.w._ag.data.append(d)
+                u.wDatapump.ack._ag.data.append(driverIndex)
+
+            sectors.append((addr, valBase, size))
+
+        prepare(0, 0x1000, 3, 99, _id=0)
+        prepare(0, 0x2000, 1, 100, _id=0)
+        prepare(0, 0x3000, 16, 101)
+        prepare(1, 0x4000, 3, 200, _id=1)
+        prepare(1, 0x5000, 1, 201, _id=1)  # + prepare(1, 0x6000, 16, 202) #+ prepare(1, 0x7000, 16, 203)
+
+        self.doSim(2000 * Time.ns)
+
+        for addr, seed, size in sectors:
+            expected = [seed + i for i in range(size)]
+            self.assertValSequenceEqual(m.getArray(addr, 8, size), expected)
+
+    def test_randomized2(self):
+        u = self.u
+        m = DenseMemory(self.DATA_WIDTH, u.clk, wDatapumpIntf=u.wDatapump)
+        N = 50
+        _mask = mask(self.DATA_WIDTH // 8)
+
+        for d in u.drivers:
+            self.randomize(d.req)
+            self.randomize(d.w)
+            self.randomize(d.ack)
+
+        self.randomize(u.wDatapump.req)
+        self.randomize(u.wDatapump.w)
+        self.randomize(u.wDatapump.ack)
+
+        sectors = []
+        framesCnt = [0 for _ in range(self.DRIVER_CNT)]
         for i in range(N // self.DRIVER_CNT):
             for _id, d in enumerate(u.drivers):
-                size = self._rand.getrandbits(3) + 1 
+                size = self._rand.getrandbits(3) + 1
                 magic = self._rand.getrandbits(16)
                 addr = m.calloc(size, 8, initValues=[None for _ in range(size)])
-                
+
                 d.req._ag.data.append((_id, addr, size - 1, 0))
                 for i in range(size):
                     data = (magic + i, _mask, int(i == size - 1))
                     d.w._ag.data.append(data)
-                
+
                 values = [i + magic for i in range(size)]
-                sectors.append((addr, values))
-        
-        self.doSim(self.DRIVER_CNT * N * 100 * Time.ns)
-        
-        for addr, expected in sectors:
+                sectors.append((_id, addr, values))
+                framesCnt[_id] += 1
+
+        self.doSim(self.DRIVER_CNT * N * 110 * Time.ns)
+
+        for _id, d in enumerate(u.drivers):
+            self.assertEmpty(d.req._ag.data)
+            self.assertEmpty(d.w._ag.data)
+            self.assertEquals(len(u.drivers[_id].ack._ag.data), framesCnt[_id])
+
+        for _id, addr, expected in sectors:
             v = m.getArray(addr, 8, len(expected))
             self.assertSequenceEqual(v, expected)
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    suite.addTest(WStrictOrderInterconnectTC('test_randomized2'))
-    # suite.addTest(unittest.makeSuite(WStrictOrderInterconnectTC))
+    # suite.addTest(WStrictOrderInterconnectTC('test_randomized'))
+    suite.addTest(unittest.makeSuite(WStrictOrderInterconnectTC))
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
