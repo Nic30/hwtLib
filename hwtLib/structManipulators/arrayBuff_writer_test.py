19a6ef21c7c33782b534ecaec30133de33d9df4f
nic30 <nic30@seznam.cz>
2017-03-07 19:10:45 +0100

    new: ArrayBuff_writer_TC.test_fullFill_extraAck

--- hwtLib/structManipulators/arrayBuff_writer_test.py
+++ hwtLib/structManipulators/arrayBuff_writer_test.py
@@ -13,6 +13,7 @@ from hwtLib.structManipulators.arrayBuff_writer import ArrayBuff_writer
 
 class ArrayBuff_writer_TC(SimTestCase):
     def setUp(self):
+        super(ArrayBuff_writer_TC, self).setUp()
         self.u = ArrayBuff_writer()
         self.u.TIMEOUT.set(32)
         self.ID = evalParam(self.u.ID).val
@@ -159,21 +160,22 @@ class ArrayBuff_writer_TC(SimTestCase):
 
         self.assertValEqual(self.u.uploaded._ag.data[-1], N)
 
-    def test_fullFill_randomized2(self):
+    def test_fullFill_extraAck(self):
         u = self.u
         N = 16
-        randomize = self.randomize
+        #randomize = self.randomize
 
         u.baseAddr._ag.dout.append(0x1230)
         u.items._ag.data.extend([1 + i for i in range(N)])
-        u.wDatapump.ack._ag.data.extend([NOP for _ in range(N * 2)] + [self.ID, ])
+        u.wDatapump.ack._ag.data.extend([NOP for _ in range(N * 2 - 1)] + 
+                                        [self.ID + 1, self.ID, self.ID + 1])
 
-        randomize(u.wDatapump.w)
-        randomize(u.items)
-        randomize(u.wDatapump.req)
-        randomize(u.wDatapump.ack)
+        #randomize(u.wDatapump.w)
+        #randomize(u.items)
+        #randomize(u.wDatapump.req)
+        #randomize(u.wDatapump.ack)
 
-        self.doSim(80 * 10 * Time.ns)
+        self.doSim(150 * 10 * Time.ns)
 
         self.assertEqual(len(u.items._ag.data), 0)
 
@@ -181,11 +183,29 @@ class ArrayBuff_writer_TC(SimTestCase):
                                     [(self.ID, 0x1230, N - 1, 0)])
         w = u.wDatapump.w._ag.data
         self.assertEqual(len(w), N)
-        self.assertEqual(len(u.wDatapump.ack._ag.data), 0)
+        self.assertEqual(u.wDatapump.ack._ag.actualData, self.ID + 1)
 
         self.assertValEqual(self.u.uploaded._ag.data[-1], N)
+    
+    def test_fullFill_withoutAck(self):
+        u = self.u
+        N = 16
 
-    def test_fullFill_randomized3(self, N = None):
+        u.baseAddr._ag.dout.append(0x1230)
+        u.items._ag.data.extend([1 + i for i in range(N)])
+
+        self.doSim(150 * 10 * Time.ns)
+
+        self.assertEqual(len(u.items._ag.data), 0)
+
+        self.assertValSequenceEqual(u.wDatapump.req._ag.data,
+                                    [(self.ID, 0x1230, N - 1, 0)])
+        w = u.wDatapump.w._ag.data
+        self.assertEqual(len(w), N)
+
+        self.assertValEqual(self.u.uploaded._ag.data[-1], 0)
+        
+    def test_fullFill_randomized3(self, N=None):
         u = self.u
         BASE = 0x1230
         MAGIC = 1
@@ -200,7 +220,7 @@ class ArrayBuff_writer_TC(SimTestCase):
 
         def enReq(s):
             u.wDatapump.req._ag.enable = False
-            yield s.wait(40 * 10 * Time.ns)
+            yield s.wait(32 * 10 * Time.ns)
             yield from agent_randomize(u.wDatapump.req._ag, 50 * Time.ns, self._rand.getrandbits(64))(s)
 
         self.procs.append(enReq)
@@ -210,7 +230,7 @@ class ArrayBuff_writer_TC(SimTestCase):
         # self.randomize(u.req)
         self.randomize(u.wDatapump.ack)
 
-        self.doSim(N * 500 * Time.ns)
+        self.doSim(N * 100 * Time.ns)
 
         self.assertEmpty(u.items._ag.data)
         d = m.getArray(BASE, self.DATA_WIDTH // 8, self.ITEMS)
@@ -218,14 +238,14 @@ class ArrayBuff_writer_TC(SimTestCase):
         expected = [i + MAGIC if i >= 10 else i + self.ITEMS + MAGIC   for i in range(self.ITEMS)]
 
         self.assertValSequenceEqual(d, expected)
-        self.assertValEqual(self.u.uploaded._ag.data[-1], N)
+        self.assertValEqual(u.uploaded._ag.data[-1], N)
         
-    #def test_fullFill_randomized4(self):
+    # def test_fullFill_randomized4(self):
     #    self.test_fullFill_randomized3(N=3*self.ITEMS)
   
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    suite.addTest(ArrayBuff_writer_TC('test_fullFill_randomized4'))
-    #suite.addTest(unittest.makeSuite(ArrayBuff_writer_TC))
+    suite.addTest(ArrayBuff_writer_TC('test_fullFill_withoutAck'))
+    # suite.addTest(unittest.makeSuite(ArrayBuff_writer_TC))
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
