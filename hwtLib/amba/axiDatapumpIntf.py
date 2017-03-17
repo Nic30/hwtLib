969bfde9d7f15fd661ca6cb0f5c8843490831dd4
nic30 <nic30@seznam.cz>
2017-03-18 00:05:28 +0100

    codestyle pep8

--- hwtLib/amba/axiDatapumpIntf.py
+++ hwtLib/amba/axiDatapumpIntf.py
@@ -8,7 +8,9 @@ from hwt.synthesizer.param import Param
 from hwtLib.amba.axis import AxiStream_withId, AxiStream
 
 
-ag = lambda i: i._getSimAgent()(i) 
+def ag(i):
+    i._getSimAgent()(i)
+
 
 class AddrSizeHs(Handshaked):
     def _config(self):
@@ -16,32 +18,33 @@ class AddrSizeHs(Handshaked):
         self.ADDR_WIDTH = Param(32)
         self.DATA_WIDTH = Param(64)
         self.MAX_LEN = Param(4096 // 8 - 1)
-    
+
     def _declr(self):
         self.id = VectSignal(self.ID_WIDTH)
-        
+
         self.addr = VectSignal(self.ADDR_WIDTH)
         #  len is number of words -1
         self.len = VectSignal(log2ceil(self.MAX_LEN))
-        
+
         # rem is number of bits in last word which is valid - 1
         self.rem = VectSignal(log2ceil(self.DATA_WIDTH // 8))
 
         HandshakeSync._declr(self)
-    
+
     def _getSimAgent(self):
         return AddrSizeHsAgent
+
 
 class AddrSizeHsAgent(HandshakedAgent):
     def doRead(self, s):
         intf = self.intf
         r = s.read
-        
+
         _id = r(intf.id)
         addr = r(intf.addr)
         _len = r(intf.len)
         rem = r(intf.rem)
-        
+
         return (_id, addr, _len, rem)
 
     def mkReq(self, addr, _len, rem=0, _id=0):
@@ -50,17 +53,17 @@ class AddrSizeHsAgent(HandshakedAgent):
     def doWrite(self, s, data):
         intf = self.intf
         w = s.w
-        
+
         if data is None:
             data = [None for _ in range(4)]
 
         _id, addr, _len, rem = data
-        
+
         w(_id, intf.id)
         w(addr, intf.addr)
         w(_len, intf.len)
         w(rem, intf.rem)
-    
+
 
 class AxiRDatapumpIntf(Interface):
     """
@@ -68,55 +71,52 @@ class AxiRDatapumpIntf(Interface):
     """
     def _config(self):
         AddrSizeHs._config(self)
-        
+
     def _declr(self):
         with self._paramsShared():
             # user requests
             self.req = AddrSizeHs()
             self.r = AxiStream_withId(masterDir=DIRECTION.IN)
-    
+
     def _getSimAgent(self):
         return AxiRDatapumpIntfAgent
-    
+
+
 class AxiRDatapumpIntfAgent(AgentBase):
     """
     Composite agent with agent for every AxiRDatapumpIntf channel
     enable is shared
     """
-    
+
     @property
     def enable(self):
         return self.__enable
-    
+
     @enable.setter
     def enable(self, v):
         """
         Distribute change of enable on child agents
         """
         self.__enable = v
-        
+
         for o in [self.req, self.r]:
             o.enable = v
 
     def __init__(self, intf):
         self.__enable = True
         self.intf = intf
-        
-        
+
         self.req = intf.req._ag = ag(intf.req)
         self.r = intf.r._ag = ag(intf.r)
-                
-        
-        
+
     def getDrivers(self):
-        return (self.req.getDrivers() + 
-                self.r.getMonitors() 
+        return (self.req.getDrivers() +
+                self.r.getMonitors()
                 )
-    
-    
+
     def getMonitors(self):
-        return (self.req.getMonitors() + 
-                self.r.getDrivers() 
+        return (self.req.getMonitors() +
+                self.r.getDrivers()
                 )
 
 
@@ -126,57 +126,56 @@ class AxiWDatapumpIntf(Interface):
     """
     def _config(self):
         AddrSizeHs._config(self)
-        
+
     def _declr(self):
         with self._paramsShared():
             # user requests
             self.req = AddrSizeHs()
             self.w = AxiStream()
-        
+
         self.ack = Handshaked(masterDir=DIRECTION.IN)
         self.ack._replaceParam("DATA_WIDTH", self.ID_WIDTH)
-    
+
     def _getSimAgent(self):
         return AxiWDatapumpIntfAgent
-    
+
+
 class AxiWDatapumpIntfAgent(AgentBase):
     """
     Composite agent with agent for every AxiRDatapumpIntf channel
     enable is shared
     """
-    
+
     @property
     def enable(self):
         return self.__enable
-    
+
     @enable.setter
     def enable(self, v):
         """
         Distribute change of enable on child agents
         """
         self.__enable = v
-        
+
         for o in [self.req, self.w, self.ack]:
             o.enable = v
 
     def __init__(self, intf):
         self.__enable = True
         self.intf = intf
-        
-        
+
         self.req = intf.req._ag = ag(intf.req)
         self.w = intf.w._ag = ag(intf.w)
         self.ack = intf.ack._ag = ag(intf.ack)
-                
+
     def getDrivers(self):
-        return (self.req.getDrivers() + 
-                self.w.getDrivers() + 
-                self.ack.getMonitors() 
+        return (self.req.getDrivers() +
+                self.w.getDrivers() +
+                self.ack.getMonitors()
                 )
-    
-    
+
     def getMonitors(self):
-        return (self.req.getMonitors() + 
-                self.w.getMonitors() + 
-                self.ack.getDrivers() 
+        return (self.req.getMonitors() +
+                self.w.getMonitors() +
+                self.ack.getDrivers()
                 )
--- hwtLib/mem/cam.py
+++ hwtLib/mem/cam.py
@@ -15,26 +15,27 @@ from hwt.synthesizer.param import Param, evalParam
 class AddrDataHsAgent(HandshakedAgent):
     def doWrite(self, s, data):
         i = self.intf
-        w = s.write 
+        w = s.write
         if data is None:
             addr, d, mask = None, None, None
-        else: 
+        else:
             addr, d, mask = data
-        
+
         w(addr, i.addr)
         w(d, i.data)
         w(mask, i.mask)
-        
+
     def doRead(self, s):
         i = self.intf
         r = s.read
         return r(i.addr), r(i.data), r(i.mask)
-        
+
+
 class AddrDataHs(Handshaked):
     def _config(self):
         Handshaked._config(self)
         self.ADDR_WIDTH = Param(4)
-        
+
     def _declr(self):
         Handshaked._declr(self)
         self.addr = Signal(dtype=vecT(self.ADDR_WIDTH))
@@ -47,17 +48,17 @@ class AddrDataHs(Handshaked):
 class Cam(Unit):
     """
     Content addressable memory
-    
+
     Simple combinational version
 
     MATCH_LATENCY = 1
     """
     _serializerMode = SERI_MODE.PARAMS_UNIQ
-    
+
     def _config(self):
         self.DATA_WIDTH = Param(36)
         self.ITEMS = Param(16)
-        
+
     def _declr(self):
         addClkRstn(self)
         with self._paramsShared():
@@ -66,40 +67,40 @@ class Cam(Unit):
             self.write.ADDR_WIDTH.set(log2ceil(self.ITEMS - 1))
         self.out = VldSynced()
         self.out._replaceParam("DATA_WIDTH", self.ITEMS)
-    
-    
+
     def writeHandler(self, mem):
         w = self.write
         w.rd ** 1
-        
+
         If(self.clk._onRisingEdge() & w.vld,
            mem[w.addr] ** Concat(w.data, w.mask[0])
         )
-        
+
     def matchHandler(self, mem):
         key = self.match
-        
+
         out = self._reg("out_reg", self.out.data._dtype, defVal=0)
         outNext = out.next
         outVld = self._reg("out_vld_reg", defVal=0)
-        
+
         key.rd ** 1
         outVld ** key.vld
-        
+
         for i in range(evalParam(self.ITEMS).val):
-            outNext[i] ** mem[i]._eq(Concat(key.data , hBit(1)))
-        
+            outNext[i] ** mem[i]._eq(Concat(key.data, hBit(1)))
+
         self.out.data ** out
         self.out.vld ** outVld 
-        
-    
+
     def _impl(self):
         # +1 bit to validity check
-        self._mem = self._sig("cam_mem", Array(vecT(self.DATA_WIDTH + 1), self.ITEMS),
-                                         [0 for _ in range(evalParam(self.ITEMS).val)]) 
+        self._mem = self._sig("cam_mem", Array(vecT(self.DATA_WIDTH + 1), 
+                                               self.ITEMS),
+                                               [0 for _ in range(evalParam(self.ITEMS).val)])
         self.writeHandler(self._mem)
         self.matchHandler(self._mem)
-        
+
+
 if __name__ == "__main__":
     from hwt.synthesizer.shortcuts import toRtl
     u = Cam()
