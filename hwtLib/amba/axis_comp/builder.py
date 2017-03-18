d3ee35696c8587ea4201b62d207c3424f5a6857a
nic30 <nic30@seznam.cz>
2017-03-18 22:17:20 +0100

    rm deprecated axi stream components

--- hwtLib/amba/axis_comp/builder.py
+++ hwtLib/amba/axis_comp/builder.py
@@ -3,19 +3,37 @@ from hwtLib.amba.axis_comp.fifo import AxiSFifo
 from hwtLib.amba.axis_comp.fork import AxiSFork
 from hwtLib.amba.axis_comp.mux import AxiSMux
 from hwtLib.amba.axis_comp.reg import AxiSReg
+from hwt.code import If
 
 
 class AxiSBuilder(AbstractStreamBuilder):
     """
-    Helper class which simplifies building of large stream paths 
+    Helper class which simplifies building of large stream paths
     """
     FifoCls = AxiSFifo
     ForkCls = AxiSFork
-    RegCls  = AxiSReg
-    MuxCls  = AxiSMux
-    
+    RegCls = AxiSReg
+    MuxCls = AxiSMux
+
     def resize(self, newDataWidth):
+        """
+        Change datawidth of axi stream
+        """
         from hwtLib.amba.axis_comp.resizer import AxiS_resizer
-        return self._genericInstance(AxiS_resizer, 
+        return self._genericInstance(AxiS_resizer,
                                      "resize",
                                      lambda u: u.OUT_DATA_WIDTH.set(newDataWidth))
+
+    def startOfFrame(self):
+        """
+        generate start of frame signal, high when we expect new frame to start
+        """
+        lastseen = self.parent._reg(self.name + "_sof_lastseen", defVal=1)
+        intf = self.end
+
+        ack = intf.valid & intf.ready
+        If(ack,
+           lastseen ** intf.last
+        )
+
+        return lastseen
--- hwtLib/amba/axis_comp/frameLinkConv.py
+++ hwtLib/amba/axis_comp/frameLinkConv.py
@@ -8,7 +8,6 @@ from hwt.interfaces.utils import addClkRstn, propagateClkRstn
 from hwt.synthesizer.interfaceLevel.unit import Unit
 from hwt.synthesizer.param import Param, evalParam
 from hwtLib.amba.axis import AxiStream_withUserAndStrb
-from hwtLib.amba.axis_comp.sof import AxiSsof
 from hwtLib.interfaces.frameLink import FrameLink
 
 
@@ -18,6 +17,7 @@ def strbToRem(strbBits, remBits):
         rem = vec(i, remBits)
         yield strb, rem
 
+
 class AxiSToFrameLink(Unit):
     """
     Axi 4 stream to FrameLink 
@@ -26,33 +26,33 @@ class AxiSToFrameLink(Unit):
     def _config(self):
         self.DATA_WIDTH = Param(32)
         self.USER_WIDTH = Param(2)
-        
+
     def _declr(self):
         with self._paramsShared():
             addClkRstn(self)
             self.dataIn = AxiStream_withUserAndStrb()
             self.dataOut = FrameLink()
-        
-        self.sofDetect = AxiSsof()
-        
+
     def _impl(self):
         assert(evalParam(self.USER_WIDTH).val == 2)  # this is how is protocol specified
         In = self.dataIn
         Out = self.dataOut
-        
+
         propagateClkRstn(self)
-        sof = self.sofDetect.sof
-        
+        lastSeenLast = self._reg("lastSeenLast", defVal=1)
+        sof = lastSeenLast
+
         Out.data ** In.data
         Out.src_rdy_n ** ~In.valid
-        
+
         outRd = ~Out.dst_rdy_n
-        self.sofDetect.listenOn(In, outRd)
+        If(In.valid & outRd,
+           lastSeenLast ** In.last
+        )
         In.ready ** outRd
-        
+
         Out.eof_n ** ~In.last
-        
-        
+
         # AXI_USER(0) -> FL_SOP_N
         # Always set FL_SOP_N when FL_SOF_N - added for compatibility with xilinx 
         # axi components. Otherwise FL_SOP_N would never been set and FrameLink
@@ -63,7 +63,7 @@ class AxiSToFrameLink(Unit):
         ).Else(
            Out.sop_n ** ~sop
         )
-        
+
         # AXI_USER(1) -> FL_EOP_N
         # Always set FL_EOP_N when FL_EOF_N - added for compatibility with xilinx 
         # axi components. Otherwise FL_EOP_N would never been set and FrameLink
@@ -78,22 +78,21 @@ class AxiSToFrameLink(Unit):
         remMap = []
         remBits = Out.rem._dtype.bit_length()
         strbBits = In.strb._dtype.bit_length()
-        
+
         for strb, rem in strbToRem(strbBits, remBits):
             remMap.append((strb, Out.rem ** rem))
-        
+
         end_of_part_or_transaction = In.last | eop
-        
-        
+
         If(end_of_part_or_transaction,
             Switch(In.strb)\
             .addCases(remMap)
         ).Else(
             Out.rem ** vec(-1, remBits)
         )
-        
+
         Out.sof_n ** ~sof
-        
+
 
 class FrameLinkToAxiS(Unit):
     """
@@ -101,29 +100,28 @@ class FrameLinkToAxiS(Unit):
     """
     def _config(self):
         AxiSToFrameLink._config(self)
-    
+
     def _declr(self):
         with self._paramsShared():
             addClkRstn(self)
             self.dataIn = FrameLink()
             self.dataOut = AxiStream_withUserAndStrb()
-    
+
     def _impl(self):
         In = self.dataIn
         Out = self.dataOut
-        
+
         sop = self._sig("sop")
         eop = self._sig("eop")
-        
-        
+
         Out.data ** In.data 
         Out.valid ** ~In.src_rdy_n
-        In.dst_rdy_n ** ~Out.ready 
-        
+        In.dst_rdy_n ** ~Out.ready
+
         Out.last ** ~In.eof_n
-        eop ** ~In.eop_n 
+        eop ** ~In.eop_n
         sop ** ~In.sop_n
-        
+
         Out.user ** Concat(eop, sop)
 
         strbMap = []
@@ -133,7 +131,8 @@ class FrameLinkToAxiS(Unit):
             strbMap.append((rem, Out.strb ** strb))
         Switch(In.rem).addCases(strbMap)
 
+
 if __name__ == "__main__":
     from hwt.synthesizer.shortcuts import toRtl
-    u = FrameLinkToAxiS()
+    u = AxiSToFrameLink()
     print(toRtl(u))
