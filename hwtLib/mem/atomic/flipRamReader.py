c838f0bd8a399761630fbfe42bc0ed57d47d4372
nic30 <nic30@seznam.cz>
2017-04-21 22:09:41 +0200

    codestyle, tests update

--- hwtLib/mem/atomic/flipRamReader.py
+++ hwtLib/mem/atomic/flipRamReader.py
@@ -8,14 +8,13 @@ from hwtLib.handshaked.ramReader import HsRamPortReader
 class HsFlipRamReader(HsRamPortReader):
     """
     Same as HsBramPortReader but has one BRAM port more.
-    Every data which is readed, is  overwritten with 0.
+    Every data which is read, is  overwritten with 0.
     """
     def _declr(self):
         super()._declr()
         with self._paramsShared():
             self.dataWriteBack = BramPort_withoutClk()
-                
-    
+
     def cleanAfterRead(self):
         st = self.st
         st_t = st._dtype
@@ -23,13 +22,14 @@ class HsFlipRamReader(HsRamPortReader):
         wb.we ** (self.data_flag & st._eq(st_t.sendingData))
         wb.din ** 0
         wb.en ** 1
-        wb.addr ** self.addr 
-                
+        wb.addr ** self.addr
+
     def _impl(self):
         HsRamPortReader._impl(self)
         self.cleanAfterRead()
-        
+
+
 if __name__ == "__main__":
     from hwt.synthesizer.shortcuts import toRtl
     u = HsFlipRamReader()
-    print(toRtl(u))        
+    print(toRtl(u))
--- hwtLib/mem/atomic/flipReg_test.py
+++ hwtLib/mem/atomic/flipReg_test.py
@@ -5,34 +5,29 @@ import unittest
 
 from hwt.hdlObjects.constants import Time
 from hwt.simulator.agentConnector import valuesToInts
-from hwt.simulator.shortcuts import simUnitVcd, simPrepare
+from hwt.simulator.simTestCase import SimTestCase
 from hwtLib.mem.atomic.flipReg import FlipRegister
 
 
-class FlipRegTC(unittest.TestCase):
+class FlipRegTC(SimTestCase):
     def setUp(self):
-        self.u, self.model, self.procs = simPrepare(FlipRegister())
-        
-    def runSim(self, name, time=90 * Time.ns):
-        simUnitVcd(self.model, self.procs,
-                "tmp/flipReg_%s.vcd" % name,
-                time=90 * Time.ns)
-    
+        self.u = FlipRegister()
+        self.prepareUnit(self.u)
+
     def test_simpleWriteAndSwitch(self):
         u = self.u
-        
+
         # u.select_sig._ag.initDelay = 6 * Time.ns
         u.select_sig._ag.data = [0, 0, 0, 0, 1, 0]
         u.first._ag.dout = [1]
         u.second._ag.dout = [2]
-        
-        self.runSim("simpleWriteAndSwitch")
+
+        self.doSim(90 * Time.ns)
 
         self.assertSequenceEqual([0, 0, 1, 1, 2, 1, 1, 1, 1], valuesToInts(u.first._ag.din))
         self.assertSequenceEqual([0, 0, 2, 2, 1, 2, 2, 2, 2], valuesToInts(u.second._ag.din))
 
-    
-    
+
 if __name__ == "__main__":
     suite = unittest.TestSuite()
     # suite.addTest(TwoCntrsTC('test_withStops'))
