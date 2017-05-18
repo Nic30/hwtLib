39a000894260c1a5a14c0ef4d359d2954d524cd9
nic30 <nic30@seznam.cz>
2017-05-18 15:44:55 +0200

    implement BramPortSimMemSpaceMaster._read

--- hwtLib/mem/bramPortSimMemSpaceMaster.py
+++ hwtLib/mem/bramPortSimMemSpaceMaster.py
@@ -1,4 +1,4 @@
-from hwt.hdlObjects.constants import WRITE
+from hwt.hdlObjects.constants import WRITE, READ
 from hwtLib.sim.abstractMemSpaceMaster import AbstractMemSpaceMaster
 
 
@@ -10,5 +10,12 @@ class BramPortSimMemSpaceMaster(AbstractMemSpaceMaster):
         w = self._bus._ag.requests
         # (request type, address, [write data])
         w.append((WRITE, addr, data))
+        if thenFn:
+            raise NotImplementedError()
+
+    def _read(self, addr, size, thenFn):
+        r = self._bus._ag.requests
+        # (request type, address, [write data])
+        r.append((READ, addr))
         if thenFn:
             raise NotImplementedError()
