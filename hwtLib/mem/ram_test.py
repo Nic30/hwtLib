81ee76deb22eb77a54e24d6a7d54372bc42b7ecb
nic30 <nic30@seznam.cz>
2017-05-19 12:02:46 +0200

    RamTC rewrite deprecated

--- hwtLib/mem/ram_test.py
+++ hwtLib/mem/ram_test.py
@@ -5,27 +5,26 @@ import unittest
 
 from hwt.hdlObjects.constants import WRITE, READ, Time
 from hwt.simulator.agentConnector import valuesToInts
-from hwt.simulator.shortcuts import simUnitVcd, simPrepare
+from hwt.simulator.simTestCase import SimTestCase
 from hwtLib.mem.ram import Ram_sp
 
 
-class RamTC(unittest.TestCase):
+class RamTC(SimTestCase):
 
     def test_writeAndRead(self):
         u = Ram_sp()
         u.DATA_WIDTH.set(8)
         u.ADDR_WIDTH.set(3)
-        u, model, procs = simPrepare(u)
+        self.prepareUnit(u)
 
         u.a._ag.requests = [(WRITE, 0, 5), (WRITE, 1, 7),
                             (READ, 0), (READ, 1),
                             (READ, 0), (READ, 1), (READ, 2)] 
         
         
-        simUnitVcd(model, procs,
-                   "tmp/ram_writeAndRead.vcd", time=110 * Time.ns)
+        self.doSim(110 * Time.ns)
         self.assertSequenceEqual([5, 7, None, None, None, None, None, None],
-                                  valuesToInts(model.ram_memory._val.val))
+                                  valuesToInts(self.model.ram_memory._val.val))
         self.assertSequenceEqual([5, 7, 5, 7, None],
                                  valuesToInts(u.a._ag.readed))
 
