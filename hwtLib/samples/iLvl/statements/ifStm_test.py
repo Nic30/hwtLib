bc2eb80bcadd7a53247fbe8584eeb1b622d76fd7
nic30 <nic30@seznam.cz>
2017-05-19 15:19:03 +0200

    tests for ifStm examples #1

--- hwtLib/samples/iLvl/statements/ifStm_test.py
+++ hwtLib/samples/iLvl/statements/ifStm_test.py
@@ -3,17 +3,16 @@
 
 from hwt.hdlObjects.constants import Time
 from hwt.simulator.agentConnector import agInts
-from hwtLib.samples.iLvl.statements.ifStm import SimpleIfStatement
+from hwtLib.samples.iLvl.statements.ifStm import SimpleIfStatement,\
+    SimpleIfStatement2, SimpleIfStatement2b, SimpleIfStatement2c
 from hwt.simulator.simTestCase import SimTestCase
 
 
 class IfStmTC(SimTestCase):
-    def setUp(self):
-        self.u = SimpleIfStatement()
-        self.prepareUnit(self.u)
 
-    def test_allCases(self):
-        u = self.u
+    def test_SimpleIfStatement(self):
+        u = SimpleIfStatement()
+        self.prepareUnit(u)
 
         u.a._ag.data = [1, 1, 1, 0, 0, 0, 0, 0]
         u.b._ag.data = [0, 1, None, 0, 1, None, 1, 0]
@@ -23,6 +22,71 @@ class IfStmTC(SimTestCase):
 
         self.assertSequenceEqual([0, 1, None, 0, 1, None, 0, 0], agInts(u.d))
 
+    def test_SimpleIfStatement2(self):
+        u = SimpleIfStatement2()
+        self.prepareUnit(u)
+
+        #If(a,
+        #    If(b & c,
+        #       r ** 1,
+        #    ).Else(
+        #       r ** 0
+        #    )
+        #)
+        #d ** r
+
+        u.a._ag.data = [1, 1, 1,    0, 0, 0,    1, 0, 1, 0]
+        u.b._ag.data = [0, 1, None, 0, 1, None, 1, 0, 0, 0]
+        u.c._ag.data = [0, 0, 0,    0, 1, 0,    1, 0, 1, 0]
+        expected_dd  = [0, 0, 0,    0, 0, 0,    0, 1, 1, 0]
+
+        self.doSim(100 * Time.ns)
+
+        self.assertValSequenceEqual(u.d._ag.data, expected_dd)
+
+    def test_SimpleIfStatement2b(self):
+        u = SimpleIfStatement2b()
+        self.prepareUnit(u)
+        #If(a & b,
+        #    If(c,
+        #       r ** 1,
+        #    )
+        #).Elif(c,
+        #    r ** 0
+        #)
+        #d ** r
+
+        u.a._ag.data = [1, 1, 1,    0, 0, 0,    1, 0, 1, 0]
+        u.b._ag.data = [0, 1, None, 0, 1, None, 1, 0, 0, 0]
+        u.c._ag.data = [0, 0, 0,    0, 1, 0,    1, 0, 1, 0]
+        expected_dd  = [0, 0, 0,    0, 0, 0,    0, 1, 1, 0]
+
+        self.doSim(100 * Time.ns)
+
+        self.assertValSequenceEqual(u.d._ag.data, expected_dd)
+
+    def test_SimpleIfStatement2c(self):
+        u = SimpleIfStatement2c()
+        self.prepareUnit(u)
+        #If(a & b,
+        #    If(c,
+        #       r ** 0,
+        #    )
+        #).Elif(c,
+        #    r ** 1
+        #).Else(
+        #    r ** 2
+        #)
+        #d ** r
+
+        u.a._ag.data = [0, 1, 1, 1,    0,    0, 0,    1, 0, 1, 0]
+        u.b._ag.data = [0, 0, 1, None, 0,    1, None, 1, 0, 0, 0]
+        u.c._ag.data = [1, 0, 0, 0,    0,    1, 0,    1, 0, 1, 0]
+        expected_dd  = [0, 0, 2, 2,    None, 2, 1,    2, 0, 2]
+
+        self.doSim(100 * Time.ns)
+
+        self.assertValSequenceEqual(u.d._ag.data, expected_dd)
 
 if __name__ == "__main__":
     import unittest
