c7b1d29eb6f50d2d02ee437599a55e36e8ffde03
nic30 <nic30@seznam.cz>
2017-05-21 20:29:54 +0200

    AxiSBuilder add forge as TODO

--- hwtLib/amba/axis_comp/builder.py
+++ hwtLib/amba/axis_comp/builder.py
@@ -1,10 +1,12 @@
 from hwt.code import If
+from hwt.hdlObjects.types.struct import HStruct
 from hwtLib.abstract.streamBuilder import AbstractStreamBuilder
 from hwtLib.amba.axis_comp.append import AxiS_append
 from hwtLib.amba.axis_comp.fifo import AxiSFifo
 from hwtLib.amba.axis_comp.fork import AxiSFork
 from hwtLib.amba.axis_comp.mux import AxiSMux
 from hwtLib.amba.axis_comp.reg import AxiSReg
+from hwtLib.amba.axis_comp.frameForge import AxiS_frameForge
 
 
 class AxiSBuilder(AbstractStreamBuilder):
@@ -67,8 +69,45 @@ class AxiSBuilder(AbstractStreamBuilder):
     def extend(self, listOfAxis):
         """
         For each axi stream from "listOfAxis" append frame behind frame from actual "end"
+        
+        :attention: frames are not merged they are just appended
+            to merge frames use "forge"
         """
         for axis in listOfAxis:
             self.append(axis)
 
         return self
+    
+    #[TODO]
+    #@classmethod
+    #def forge(cls, parent, parts, intfCls, setupFn=None, once=False, name=None):
+    #    """
+    #    generate frame from parts
+    #    
+    #    :param parent: unit where generated units should be instantiated
+    #    :param parts: list of parts (signals, interfaces, values etc)
+    #    :param intfCls: class for output interface
+    #    :param setupFn: setup function for output interface
+    #    :param once: specifies if frame generation should be repeated or not
+    #    :param name: name prefix for generated units
+    #    """
+    #    
+    #    
+    #    template = HStruct(
+    #                    )
+    #
+    #    
+    #    u = AxiS_frameForge(intfCls,
+    #                        template
+    #                        )
+    #    if setupFn:
+    #        setupFn(u)
+    #    
+    #    self = AxiSBuilder(parent, u.dataOut, name)
+    #    setattr(parent, self._findSuitableName("append"), u)
+    #    
+    #    self.lastComp = u
+    #    self.end = u.dataOut
+        
+        
+        
