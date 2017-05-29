068a684b91256616b0d4e546209a11c7897a2d93
nic30 <nic30@seznam.cz>
2017-05-29 22:47:46 +0200

    codestyle: eth types with capital at the begin

--- hwtLib/types/net/eth.py
+++ hwtLib/types/net/eth.py
@@ -6,12 +6,12 @@ mac_t = vecT(6 * 8)
 
 
 syncword = 0b1010101010101010101010101010101010101010101010101010101010101011
-ethPreambule = HStruct(
+EthPreambule = HStruct(
     (vecT(7 * 8), "preambule"),
     (vecT(8), "startOfFrameDelimiter")
     )
 
-eth2Header = HStruct(
+Eth2Header = HStruct(
     (mac_t, "dst"),
     (mac_t, "src"),
     (vecT(2 * 8), "type")
