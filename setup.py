d1dad0b17f4a52d549ed0c949c2846ec7db58532
nic30 <nic30@seznam.cz>
2017-06-20 15:21:40 +0200

    specify min version of hwt in setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='hwtLib',
       author='Michal Orsak',
       author_email='michal.o.socials@gmail.com',
       install_requires=[
-        'hwt',
+        'hwt>=1.6',
         'Pillow',  # there are some components which are working with images
       ],
       license='MIT',
