6e02644778d3745dce623aa49b31080fb7b8e329
nic30 <nic30@seznam.cz>
2017-06-22 02:01:16 +0200

    rm deprecated AddrSpaceItem

--- hwtLib/abstract/discoverAddressSpace.py
+++ hwtLib/abstract/discoverAddressSpace.py
@@ -1,6 +1,3 @@
-import pprint
-
-from hwt.bitmask import mask
 from hwt.hdlObjects.assignment import Assignment
 from hwt.hdlObjects.operator import Operator
 from hwt.hdlObjects.operatorDefs import AllOps
@@ -41,75 +38,6 @@ def getParentUnit(sig):
     while not isinstance(intf._parent, UnitBase):
         intf = intf._parent
     return intf._parent
-
-
-class AddrSpaceItem(object):
-    """
-    Container of informations about space in memory
-    :ivar port: port for this item on converter
-    :ivar children: nested addr space items (dict addr:addrspaceitem)
-    """
-    def __init__(self, addr, name, size=1, origin=None, alignOffsetBits=None):
-        """
-        :param addr: base addr for this addr item
-        :param name: name of this addr item, (port with same name will be created for this item)
-        :param size: used for memories, number of items in memory
-        :param origin: object from which this was generated usually HStructField
-        """
-        self.port = None
-        self.children = {}
-
-        self.name = name
-        self.addr = addr
-        self.size = size
-        self.origin = origin
-
-    def assertNoOverlap(self, nextItem):
-        left0 = self.addr + self.size
-        right1 = nextItem.addr
-        assert left0 <= right1, (self, nextItem)
-
-    @staticmethod
-    def checkOverlapping(addrSpace):
-        """
-        :param addrSpace: sorted list of AddrSpaceItems
-        """
-        last = None
-        for item in addrSpace:
-            if last is None:
-                pass
-            else:
-                last.assertNoOverlap(item)
-
-            last = item
-
-    def getMyAddrPrefix(self):
-        """
-        :return: None if base addr is not aligned to size and prefix can not be used
-                 tuple (prefix, subAddrBits) if can be mapped by prefix
-        """
-        if self.size == 1:
-            return self.addr, 0
-        else:
-            subAddrBits = (self.size - 1).bit_length()
-
-        if self.addr & mask(subAddrBits):
-            # is addr is not aligned to size
-            return None
-        return self.addr >> subAddrBits, subAddrBits
-
-    def __repr__(self):
-        if self.children:
-            children = ",\n" + pprint.pformat(self.children, 2) + "\n "
-        else:
-            children = ""
-
-        if self.size is None:
-            size = 1
-        else:
-            size = self.size
-
-        return "<AddrSpaceItem %s, %d, size=%d%s>" % (self.name, self.addr, size, children)
 
 
 class AddressSpaceProbe(object):
