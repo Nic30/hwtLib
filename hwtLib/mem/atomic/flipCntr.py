48e0d1b6ea46b6e54daeb8d185870c4a899566aa
nic30 <nic30@seznam.cz>
2017-07-23 13:51:16 +0200

    codestyle

--- hwtLib/mem/atomic/flipCntr.py
+++ hwtLib/mem/atomic/flipCntr.py
@@ -14,12 +14,12 @@ from hwtLib.mem.atomic.flipReg import FlipRegister
 class FlipCntr(Unit):
     """
     Counter with FlipRegister which is form memory with atomic access
-    
+
     interface doFilip drives switching of memories in flip register
     dataIn has higher priority than doIncr
     """
     _serializerMode = SERI_MODE.ONCE
-    
+
     def _config(self):
         self.DATA_WIDTH = Param(18)
 
@@ -30,30 +30,29 @@ class FlipCntr(Unit):
             self.doFlip = HandshakeSync()
             self.data = RegCntrl()
             self.cntr = FlipRegister()
-    
+
     def flipHandler(self):
         self.doFlip.rd ** 1
-        
+
         flipSt = self._reg("flipState", defVal=0)
         If(self.doFlip.vld,
             flipSt ** ~flipSt
         )
         self.cntr.select_sig ** flipSt
-        
-    
+
     def dataHanldler(self):
         cntr = self.cntr
         cntr.first.dout.data ** (cntr.first.din + 1)
         cntr.first.dout.vld ** self.doIncr
-        
+
         cntr.second ** self.data
-        
-    
+
     def _impl(self):
         propagateClkRstn(self)
         self.flipHandler()
         self.dataHanldler()
-        
+
+
 if __name__ == "__main__":
     from hwt.synthesizer.shortcuts import toRtl
     print(toRtl(FlipCntr()))
