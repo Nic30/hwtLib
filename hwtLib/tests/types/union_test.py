5516a8c2b9d75664569704e00203addc13089307
nic30 <nic30@seznam.cz>
2017-08-17 20:08:58 +0200

    tests for union value

--- hwtLib/tests/types/union_test.py
+++ hwtLib/tests/types/union_test.py
@@ -3,7 +3,9 @@
 
 import unittest
 from hwt.hdlObjects.types.union import HUnion
-from hwtLib.types.ctypes import uint8_t, uint16_t
+from hwtLib.types.ctypes import uint8_t, uint16_t, int8_t
+from hwt.bitmask import mask
+from hwt.hdlObjects.types.struct import HStruct
 
 
 class UnionTC(unittest.TestCase):
@@ -28,6 +30,44 @@ class UnionTC(unittest.TestCase):
                 (uint8_t, None),
                 (uint8_t, "b"),
             )
+
+    def test_value_simple(self):
+        t = HUnion(
+                (uint8_t, "unsigned"),
+                (int8_t, "signed"),
+            )
+        v = t.fromPy(None)
+        
+        v.unsigned = mask(8)
+        self.assertEqual(int(v.signed), -1)
+        
+        v.signed = 0
+        self.assertEqual(int(v.unsigned), 0)
+
+    def test_value_struct_and_bits(self):
+        t = HUnion(
+                (uint16_t, "bits"),
+                (HStruct(
+                  (uint8_t, "lower"),
+                  (uint8_t, "upper"),
+                 ), "struct"),
+            )
+
+        v = t.fromPy(None)
+        
+        v.struct.upper = 1
+        self.assertEqual(v.bits.val, 1 << 8)
+        self.assertEqual(v.bits.vldMask, mask(8) << 8)
+        
+        v.struct.lower = 1
+        self.assertEqual(v.bits.val, (1 << 8) | 1)
+        self.assertEqual(v.bits.vldMask, mask(16))
+        
+        v.bits = 2
+
+        self.assertEqual(int(v.struct.lower), 2)
+        self.assertEqual(int(v.struct.upper), 0)
+
 
 
 if __name__ == '__main__':
