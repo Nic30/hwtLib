87aea205953e5626e2d990d7d3094d4e3907351d
Nic30 <nic30@seznam.cz>
2018-03-15 10:13:01 +0100

    update api of sim mem handlers

--- hwtLib/ipif/simMaster.py
+++ hwtLib/ipif/simMaster.py
@@ -6,12 +6,18 @@ class IPFISimMaster(AbstractMemSpaceMaster):
     """
     Simulation address space master for IPIF interface
     """
-    def _write(self, addr, size, data, mask):
+    def _write(self, addr, size, data, mask, onDone=None):
+        if onDone:
+            raise NotImplementedError()
+
         w = self._bus._ag.requests
         # (request type, address, [write data])
         w.append((WRITE, addr, data, mask))
+        
+    def _read(self, addr, size, onDone=None):
+        if onDone:
+            raise NotImplementedError()
 
-    def _read(self, addr, size):
         r = self._bus._ag.requests
         # (request type, address, [write data])
         r.append((READ, addr))
