c442c2cc6557a068587c9c97b7f6203e416595c9
Nic30 <nic30@seznam.cz>
2018-09-30 13:39:11 +0200

    add simple ip core packaging example

---
+++ hwtLib/examples/simple_ip.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from hwt.interfaces.std import Signal
+from hwt.synthesizer.unit import Unit
+
+
+class SimpleUnit(Unit):
+    def _declr(self):
+        self.a = Signal()
+        self.b = Signal()._m()
+
+    def _impl(self):
+        self.b(self.a)
+
+
+if __name__ == "__main__":  # alias python main function
+    from hwt.serializer.vhdl.serializer import VhdlSerializer
+    from hwt.serializer.ip_packager.packager import IpPackager
+    from os.path import expanduser
+
+    # create instance of Unit (unit is like verilog module)
+    u = SimpleUnit()
+    # create instace of IpPackager and configure it
+    p = IpPackager(u, serializer=VhdlSerializer)
+    # generate IP-core package
+    p.createPackage(expanduser("~/ip_repo"))
