69598e33aecd8ec2d95002071b25affe1ea74f6b
Nic30 <nic30@seznam.cz>
2018-12-12 00:04:49 +0100

    ClkSynchronizer: add missing clk association

--- hwtLib/clocking/clkSynchronizer.py
+++ hwtLib/clocking/clkSynchronizer.py
@@ -24,11 +24,13 @@ class ClkSynchronizer(Unit):
     def _declr(self):
         self.rst = Rst()
 
-        self.inData = Signal(dtype=self.DATA_TYP)
         self.inClk = Clk()
+        with self._associated(clk=self.inClk):
+            self.inData = Signal(dtype=self.DATA_TYP)
 
-        self.outData = Signal(dtype=self.DATA_TYP)._m()
         self.outClk = Clk()
+        with self._associated(clk=self.outClk):
+            self.outData = Signal(dtype=self.DATA_TYP)._m()
 
     def _impl(self):
         def reg(name, clk):
