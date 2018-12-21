cd3427d463190376ccbfca71e746f2f987e8288e
Nic30 <nic30@seznam.cz>
2018-12-21 01:46:12 +0100

    codestyle

--- hwtLib/examples/hierarchy/hierarchySerialization_test.py
+++ hwtLib/examples/hierarchy/hierarchySerialization_test.py
@@ -13,7 +13,7 @@ from hwtLib.examples.hierarchy.netFilter import NetFilter
 from hwtLib.tests.statementTrees import StatementTreesTC
 
 
-def readContent(file_name:str):
+def readContent(file_name: str):
     THIS_DIR = os.path.dirname(os.path.realpath(__file__))
     with open(os.path.join(THIS_DIR, file_name)) as f:
         return f.read()
