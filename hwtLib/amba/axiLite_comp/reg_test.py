aed73911e4e2d3748691097db0b7f94925220335
Nic30 <nic30@seznam.cz>
2019-12-06 08:53:37 +0100

    AxiLiteReg: tests cleanup

--- hwtLib/amba/axiLite_comp/reg_test.py
+++ hwtLib/amba/axiLite_comp/reg_test.py
@@ -7,7 +7,7 @@ from hwtLib.amba.axiLite_comp.endpoint_test import AxiLiteEndpointTC
 from hwtLib.amba.axiLite_comp.reg import AxiLiteReg
 
 
-class EpWithReg(Unit):
+class AxiLiteEpWithReg(Unit):
     """
     Unit with AxiLiteEndpoint and AxiLiteReg together
     """
@@ -42,7 +42,7 @@ class EpWithReg(Unit):
 
 class AxiRegTC(AxiLiteEndpointTC):
     def mySetUp(self, data_width=32):
-        u = self.u = EpWithReg(self.STRUCT_TEMPLATE)
+        u = self.u = AxiLiteEpWithReg(self.STRUCT_TEMPLATE)
 
         self.DATA_WIDTH = data_width
         u.DATA_WIDTH = self.DATA_WIDTH
