fcd622695cc50452367c99c0e832804ca15353cb
Nic30 <nic30@seznam.cz>
2019-12-13 16:05:47 +0100

    ports with array type: additional tests

---
+++ hwtLib/examples/specialIntfTypes/InterfaceWithVHDLUnconstrainedArrayImportedType2.vhd
@@ -0,0 +1,23 @@
+--
+--    .. hwt-schematic::
+--    
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+ENTITY InterfaceWithVHDLUnconstrainedArrayImportedType2 IS
+    GENERIC (SIZE_X: INTEGER := 3
+    );
+    PORT (din_0: IN UNSIGNED(7 DOWNTO 0);
+        din_1: IN UNSIGNED(7 DOWNTO 0);
+        din_2: IN UNSIGNED(7 DOWNTO 0);
+        dout: OUT mem(0 to 3)(8 downto 0)
+    );
+END ENTITY;
+
+ARCHITECTURE rtl OF InterfaceWithVHDLUnconstrainedArrayImportedType2 IS
+BEGIN
+    dout(0) <= din_0;
+    dout(1) <= din_1;
+    dout(2) <= din_2;
+END ARCHITECTURE;
