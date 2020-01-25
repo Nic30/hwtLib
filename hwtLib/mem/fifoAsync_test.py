c72f1c763112f43311477beaf0507adcad7eaf6c
Nic30 <nic30@seznam.cz>
2020-01-25 14:06:47 +0100

    dual reset async FIFOs

--- hwtLib/mem/fifoAsync_test.py
+++ hwtLib/mem/fifoAsync_test.py
@@ -12,6 +12,7 @@ from hwtLib.mem.fifo_test import FifoTC
 class FifoAsyncTC(FifoTC):
     IN_CLK = 30 * Time.ns
     CLK = max(FifoTC.OUT_CLK, IN_CLK)
+    ITEMS = 4
 
     @classmethod
     def getUnit(cls):
@@ -23,14 +24,18 @@ class FifoAsyncTC(FifoTC):
     def setUp(self):
         FifoTC.setUp(self)
         self.u.dataIn_clk._ag.period = self.IN_CLK
+        OUT_CLK = self.u.dataOut_clk._ag.period
+        RST_DELAY = max(self.IN_CLK, OUT_CLK) * 1.6
+        self.u.dataIn_rst_n._ag.initDelay = \
+            self.u.dataOut_rst_n._ag.initDelay = RST_DELAY
 
-    def test_tryMore2(self, capturedOffset=1):
+    def test_tryMore2(self, capturedOffset=0):
         FifoTC.test_tryMore2(self, capturedOffset=capturedOffset)
 
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    # suite.addTest(FifoAsyncTC('test_fifoSingleWord'))
+    # suite.addTest(FifoAsyncTC('test_tryMore'))
     suite.addTest(unittest.makeSuite(FifoAsyncTC))
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
