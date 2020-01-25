19a775226f7de3d6e3ccafb27fc2506dd999cfd9
Nic30 <nic30@seznam.cz>
2020-01-25 14:07:58 +0100

    AxiCdc

--- hwtLib/amba/axi_comp/axi_buff_cdc.py
+++ hwtLib/amba/axi_comp/axi_buff_cdc.py
@@ -1,30 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from hwt.interfaces.std import Clk
-from hwtLib.amba.axis_comp.builder import AxiSBuilder
-from hwtLib.amba.axi_comp.axi_buff import AxiBuff
 from hwt.code import isPow2
+from hwt.interfaces.std import Clk, Rst_n
+from hwt.synthesizer.param import Param
+from hwtLib.amba.axi_comp.axi_buff import AxiBuff
+from hwtLib.amba.axis_comp.builder import AxiSBuilder
 
 
-class AxiClockDomainCrossing(AxiBuff):
+class AxiBuffCdc(AxiBuff):
     """
     Clock domain crossing with buffers for AXI3/4/Lite and others
-    
-    .. hwt-schematic:: _example_AxiClockDomainCrossing
+
+    :note: for DEPTH = 1 CDC register is used, else AsyncFifo
+    .. hwt-schematic:: _example_AxiBuffCdc
     """
 
     def _config(self):
         AxiBuff._config(self)
         self.ADDR_BUFF_DEPTH += 1
         self.DATA_BUFF_DEPTH += 1
+        self.M_FREQ = Param(int(102e6))
+        self.S_FREQ = Param(int(102e6))
 
     def _declr(self):
-        super(AxiClockDomainCrossing, self)._declr()
+        super(AxiBuffCdc, self)._declr()
+        self.clk.FREQ = self.M_FREQ
+        self.rst_n._make_association(clk=self.clk)
+
+        self.m._make_association(clk=self.clk, rst=self.rst_n)
+
         self.s_clk = Clk()
-        self.s._make_association(clk=self.s_clk)
-        assert isPow2(self.ADDR_BUFF_DEPTH - 1), self.ADDR_BUFF_DEPTH
-        assert isPow2(self.DATA_BUFF_DEPTH - 1), self.DATA_BUFF_DEPTH
+        self.s_clk.FREQ = self.S_FREQ
+
+        self.s_rst_n = Rst_n()
+        self.s_rst_n._make_association(clk=self.s_clk)
+
+        self.s._make_association(clk=self.s_clk, rst=self.s_rst_n)
+        assert self.ADDR_BUFF_DEPTH == 1 or isPow2(self.ADDR_BUFF_DEPTH - 1), (
+            self.ADDR_BUFF_DEPTH, "size 2**n + 1 for output reg")
+        assert self.DATA_BUFF_DEPTH == 1 or isPow2(self.DATA_BUFF_DEPTH - 1), (
+            self.DATA_BUFF_DEPTH, "size 2**n + 1 for output reg")
 
     def _impl(self):
         ADDR_DEPTH = self.ADDR_BUFF_DEPTH
@@ -35,6 +51,7 @@ class AxiClockDomainCrossing(AxiBuff):
             i = AxiSBuilder(self, m, name).buff_cdc(
                 items=depth,
                 clk=self.s_clk,
+                rst=self.s_rst_n,
             ).end
             s(i)
 
@@ -43,17 +60,18 @@ class AxiClockDomainCrossing(AxiBuff):
             i = AxiSBuilder(self, m, name).buff_cdc(
                 items=depth,
                 clk=self.clk,
+                rst=self.rst_n,
             ).end
             s(i)
 
 
-def _example_AxiClockDomainCrossing():
+def _example_AxiBuffCdc():
     from hwtLib.amba.axi4 import Axi4
-    u = AxiClockDomainCrossing(Axi4)
+    u = AxiBuffCdc(Axi4)
     return u
 
 
 if __name__ == "__main__":
     from hwt.synthesizer.utils import toRtl
-    u = _example_AxiClockDomainCrossing()
+    u = _example_AxiBuffCdc()
     print(toRtl(u))
