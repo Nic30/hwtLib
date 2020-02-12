86913f8d1200cf111fa758e7c3cf4c7c24624740
Nic30 <nic30@seznam.cz>
2020-02-12 21:34:08 +0100

    FullDuplexAxiStream._config share with AxiStream

--- hwtLib/amba/fullDuplexAxiStream.py
+++ hwtLib/amba/fullDuplexAxiStream.py
@@ -1,13 +1,12 @@
 from hwt.hdl.constants import DIRECTION
 from hwt.synthesizer.interface import Interface
-from hwt.synthesizer.param import Param
 from hwtLib.amba.axis import AxiStream
 
 
 class FullDuplexAxiStream(Interface):
 
     def _config(self):
-        self.DATA_WIDTH = Param(64)
+        AxiStream._config(self)
 
     def _declr(self):
         with self._paramsShared():
