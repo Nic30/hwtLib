9771a2b810632e5541248f04466c8af932dcd4df
Nic30 <nic30@seznam.cz>
2020-02-21 11:08:57 +0100

    codestyle

--- hwtLib/amba/axi_comp/builder.py
+++ hwtLib/amba/axi_comp/builder.py
@@ -1,8 +1,8 @@
 from hwtLib.abstract.componentBuilder import AbstractComponentBuilder
+from hwtLib.amba.axiLite_comp.to_axi import AxiLite_to_Axi
 from hwtLib.amba.axi_comp.buff import AxiBuff
 from hwtLib.amba.axi_comp.buff_cdc import AxiBuffCdc
 from hwtLib.amba.axi_comp.resize import AxiResize
-from hwtLib.amba.axiLite_comp.to_axi import AxiLite_to_Axi
 
 
 class AxiBuilder(AbstractComponentBuilder):
