1484ba6c8d28de081ff5b85746ed64d7fa525fa4
Nic30 <nic30@seznam.cz>
2020-03-03 14:00:28 +0100

    AxiStreamFullDuplex: configurable rx,tx

--- hwtLib/amba/axis_fullduplex.py
+++ hwtLib/amba/axis_fullduplex.py
@@ -1,5 +1,6 @@
 from hwt.hdl.constants import DIRECTION
 from hwt.synthesizer.interface import Interface
+from hwt.synthesizer.param import Param
 from hwtLib.amba.axis import AxiStream
 
 
@@ -7,8 +8,13 @@ class AxiStreamFullDuplex(Interface):
 
     def _config(self):
         AxiStream._config(self)
+        self.HAS_RX = Param(True)
+        self.HAS_TX = Param(True)
 
     def _declr(self):
         with self._paramsShared():
-            self.tx = AxiStream()
-            self.rx = AxiStream(masterDir=DIRECTION.IN)
+            if self.HAS_TX:
+                self.tx = AxiStream()
+
+            if self.HAS_RX:
+                self.rx = AxiStream(masterDir=DIRECTION.IN)
