01185da30e515290bf397f11bcac59a72cd5d926
Nic30 <nic30@seznam.cz>
2020-03-05 20:19:22 +0100

    AxiStreamFullDuplexAgent

--- hwtLib/amba/axis_fullduplex.py
+++ hwtLib/amba/axis_fullduplex.py
@@ -2,6 +2,8 @@ from hwt.hdl.constants import DIRECTION
 from hwt.synthesizer.interface import Interface
 from hwt.synthesizer.param import Param
 from hwtLib.amba.axis import AxiStream
+from pycocotb.agents.base import AgentBase
+from pycocotb.hdlSimulator import HdlSimulator
 
 
 class AxiStreamFullDuplex(Interface):
@@ -18,3 +20,33 @@ class AxiStreamFullDuplex(Interface):
 
             if self.HAS_RX:
                 self.rx = AxiStream(masterDir=DIRECTION.IN)
+
+    def _initSimAgent(self, sim: HdlSimulator):
+        self._ag = AxiStreamFullDuplexAgent(sim, self)
+
+
+class AxiStreamFullDuplexAgent(AgentBase):
+    def __init__(self, sim: HdlSimulator, intf: AxiStreamFullDuplex):
+        super(AxiStreamFullDuplexAgent, self).__init__(sim, intf)
+        if intf.HAS_TX:
+            intf.tx._initSimAgent(sim)
+        if intf.HAS_RX:
+            intf.rx._initSimAgent(sim)
+
+    def getDrivers(self):
+        i = self.intf
+        d = []
+        if i.HAS_TX:
+            d.extend(i.tx._ag.getDrivers())
+        if i.HAS_RX:
+            d.extend(i.rx._ag.getMonitors())
+        return d
+
+    def getMonitors(self):
+        i = self.intf
+        d = []
+        if i.HAS_TX:
+            d.extend(i.tx._ag.getMonitors())
+        if i.HAS_RX:
+            d.extend(i.rx._ag.getDrivers())
+        return d
