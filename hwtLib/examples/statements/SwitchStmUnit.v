6c2a43b41d0fcf2612502fe82ead11d9f17c842a
Nic30 <nic30@seznam.cz>
2020-04-24 21:56:31 +0200

    update vhdl/verilog codestyle in examples

--- hwtLib/examples/statements/SwitchStmUnit.v
+++ hwtLib/examples/statements/SwitchStmUnit.v
@@ -1,31 +1,25 @@
-/*
-
-    Example which is using switch statement to create multiplexer
-
-    .. hwt-schematic::
-    
-*/
-module SwitchStmUnit(input a,
-        input b,
-        input c,
-        output reg out,
-        input [2:0] sel
-    );
-
-    always @(a or b or c or sel) begin: assig_process_out
+//
+//    Example which is using switch statement to create multiplexer
+//
+//    .. hwt-schematic::
+//    
+module SwitchStmUnit (
+    input  a,
+    input  b,
+    input  c,
+    output reg out,
+    input [2:0] sel
+);
+    always @(a, b, c, sel) begin: assig_process_out
         case(sel)
-        3'b000: begin
-            out = a;
-        end
-        3'b001: begin
-            out = b;
-        end
-        3'b010: begin
-            out = c;
-        end
-        default: begin
-            out = 1'b0;
-        end
+            3'b000:
+                out = a;
+            3'b001:
+                out = b;
+            3'b010:
+                out = c;
+            default:
+                out = 1'b0;
         endcase
     end
 
