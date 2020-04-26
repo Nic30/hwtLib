7dcbfeddb119ef86874e57ac8ca8d43fccfe08d9
Nic30 <nic30@seznam.cz>
2020-04-26 23:15:53 +0200

    update systemc examples

--- hwtLib/examples/statements/FsmExample.cpp
+++ hwtLib/examples/statements/FsmExample.cpp
@@ -1,74 +1,68 @@
-/*
-
-    .. hwt-schematic::
-
-*/
-
 #include <systemc.h>
 
-
+//
+//    .. hwt-schematic::
+//    
 SC_MODULE(FsmExample) {
-    //interfaces
+    // ports
     sc_in<sc_uint<1>> a;
     sc_in<sc_uint<1>> b;
     sc_in_clk clk;
     sc_out<sc_uint<3>> dout;
     sc_in<sc_uint<1>> rst_n;
-
-    //internal signals
-    sc_uint<2> st = 0;
+    // component instances
+    // internal signals
+    sc_uint<2> st(0);
     sc_signal<sc_uint<2>> st_next;
-
-    //processes inside this component
     void assig_process_dout() {
         switch(st) {
-        case 0:
-            dout.write(sc_uint<3>("001"));
-            break;
-        case 1:
-            dout.write(sc_uint<3>("010"));
-            break;
+        case 0: {
+                dout.write(sc_uint<3>(0b001));
+                break;
+            }
+        case 1: {
+                dout.write(sc_uint<3>(0b010));
+                break;
+            }
         default:
-            dout.write(sc_uint<3>("011"));
-            break;
+                dout.write(sc_uint<3>(0b011));
         }
     }
+
     void assig_process_st() {
-        if (rst_n.read() == 0) {
+        if (rst_n.read() == sc_uint<1>(0b0))
             st = 0;
-        } else {
+        else
             st = st_next.read();
-        }
     }
+
     void assig_process_st_next() {
         switch(st) {
-        case 0:
-            if ((a.read() & b.read()) == 1) {
-                st_next.write(2);
-            } else if (b.read() == 1) {
-                st_next.write(1);
-            } else {
-                st_next.write(st);
+        case 0: {
+                if ((a.read() & b.read()) == sc_uint<1>(0b1))
+                    st_next.write(2);
+                else if (b.read() == sc_uint<1>(0b1))
+                    st_next.write(1);
+                else
+                    st_next.write(st);
+                break;
             }
-            break;
-        case 1:
-            if ((a.read() & b.read()) == 1) {
-                st_next.write(2);
-            } else if (a.read() == 1) {
-                st_next.write(0);
-            } else {
-                st_next.write(st);
+        case 1: {
+                if ((a.read() & b.read()) == sc_uint<1>(0b1))
+                    st_next.write(2);
+                else if (a.read() == sc_uint<1>(0b1))
+                    st_next.write(0);
+                else
+                    st_next.write(st);
+                break;
             }
-            break;
         default:
-            if ((a.read() & ~b.read()) == 1) {
-                st_next.write(0);
-            } else if ((~a.read() & b.read()) == 1) {
-                st_next.write(1);
-            } else {
-                st_next.write(st);
-            }
-            break;
+                if ((a.read() & ~(b.read())) == sc_uint<1>(0b1))
+                    st_next.write(0);
+                else if ((~(a.read()) & b.read()) == sc_uint<1>(0b1))
+                    st_next.write(1);
+                else
+                    st_next.write(st);
         }
     }
 
