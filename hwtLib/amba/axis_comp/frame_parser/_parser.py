605b0b1278e9138b1cf625e9525485fff33701c5
Nic30 <nic30@seznam.cz>
2020-04-27 17:03:52 +0200

    AxiS_frameParser: do not share T with subcomponents

--- hwtLib/amba/axis_comp/frame_parser/_parser.py
+++ hwtLib/amba/axis_comp/frame_parser/_parser.py
@@ -206,7 +206,7 @@ class AxiS_frameParser(AxiSCompBase, TemplateConfigured):
                     # then there will be a variable size suffix
                     children[0].OVERFLOW_SUPPORT = True
 
-                with self._paramsShared(exclude=({"OVERFLOW_SUPPORT"}, {})):
+                with self._paramsShared(exclude=({"OVERFLOW_SUPPORT", "T"}, {})):
                     self.children = children
 
     def parser_fsm(self, words):
