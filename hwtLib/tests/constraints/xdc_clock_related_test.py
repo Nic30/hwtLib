d47fb9c51ef828ae1064765e2b607a8bbd65e46e
Nic30 <nic30@seznam.cz>
2020-05-11 00:24:08 +0200

    tests: clock related constrains for shared components

--- hwtLib/tests/constraints/xdc_clock_related_test.py
+++ hwtLib/tests/constraints/xdc_clock_related_test.py
@@ -4,11 +4,59 @@ import os
 from tempfile import TemporaryDirectory
 import unittest
 
+from hwt.interfaces.std import Rst_n, Clk
 from hwt.serializer.store_manager import SaveToFilesFlat
 from hwt.serializer.vhdl import Vhdl2008Serializer
 from hwt.serializer.xdc.serializer import XdcSerializer
+from hwt.synthesizer.param import Param
+from hwt.synthesizer.unit import Unit
 from hwt.synthesizer.utils import toRtl
+from hwtLib.amba.axis_fullduplex import AxiStreamFullDuplex
 from hwtLib.mem.fifoAsync import FifoAsync
+from hwtLib.amba.axis_comp.builder import AxiSBuilder
+
+
+# note: not in main library as typical usecase differs to much
+#     and it is more simple to instanciate 2 independent CDCs
+#     rather than using complex configuration
+#     (separate clk, rsts for rx/tx, different buff sizes, ...)
+class AxiStreamFullDuplexCdc(Unit):
+    """
+    CDC for AxiStreamFullDuplex interface
+    (2x async fifo with same params, hdl component shared)
+    """
+
+    def _config(self):
+        self.DEPTH = Param(0)
+        self.IN_FREQ = Param(int(100e6))
+        self.OUT_FREQ = Param(int(100e6))
+        AxiStreamFullDuplex._config(self)
+
+    def _declr(self):
+        self.dataIn_clk = Clk()
+        self.dataIn_clk.FREQ = self.IN_FREQ
+        self.dataOut_clk = Clk()
+        self.dataOut_clk.FREQ = self.OUT_FREQ
+
+        with self._paramsShared():
+            with self._associated(clk=self.dataIn_clk):
+                self.dataIn_rst_n = Rst_n()
+                with self._associated(rst=self.dataIn_rst_n):
+                    self.dataIn = AxiStreamFullDuplex()
+
+            with self._associated(clk=self.dataOut_clk):
+                self.dataOut_rst_n = Rst_n()
+                with self._associated(rst=self.dataOut_rst_n):
+                    self.dataOut = AxiStreamFullDuplex()._m()
+
+    def _impl(self):
+        tx = AxiSBuilder(self, self.dataIn.tx).buff_cdc(
+            self.dataOut_clk, self.dataOut_rst_n, self.DEPTH).end
+        self.dataOut.tx(tx)
+
+        rx = AxiSBuilder(self, self.dataOut.rx).buff_cdc(
+            self.dataIn_clk, self.dataIn_rst_n, self.DEPTH).end
+        self.dataIn.rx(rx)
 
 
 class ConstraintsXdcClockRelatedTC(unittest.TestCase):
@@ -21,7 +69,8 @@ class ConstraintsXdcClockRelatedTC(unittest.TestCase):
             saver = SaveToFilesFlat(Vhdl2008Serializer, build_root)
             toRtl(u, saver)
 
-            f_name = os.path.join(build_root, "constraints" + XdcSerializer.fileExtension)
+            f_name = os.path.join(
+                build_root, "constraints" + XdcSerializer.fileExtension)
             with open(f_name) as f:
                 s = f.read()
             # with open(ref_f_name, "w") as f:
@@ -35,6 +84,11 @@ class ConstraintsXdcClockRelatedTC(unittest.TestCase):
         u = FifoAsync()
         u.DEPTH = 8
         self.assert_constraints_file_eq(u, "FifoAsync.xdc")
+
+    def testAxiStreamFullDuplexCdc(self):
+        u = AxiStreamFullDuplexCdc()
+        u.DEPTH = 9
+        self.assert_constraints_file_eq(u, "AxiStreamFullDuplexCdc.xdc")
 
 
 if __name__ == '__main__':
