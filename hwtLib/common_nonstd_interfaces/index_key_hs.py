bf4ff3fc4fc04200a9636e2cdb8baf86d9a0c67d
Nic30 <nic30@seznam.cz>
2020-10-09 00:31:13 +0200

    IndexKeyHs: key as optional

--- hwtLib/common_nonstd_interfaces/index_key_hs.py
+++ hwtLib/common_nonstd_interfaces/index_key_hs.py
@@ -1,3 +1,5 @@
+from typing import Union, Tuple
+
 from hwt.interfaces.agents.handshaked import HandshakedAgent
 from hwt.interfaces.std import Handshaked, HandshakeSync, VectSignal
 from hwt.synthesizer.param import Param
@@ -12,7 +14,8 @@ class IndexKeyHs(Handshaked):
 
     def _declr(self):
         HandshakeSync._declr(self)
-        self.key = VectSignal(self.KEY_WIDTH)
+        if self.KEY_WIDTH:
+            self.key = VectSignal(self.KEY_WIDTH)
         self.index = VectSignal(self.INDEX_WIDTH)
 
     def _initSimAgent(self, sim: HdlSimulator):
@@ -21,13 +24,23 @@ class IndexKeyHs(Handshaked):
 
 class IndexKeyHsAgent(HandshakedAgent):
 
-    def get_data(self):
+    def get_data(self) -> Union[Tuple[int, int], int]:
         i = self.intf
-        return (i.key.read(), i.index.read())
+        if i.KEY_WIDTH:
+            return (i.key.read(), i.index.read())
+        else:
+            return i.index.read()
 
-    def set_data(self, data):
+    def set_data(self, data: Union[Tuple[int, int], int]):
         intf = self.intf
-        k, i = data
-        intf.key.write(k)
+        if intf.KEY_WIDTH:
+            if data is None:
+                k = None
+                i = None
+            else:
+                k, i = data
+            intf.key.write(k)
+        else:
+            i = data
         intf.index.write(i)
 
