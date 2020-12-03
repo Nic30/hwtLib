fc487857b75effed5ee2d6a9592852680f416702
Nic30 <nic30@seznam.cz>
2020-12-03 01:51:25 +0100

    If_solvable_comb_loop testes

--- hwtLib/tests/synthesizer/statements_test.py
+++ hwtLib/tests/synthesizer/statements_test.py
@@ -3,13 +3,45 @@
 
 import unittest
 
-from hwt.code import Switch
+from hwt.code import Switch, If
 from hwt.hdl.ifContainter import IfContainer
 from hwt.hdl.switchContainer import SwitchContainer
 from hwt.hdl.typeShortcuts import hBit
 from hwt.hdl.types.bits import Bits
 from hwt.hdl.types.defs import BIT
 from hwt.synthesizer.rtlLevel.netlist import RtlNetlist
+from hwt.synthesizer.unit import Unit
+from hwt.interfaces.std import Signal
+from hwt.synthesizer.utils import synthesised
+
+
+class If_solvable_comb_loop(Unit):
+
+    def _declr(self):
+        self.a = Signal()
+        self.c = Signal()._m()
+
+    def _impl(self):
+        b = self.b = self._sig("b")
+
+        return If(self.a,
+           # b is input and output of this if statement,
+           # the statement has to be split in to two to remove this dependency
+            b(self.a),
+            self.c(b),
+        )
+
+
+class If_solvable_comb_loop_nested(If_solvable_comb_loop):
+
+    def _declr(self):
+        super(If_solvable_comb_loop_nested, self)._declr()
+        self.d = Signal()
+
+    def _impl(self):
+        If(self.d,
+           super(If_solvable_comb_loop_nested, self)._impl()
+        )
 
 
 class StatementsTC(unittest.TestCase):
@@ -60,13 +92,13 @@ class StatementsTC(unittest.TestCase):
         s0 = SwitchContainer(switchOn=a, cases=[
             (hBit(0), [b(0), ])
         ])
-        
+
         s1 = SwitchContainer(switchOn=a, cases=[
             (hBit(0), [b(0), ])
         ])
         self.assertTrue(s0.isSame(s1))
         self.assertTrue(s1.isSame(s0))
-        
+
         s2 = SwitchContainer(switchOn=a, cases=[
             (hBit(0), [b(0), ]),
             (hBit(1), [b(0), ]),
@@ -139,7 +171,7 @@ class StatementsTC(unittest.TestCase):
         s0_red, io_change = s0._try_reduce()
         self.assertFalse(io_change)
         self.assertEqual(s0_red, [])
-               
+
     def test_SwitchContainer_cut_off_drivers_of(self):
         nl = RtlNetlist()
         a = nl.sig("a", BIT)
@@ -156,7 +188,7 @@ class StatementsTC(unittest.TestCase):
             (hBit(0), [b(0), ])
         ])
         self.assertTrue(s0.isSame(s0_1))
-        
+
         # reduce nothing
         s0 = Switch(a).add_cases([
             (hBit(0), [b(0), ])
@@ -196,6 +228,45 @@ class StatementsTC(unittest.TestCase):
         ]).Default(c(1))
         self.assertTrue(s2.isSame(s2_rem))
         self.assertTrue(s2_0.isSame(s2_cut))
+
+    def test_If_solvable_comb_loop(self):
+        u = If_solvable_comb_loop()
+        synthesised(u)
+        b_d = u.b.drivers
+        c_d = u.c._sigInside.drivers
+        self.assertEqual(len(b_d), 1)
+        self.assertEqual(len(c_d), 1)
+        self.assertIsNot(b_d[0], c_d[0])
+
+        self.assertIsNone(b_d[0].parentStm)
+        self.assertSequenceEqual(b_d[0]._inputs, [u.a._sigInside])
+        self.assertSequenceEqual(b_d[0]._outputs, [u.b])
+        self.assertSequenceEqual(b_d[0]._sensitivity, [u.a._sigInside])
+
+        self.assertIsNone(c_d[0].parentStm)
+        self.assertSequenceEqual(c_d[0]._inputs, [u.a._sigInside, u.b])
+        self.assertSequenceEqual(c_d[0]._outputs, [u.c._sigInside])
+        self.assertSequenceEqual(c_d[0]._sensitivity, [u.a._sigInside, u.b])
+
+    def test_If_solvable_comb_loop_nested(self):
+        u = If_solvable_comb_loop_nested()
+        synthesised(u)
+        b_d = u.b.drivers
+        c_d = u.c._sigInside.drivers
+        self.assertEqual(len(b_d), 1)
+        self.assertEqual(len(c_d), 1)
+        self.assertIsNot(b_d[0], c_d[0])
+
+        d = u.d._sigInside
+        self.assertIsNone(b_d[0].parentStm)
+        self.assertSequenceEqual(b_d[0]._inputs, [d, u.a._sigInside])
+        self.assertSequenceEqual(b_d[0]._outputs, [u.b])
+        self.assertSequenceEqual(b_d[0]._sensitivity, [d, u.a._sigInside])
+
+        self.assertIsNone(c_d[0].parentStm)
+        self.assertSequenceEqual(c_d[0]._inputs, [d, u.a._sigInside, u.b])
+        self.assertSequenceEqual(c_d[0]._outputs, [u.c._sigInside])
+        self.assertSequenceEqual(c_d[0]._sensitivity, [d, u.a._sigInside, u.b])
 
 
 if __name__ == '__main__':
