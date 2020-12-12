33b22ef744d788e05e509ebac3c6d893d974d7e7
Nic30 <nic30@seznam.cz>
2020-12-12 15:16:17 +0100

    doc

--- hwtLib/amba/__init__.py
+++ hwtLib/amba/__init__.py
@@ -12,6 +12,7 @@ This icludes:
 * and others
 
 Available specifications:
+
 * https://static.docs.arm.com/ihi0022/d/IHI0022D_amba_axi_protocol_spec.pdf
 * https://static.docs.arm.com/ihi0051/a/IHI0051A_amba4_axi4_stream_v1_0_protocol_spec.pdf
 """
