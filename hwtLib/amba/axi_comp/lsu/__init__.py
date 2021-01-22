99e9812c99bea592c8092c018f1a6b85b4b113f7
Nic30 <nic30@seznam.cz>
2021-01-22 22:21:54 +0100

    doc

--- hwtLib/amba/axi_comp/lsu/__init__.py
+++ hwtLib/amba/axi_comp/lsu/__init__.py
@@ -1,3 +1,10 @@
 """
 This package is dedicated to AXI Load/Store Units (LSU) a others like.
+
+Other LSU implementations:
+
+* https://github.com/riscv-boom/riscv-boom - has RISC-V LSU in chisel3, read kill, MSHRs
+* https://github.com/rsd-devel/rsd - has N issue RISC-V LSU
+* https://github.com/bluespec/Toooba - has RISC-V LSU in BlueSpecVerilog
+* https://github.com/openhwgroup/cv32e40p - has RISC-V LSU for in order pipeline
 """
