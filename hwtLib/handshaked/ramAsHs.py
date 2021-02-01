05f8cf8221decc907185df17b942be8aacae67d9
Nic30 <nic30@seznam.cz>
2021-02-01 18:13:03 +0100

    RamAsHs: support for read/write only variants

--- hwtLib/handshaked/ramAsHs.py
+++ hwtLib/handshaked/ramAsHs.py
@@ -3,18 +3,17 @@
 
 from hwt.code import If
 from hwt.hdl.constants import DIRECTION
+from hwt.hdl.types.defs import BIT
+from hwt.hdl.types.struct import HStruct
 from hwt.interfaces.std import Handshaked, BramPort_withoutClk
 from hwt.interfaces.utils import addClkRstn
 from hwt.serializer.mode import serializeParamsUniq
 from hwt.synthesizer.interface import Interface
 from hwt.synthesizer.param import Param
 from hwt.synthesizer.unit import Unit
+from hwtLib.common_nonstd_interfaces.addr_data_hs import AddrDataHs
 from hwtSimApi.agents.base import AgentBase
 from hwtSimApi.hdlSimulator import HdlSimulator
-
-from hwtLib.common_nonstd_interfaces.addr_data_hs import AddrDataHs
-from hwt.hdl.types.struct import HStruct
-from hwt.hdl.types.defs import BIT
 
 
 class RamHsRAgent(AgentBase):
@@ -45,14 +44,16 @@ class RamHsRAgent(AgentBase):
         self.data = intf.data._ag
 
     def getDrivers(self):
-        return (self.addr.getDrivers() +
-                self.data.getMonitors()
-                )
+        return (
+            self.addr.getDrivers() +
+            self.data.getMonitors()
+        )
 
     def getMonitors(self):
-        return (self.addr.getMonitors() +
-                self.data.getDrivers()
-                )
+        return (
+            self.addr.getMonitors() +
+            self.data.getDrivers()
+        )
 
 
 class RamHsR(Interface):
@@ -79,27 +80,27 @@ class RamHsR(Interface):
 @serializeParamsUniq
 class RamAsHs(Unit):
     """
-    Converter from ram port to handshaked interfaces
+    Converter from a single ram port to handshaked interfaces
 
     .. hwt-autodoc::
     """
 
     def _config(self):
-        self.ADDR_WIDTH = Param(32)
-        self.DATA_WIDTH = Param(64)
+        BramPort_withoutClk._config(self)
 
     def _declr(self):
+        assert self.HAS_R or self.HAS_W
         addClkRstn(self)
         with self._paramsShared():
-            self.r = RamHsR()
-            self.w = AddrDataHs()
+            if self.HAS_R:
+                self.r = RamHsR()
+            if self.HAS_W:
+                self.w = AddrDataHs()
+                self.w.HAS_MASK = self.HAS_BE
+
             self.ram = BramPort_withoutClk()._m()
 
-    def _impl(self):
-        r = self.r
-        w = self.w
-        ram = self.ram
-
+    def read_logic(self, r: RamHsR, ram: BramPort_withoutClk):
         readDataPending = self._reg("readDataPending", def_val=0)
         readData = self._reg("readData",
                              HStruct((r.data.data._dtype, "data"),
@@ -132,20 +133,50 @@ class RamAsHs(Unit):
 
         r.addr.rd(rEn)
 
-        If(rEn & r.addr.vld,
-           ram.we(0),
-           ram.addr(r.addr.data)
-        ).Else(
-           ram.we(1),
-           ram.addr(w.addr)
-        )
-        wEn = ~rEn | ~r.addr.vld
-        w.rd(wEn)
+        return rEn, readData
 
-        ram.din(w.data)
-        ram.en((rEn & r.addr.vld) | w.vld)
-        r.data.data(readData.data)
-        r.data.vld(readData.vld)
+    def _impl(self):
+        ram = self.ram
+        if self.HAS_R:
+            r = self.r
+            rEn, readData = self.read_logic(r, ram)
+
+            if self.HAS_W:
+                # read/write
+                w = self.w
+                if self.HAS_MASK:
+                    ram.be(w.mask)
+                If(rEn & r.addr.vld,
+                   ram.we(0),
+                   ram.addr(r.addr.data)
+                ).Else(
+                   ram.we(1),
+                   ram.addr(w.addr)
+                )
+                wEn = ~rEn | ~r.addr.vld
+                w.rd(wEn)
+
+                ram.din(w.data)
+                ram.en((rEn & r.addr.vld) | w.vld)
+                r.data.data(readData.data)
+                r.data.vld(readData.vld)
+            else:
+                # read only
+                ram.addr(r.addr.data)
+                ram.en(rEn & r.addr.vld)
+                r.data.data(readData.data)
+                r.data.vld(readData.vld)
+
+        elif self.HAS_W:
+            # write only
+            w = self.w
+            w.rd(1)
+            if self.HAS_MASK:
+                ram.be(w.mask)
+            ram.we(w.vld)
+            ram.addr(w.addr)
+            ram.din(w.data)
+            ram.en(w.vld)
 
 
 if __name__ == "__main__":
