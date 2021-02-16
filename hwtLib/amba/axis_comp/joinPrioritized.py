3e1b49cb68ef5e58114e9a3597a45f23bf2cd969
Nic30 <nic30@seznam.cz>
2021-02-16 10:29:24 +0100

    doc

--- hwtLib/amba/axis_comp/joinPrioritized.py
+++ hwtLib/amba/axis_comp/joinPrioritized.py
@@ -28,18 +28,21 @@ class AxiSJoinPrioritized(AxiSCompBase, HsJoinPrioritized):
         join._updateParamsFrom(self)
         join.get_valid_signal = self.get_valid_signal
         join.get_ready_signal = self.get_ready_signal
+
         self.join = join
+
+        # en override
         ens = [self._reg(f"en{i:d}", def_val=0) for i in range(len(self.dataIn))]
         all_0 = ~Or(*ens)
-        for _en, din, c_din in zip(ens, self.dataIn, self.join.dataIn):
-            en = (all_0 | _en)
+        for en_flag, din, c_din in zip(ens, self.dataIn, self.join.dataIn):
+            en = (all_0 | en_flag)
             vld = self.get_valid_signal(c_din)
             vld(en & self.get_valid_signal(din))
             rd = self.get_ready_signal(din)
             rd(en & self.get_ready_signal(c_din))
             self.dataConnectionExpr(din, c_din)
             If(rd & vld,
-               _en(~din.last)
+               en_flag(~din.last)
             )
         self.dataOut(join.dataOut)
 
