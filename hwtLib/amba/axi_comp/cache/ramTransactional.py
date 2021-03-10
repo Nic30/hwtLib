133ce46ca02e6b54bd585cc6f0b88be16c7b5ced
Nic30 <nic30@seznam.cz>
2021-03-10 20:25:37 +0100

    RamTransactionalTC: simplify testing

--- hwtLib/amba/axi_comp/cache/ramTransactional.py
+++ hwtLib/amba/axi_comp/cache/ramTransactional.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from typing import Tuple, List
+
 from hwt.code import Concat, If
+from hwt.code_utils import rename_signal
 from hwt.hdl.constants import READ, WRITE
 from hwt.hdl.types.bits import Bits
 from hwt.hdl.types.defs import BIT
@@ -10,22 +13,26 @@ from hwt.interfaces.agents.handshaked import UniversalHandshakedAgent
 from hwt.interfaces.agents.universalComposite import UniversalCompositeAgent
 from hwt.interfaces.hsStructIntf import HsStructIntf
 from hwt.interfaces.std import HandshakeSync, VectSignal, Signal
+from hwt.interfaces.structIntf import StructIntf
 from hwt.interfaces.utils import addClkRstn, propagateClkRstn
 from hwt.math import log2ceil
 from hwt.synthesizer.interface import Interface
 from hwt.synthesizer.param import Param
+from hwt.synthesizer.rtlLevel.rtlSignal import RtlSignal
 from hwt.synthesizer.unit import Unit
 from hwtLib.amba.axis import AxiStream
-from hwtLib.handshaked.ramAsHs import RamAsHs
+from hwtLib.common_nonstd_interfaces.addr_data_hs import AddrDataHs
+from hwtLib.handshaked.ramAsHs import RamAsHs, RamHsR
 from hwtLib.handshaked.reg import HandshakedReg
 from hwtLib.handshaked.streamNode import StreamNode
 from hwtLib.mem.ram import RamSingleClock
 from hwtSimApi.hdlSimulator import HdlSimulator
 from ipCorePackager.constants import DIRECTION
 from pyMathBitPrecise.bit_utils import mask
+from hwt.synthesizer.hObjList import HObjList
 
 
-class RamHsR_addr(HandshakeSync):
+class TransRamHsR_addr(HandshakeSync):
     """
     .. hwt-autodoc::
     """
@@ -44,7 +51,7 @@ class RamHsR_addr(HandshakeSync):
         self._ag = UniversalHandshakedAgent(sim, self)
 
 
-class RamHsR(Interface):
+class TransRamHsR(Interface):
     """
     Handshaked RAM port
 
@@ -58,7 +65,7 @@ class RamHsR(Interface):
 
     def _declr(self):
         with self._paramsShared():
-            self.addr = RamHsR_addr()
+            self.addr = TransRamHsR_addr()
             d = self.data = AxiStream(masterDir=DIRECTION.IN)
             d.USE_STRB = False
             d.USE_KEEP = False
@@ -67,17 +74,17 @@ class RamHsR(Interface):
         self._ag = UniversalCompositeAgent(sim, self)
 
 
-class RamHsW_addr(RamHsR_addr):
+class TransRamHsW_addr(TransRamHsR_addr):
 
     def _declr(self):
+        TransRamHsR_addr._declr(self)
         self.flush = Signal()
-        RamHsR_addr._declr(self)
 
     def _initSimAgent(self, sim:HdlSimulator):
         self._ag = UniversalHandshakedAgent(sim, self)
 
 
-class RamHsW(RamHsR_addr):
+class TransRamHsW(TransRamHsR_addr):
     """
     .. hwt-autodoc::
     """
@@ -85,11 +92,11 @@ class RamHsW(RamHsR_addr):
     def _config(self):
         self.DATA_WIDTH = Param(64)
         self.USE_STRB = Param(True)
-        RamHsR_addr._config(self)
+        TransRamHsR_addr._config(self)
 
     def _declr(self):
         with self._paramsShared():
-            self.addr = RamHsW_addr()
+            self.addr = TransRamHsW_addr()
             d = self.data = AxiStream()
             d.ID_WIDTH = 0
             d.USE_KEEP = False
@@ -113,15 +120,19 @@ class RamTransactional(Unit):
         self.WORDS_WIDTH = Param(16)
         self.MAX_BLOCK_DATA_WIDTH = Param(None)
 
-    def _declr(self):
+    def _declr_io(self):
         assert self.WORDS_WIDTH % self.DATA_WIDTH == 0
         self.ITEM_WORDS = self.WORDS_WIDTH // self.DATA_WIDTH
+        self.WORD_INDEX_MAX = self.ITEM_WORDS - 1
+
         addClkRstn(self)
         with self._paramsShared():
-            self.r = RamHsR()
-            self.w = RamHsW()
-            self.flush_data = RamHsW()._m()
+            self.r = TransRamHsR()
+            self.w = TransRamHsW()
+            self.flush_data = TransRamHsW()._m()
 
+    def _declr(self):
+        self._declr_io()
         d = self.data_array = RamSingleClock()
         d.MAX_BLOCK_DATA_WIDTH = self.MAX_BLOCK_DATA_WIDTH
         d.DATA_WIDTH = self.DATA_WIDTH
@@ -129,7 +140,7 @@ class RamTransactional(Unit):
         d.PORT_CNT = (READ, WRITE)
         d.HAS_BE = True
 
-    def _impl(self):
+    def construct_ram_io(self) -> Tuple[RamHsR, AddrDataHs]:
         data_arr_r_port, data_arr_w_port = self.data_array.port
         data_arr_r_to_hs = RamAsHs()
         data_arr_w_to_hs = RamAsHs()
@@ -141,32 +152,214 @@ class RamTransactional(Unit):
         data_arr_r_port(data_arr_r_to_hs.ram)
         data_arr_w_port(data_arr_w_to_hs.ram)
 
-        da_r, da_w = data_arr_r_to_hs.r, data_arr_w_to_hs.w
-        INDEX_SUFFIX_MAX = self.ITEM_WORDS - 1
+        return data_arr_r_to_hs.r, data_arr_w_to_hs.w
 
-        # r_index = HandshakedReg(HsStructIntf)
-        # r_index.T = HStruct(
-        #     (Bits(self.ADDR_WIDTH), "index_prefix"),
-        #     (Bits(log2ceil(self.ITEM_WORDS)), "index_suffix"),
-        # )
-        # self.r_index = r_index
+    def construct_r_meta(self, flush_req: RtlSignal,
+                         read_pending: RtlSignal,
+                         r: RamHsR, w: AddrDataHs,
+                         w_index: StructIntf,
+                         r_index_o: StructIntf) -> HandshakedReg:
 
-        # w_index = HandshakedReg(HsStructIntf)
-        # w_index.T = HStruct(
-        #    *r_index_t.fields,
-        #    (BIT, "flushing"),
-        # )
-        # self.w_index = w_index
+        r_meta = HObjList(HandshakedReg(HsStructIntf) for _ in range(2))
+        for reg in r_meta:
+            reg.LATENCY = 1
+            reg.T = HStruct(
+                *([(Bits(self.ID_WIDTH), "id")] if self.ID_WIDTH else []),
+                (BIT, "flushing"),
+                (BIT, "is_last"),
+                (BIT, "is_first"),
+            )
+        self.r_meta = r_meta
+        r_meta[1].dataIn(r_meta[0].dataOut)
 
+        r_meta_i = r_meta[0].dataIn.data
+        WORD_INDEX_MAX = self.WORD_INDEX_MAX
+        w_index_o = w_index
+        flush_pending = rename_signal(self, w_index.vld & w_index_o.flushing, "flush_pending")
+
+        def id_copy():
+            if self.ID_WIDTH:
+                return \
+                If(r_meta[0].dataOut.vld,
+                   r_meta_i.id(r_meta[0].dataOut.data.id),
+                ).Else(
+                   r_meta_i.id(r_meta[1].dataOut.data.id),
+                )
+            else:
+                return []
+
+        # begin of entirely new read
+        If(flush_pending,
+            id_copy(),
+            r_meta_i.flushing(1),
+            r_meta_i.is_first(0),
+            r_meta_i.is_last(w_index_o.word_index._eq(WORD_INDEX_MAX)),
+        ).Elif(flush_req & ~read_pending,
+            r_meta_i.id(w.addr.id) if self.ID_WIDTH else [],
+            r_meta_i.flushing(1),
+            r_meta_i.is_first(1),
+            r_meta_i.is_last(0),
+        ).Else(
+            If(read_pending,
+               id_copy(),
+               r_meta_i.is_first(0),
+               r_meta_i.is_last(r_index_o.word_index._eq(WORD_INDEX_MAX)),
+            ).Else(
+               r_meta_i.id(r.addr.id) if self.ID_WIDTH else [],
+               r_meta_i.is_first(1),
+               r_meta_i.is_last(0),
+            ),
+            r_meta_i.flushing(0),
+        )
+
+        return r_meta
+
+    def construct_read_part(self,
+                            r: TransRamHsR,
+                            da_r: RamHsR,
+                            r_meta: List[HandshakedReg],
+                            flush_req: RtlSignal,
+                            read_pending: RtlSignal,
+                            r_index_o: StructIntf,
+                            r_index_i: StructIntf,
+                            w_index_o: StructIntf,
+                            flush_data: TransRamHsW):
+
+        r_disp_node = StreamNode(
+            [r.addr],
+            [da_r.addr, r_meta[0].dataIn, ],
+            skipWhen={
+                r.addr: flush_req | read_pending,
+            },
+            extraConds={
+                r.addr:~flush_req | ~read_pending,
+            }
+        )
+        r_disp_node.sync()
+
+        If(read_pending,
+            da_r.addr.data(Concat(r_index_o.item_index, r_index_o.word_index))
+        ).Else(
+            da_r.addr.data(Concat(r.addr.addr, r_index_o.word_index._dtype.from_py(0)))
+        )
+
+        w = self.w
+        If(rename_signal(self, r_disp_node.ack(), "r_disp_ack"),
+            If(r_index_o.word_index._eq(0),
+                # last item or completly new item
+                # [note] flushing has priority
+                If(flush_req,
+                    # begin of the read for flush
+                    r_index_i.item_index(w.addr.addr),
+                    r_index_i.vld(1),
+                ).Else(
+                    # begin of the normal read
+                    r_index_i.item_index(r.addr.addr),
+                    r_index_i.vld(r.addr.vld),
+                ),
+                r_index_i.word_index(1),
+            ).Elif(r_index_o.word_index._eq(self.WORD_INDEX_MAX),
+                # first addr and data is skipping this reg
+                # this means that tere has to be 1 clk of vld=0 for this first word to pass
+                r_index_i.word_index(0),
+                r_index_i.vld(0),
+            ).Else(
+                # pass next word of item
+                r_index_i.word_index(r_index_o.word_index + 1),
+                r_index_i(r_index_o, exclude=[r_index_i.word_index]),
+            )
+        )
+        _r_meta_o = r_meta[1].dataOut
+        r_meta_o = _r_meta_o.data
+        StreamNode(
+            [da_r.data, _r_meta_o],
+            [r.data, flush_data.addr, flush_data.data],
+            skipWhen={
+                r.data: _r_meta_o.vld & r_meta_o.flushing,
+                flush_data.data: _r_meta_o.vld & ~r_meta_o.flushing,
+                flush_data.addr: _r_meta_o.vld & (~r_meta_o.flushing | ~r_meta_o.is_first),
+            },
+            extraConds={
+                r.data:~r_meta_o.flushing,
+                flush_data.data: r_meta_o.flushing,
+                flush_data.addr: r_meta_o.flushing & r_meta_o.is_first,
+            },
+        ).sync()
+
+        if self.ID_WIDTH:
+            flush_data.addr.id(r_meta_o.id)
+        flush_data.addr.addr(w_index_o.item_index)
+        flush_data.addr.flush(1)
+        flush_data.data.data(da_r.data.data)
+        flush_data.data.strb(mask(flush_data.data.strb._dtype.bit_length()))
+        flush_data.data.last(r_meta_o.is_last)
+
+        if self.ID_WIDTH:
+            r.data.id(r_meta_o.id)
+        r.data.data(da_r.data.data)
+        r.data.last(r_meta_o.is_last)
+
+    def construct_write_part(self, w: TransRamHsW,
+                             da_w: AddrDataHs,
+                             w_index_i: StructIntf,
+                             w_index_o, r_index_o,
+                             da_r: RamHsR,
+                             r_meta_din: HsStructIntf):
+        WORD_INDEX_MAX = self.WORD_INDEX_MAX
+
+        write_pending = r_index_o.vld
+        w_disp_node = StreamNode(
+            [w.addr, w.data],
+            [da_w, ],
+            skipWhen={
+                # dissable address input if finishing peding transaction
+                w.addr: write_pending,
+            },
+            extraConds={
+                # stall if pending read and flush required
+                w.addr: rename_signal(self, ~write_pending & (w.addr.vld & ~w.addr.flush) | (~r_index_o.vld & da_r.addr.rd & r_meta_din.rd), "w_addr_en"),
+                # dissable input write data if no wirite transaction is pending or will be pending
+                w.data: write_pending | (w.addr.vld & (~w.addr.flush | ~r_index_o.vld)),
+            },
+        )
+        w_disp_node.sync()
+        w_disp_ack = rename_signal(self, w_disp_node.ack(), "w_disp_ack")
+        If(w_disp_ack,
+            If(w_index_o.word_index._eq(0),
+                # completly new item
+                w_index_i.item_index(w.addr.addr),
+                w_index_i.flushing(w.addr.flush),
+                w_index_i.word_index(1),
+                w_index_i.vld(w.addr.vld),  # start of a new
+            ).Elif(w_index_o.word_index._eq(WORD_INDEX_MAX),
+                # last item
+                w_index_i.word_index(0),
+                w_index_i.vld(0),  # just finished
+            ).Else(
+                # pass next word of item
+                w_index_i.word_index(w_index_o.word_index + 1),
+                w_index_i(w_index_o, exclude=[w_index_i.word_index]),
+            )
+        )
+
+        If(write_pending,
+           da_w.addr(Concat(w_index_o.item_index, w_index_o.word_index))
+        ).Else(
+           da_w.addr(Concat(w.addr.addr, w_index_o.word_index._dtype.from_py(0)))
+        )
+        da_w.mask(w.data.strb)
+        da_w.data(w.data.data)
+
+    def _impl(self):
         r_index = self._reg("r_index",
             HStruct(
-                (Bits(self.ADDR_WIDTH), "index_prefix"),
-                (Bits(log2ceil(self.ITEM_WORDS)), "index_suffix"),
+                (Bits(self.ADDR_WIDTH), "item_index"),
+                (Bits(log2ceil(self.ITEM_WORDS)), "word_index"),
                 (BIT, "vld"),
             ),
             def_val={
                 "vld": 0,
-                "index_suffix": INDEX_SUFFIX_MAX
+                "word_index": 0
             }
         )
         w_index = self._reg("w_index",
@@ -176,155 +369,21 @@ class RamTransactional(Unit):
             ),
             def_val={
                 "vld": 0,
-                "index_suffix": INDEX_SUFFIX_MAX
+                "word_index": 0
             }
         )
 
         r_index_i, r_index_o = r_index, r_index
         w_index_i, w_index_o = w_index, w_index
-
-        # r_index_i, r_index_o = r_index.dataIn.data, r_index.dataOut.data
-        # w_index_i, w_index_o = w_index.dataIn.data, w_index.dataOut.data
-
-        r_meta = HandshakedReg(HsStructIntf)
-        r_meta.T = HStruct(
-            *([(Bits(self.ID_WIDTH), "id")] if self.ID_WIDTH else []),
-            (BIT, "flushing"),
-            (BIT, "is_last"),
-            (BIT, "is_first"),
-        )
-        self.r_meta = r_meta
-        r_meta_i, r_meta_o = r_meta.dataIn.data, r_meta.dataOut.data
-        r, w = self.r, self.w
+        w = self.w
         flush_req = w.addr.vld & w.addr.flush
-
         read_pending = r_index.vld
-        read_begin = ~read_pending | r_index_o.index_suffix._eq(INDEX_SUFFIX_MAX)
-        r_disp_node = StreamNode(
-            [r.addr],
-            [da_r.addr,
-             r_meta.dataIn,  # r_index.dataIn
-            ],
-            skipWhen={
-                r.addr: flush_req | (read_pending & (r_index_o.index_suffix != INDEX_SUFFIX_MAX)),
-            },
-            extraConds={
-                r.addr:~flush_req | read_begin,
-            }
-        )
-        r_disp_node.sync()
-        If(read_begin,
-            da_r.addr.data(Concat(r.addr.addr, r_index_o.index_suffix._dtype.from_py(0)))
-        ).Else(
-            da_r.addr.data(Concat(r_index_o.index_prefix, r_index_o.index_suffix))
-        )
 
-        flush_pending = w_index.vld & w_index_o.flushing
-        # begin of entirely new read
-        If(flush_pending,
-            r_meta_i.id(r_meta_o.id) if self.ID_WIDTH else [],
-            r_meta_i.flushing(1),
-            r_meta_i.is_last(w_index_o.index_suffix._eq(INDEX_SUFFIX_MAX)),
-            r_meta_i.is_first(0),
-        ).Elif(flush_req & ~read_pending,
-            r_meta_i.id(w.addr.id) if self.ID_WIDTH else [],
-            r_meta_i.is_first(1),
-            r_meta_i.flushing(1),
-            r_meta_i.is_last(0),
-        ).Else(
-            If(read_pending,
-               r_meta_i.id(r_meta_o.id) if self.ID_WIDTH else [],
-               r_meta_i.is_first(0),
-               r_meta_i.is_last(r_index_o.index_suffix._eq(INDEX_SUFFIX_MAX)),
-            ).Else(
-               r_meta_i.id(r.addr.id) if self.ID_WIDTH else [],
-               r_meta_i.is_last(0),
-               r_meta_i.is_first(1),
-            ),
-            r_meta_i.flushing(0),
-        )
-
-        If(r_disp_node.ack(),
-            If(r_index_o.index_suffix != INDEX_SUFFIX_MAX,
-                # pass next word of item
-                r_index_i.index_suffix(r_index_o.index_suffix + 1),
-                r_index_i(r_index_o, exclude=[r_index_i.index_suffix]),
-            ).Else(
-                # last item or completly new item
-                If(w.addr.vld & w.addr.flush,
-                    r_index_i.index_prefix(w.addr.addr),
-                    r_index_i.vld(1),
-                ).Else(
-                    r_index_i.index_prefix(r.addr.addr),
-                    r_index_i.vld(r.addr.vld),
-                ),
-                r_index_i.index_suffix(1),
-            )
-        )
-
-        flush_data = self.flush_data
-        StreamNode(
-            [da_r.data, r_meta.dataOut],
-            [r.data, flush_data.addr, flush_data.data],
-            skipWhen={
-                r.data: r_meta.dataOut.vld & r_meta_o.flushing,
-                flush_data.data: r_meta.dataOut.vld & ~r_meta_o.flushing,
-                flush_data.addr: r_meta.dataOut.vld & (~r_meta_o.flushing | ~r_meta_o.is_first),
-            },
-            extraConds={
-                r.data:~r_meta_o.flushing,
-                flush_data.data: r_meta_o.flushing,
-                flush_data.addr: r_meta_o.flushing & r_meta_o.is_first,
-            },
-        ).sync()
-        flush_data.addr.addr(w_index_o.index_prefix)
-        if self.ID_WIDTH:
-            flush_data.addr.id(r_meta_o.id)
-        flush_data.addr.flush(1)
-        flush_data.data.strb(mask(flush_data.data.strb._dtype.bit_length()))
-        flush_data.data.data(da_r.data.data)
-        flush_data.data.last(r_meta_o.is_last)
-
-        if self.ID_WIDTH:
-            r.data.id(r_meta_o.id)
-        r.data.data(da_r.data.data)
-        r.data.last(r_meta_o.is_last)
-
-        write_pending = w_index.vld
-        w_disp_node = StreamNode(
-            [w.addr, w.data],
-            [da_w,  # w_index.dataIn
-             ],
-            skipWhen={
-                w.addr: write_pending,  # dissable address input if finishing peding transaction
-            },
-            extraConds={
-                w.addr:~w.addr.flush | (da_r.addr.rd & r_meta.dataIn.rd)  # stall if pending read and flush required
-            },
-        )
-        w_disp_node.sync()
-
-        If(w_disp_node.ack(),
-            If(r_index_o.index_suffix != INDEX_SUFFIX_MAX,
-                # pass next word of item
-                w_index_i.index_suffix(w_index_o.index_suffix + 1),
-                w_index_i(w_index_o, exclude=[w_index_i.index_suffix]),
-            ).Else(
-                # completly new item
-                w_index_i.index_prefix(w.addr.addr),
-                w_index_i.flushing(w.addr.flush),
-                w_index_i.index_suffix(1),
-                w_index_i.vld(w.addr.vld),
-            )
-        )
-
-        If(~write_pending,
-           da_w.addr(Concat(w.addr.addr, w_index_o.index_suffix._dtype.from_py(0)))
-        ).Else(
-           da_w.addr(Concat(w_index_o.index_prefix, w_index_o.index_suffix))
-        )
-        da_w.mask(w.data.strb)
-        da_w.data(w.data.data)
+        da_r, da_w = self.construct_ram_io()
+        r_meta = self.construct_r_meta(flush_req, read_pending, self.r, self.w, w_index, r_index_o)
+        self.construct_read_part(self.r, da_r, r_meta, flush_req, read_pending,
+                                 r_index_o, r_index_i, w_index_o, self.flush_data)
+        self.construct_write_part(w, da_w, w_index_i, w_index_o, r_index_o, da_r, r_meta[0].dataIn)
 
         propagateClkRstn(self)
 
--- hwtLib/amba/axi_comp/cache/ramTransactional_test.py
+++ hwtLib/amba/axi_comp/cache/ramTransactional_test.py
@@ -1,47 +1,137 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from hwt.hdl.constants import NOP
+from hwt.interfaces.utils import propagateClkRstn
 from hwt.simulator.simTestCase import SimTestCase
 from hwtLib.amba.axi_comp.cache.ramTransactional import RamTransactional
+from hwtLib.mem.sim.segmentedArrayProxy import SegmentedArrayProxy
 from hwtSimApi.constants import CLK_PERIOD
 from pyMathBitPrecise.bit_utils import mask
+
+
+class RamTransactionalWrap(RamTransactional):
+
+    def _declr(self):
+        RamTransactional._declr_io(self)
+        with self._paramsShared():
+            self.core = RamTransactional()
+
+    def _impl(self):
+        c = self.core
+        c.r(self.r)
+        c.w(self.w)
+        self.flush_data(c.flush_data)
+        propagateClkRstn(self)
 
 
 class RamTransactionalTC(SimTestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.u = u = RamTransactional()
+        cls.u = u = RamTransactionalWrap()
         u.ID_WIDTH = 2
         u.DATA_WIDTH = 32
         u.ADDR_WIDTH = 16
         u.WORDS_WIDTH = 64
         u.ITEMS = 32
         u.MAX_BLOCK_DATA_WIDTH = 8
-
+        cls.BURST_LEN = u.WORDS_WIDTH // u.DATA_WIDTH
         cls.compileSim(u)
 
-    def test_basic(self):
+    def setUp(self):
+        SimTestCase.setUp(self)
+        m = self.rtl_simulator.model
         u = self.u
-        TEST_LEN = u.ITEMS
-        RAM_WORDS = u.WORDS_WIDTH // u.DATA_WIDTH
-        # Skip write phase
-        u.r.addr._ag.data.extend([NOP for i in range(0, 10)])
-        # Read during writing/flushing -> delays it after write
-        u.r.addr._ag.data.extend([(0, i) for i in range(0, 10)])
-        testAddr = [(0, i, 0) for i in range(0, TEST_LEN)]
-        testInitData = [(i, mask(u.DATA_WIDTH // 8), int((i + 1) % RAM_WORDS == 0)) for i in range(0, TEST_LEN * RAM_WORDS)]
-        u.w.addr._ag.data.extend(testAddr)
-        u.w.data._ag.data.extend(testInitData)
-        testSecondAddr = [(0, i, 1) for i in range(0, TEST_LEN)]
-        testSecondData = [(i, mask(u.DATA_WIDTH // 8), int((i + 1) % RAM_WORDS == 0)) for i in reversed(range(0, TEST_LEN * RAM_WORDS))]
-        u.w.addr._ag.data.extend(testSecondAddr)
-        u.w.data._ag.data.extend(testSecondAddr)
+
+        self.DATA = SegmentedArrayProxy([
+                getattr(m.core_inst.data_array_inst, f"children_{i:d}_inst").io.ram_memory
+                for i in range(len(u.core.data_array.children))
+            ],
+            words_per_item=self.BURST_LEN
+        )
+
+    def test_nop(self):
+        u = self.u
+        self.runSim(20 * CLK_PERIOD)
+        ae = self.assertEmpty
+        ae(u.r.addr._ag.data)
+        ae(u.r.data._ag.data)
+        ae(u.w.addr._ag.data)
+        ae(u.w.data._ag.data)
+        ae(u.flush_data.addr._ag.data)
+        ae(u.flush_data.data._ag.data)
+
+    def test_read(self, TEST_LEN=10, MAGIC=5):
+        u = self.u
+        BURST_LEN = self.BURST_LEN
+        self.DATA.clean()
+        for i in range(TEST_LEN):
+            self.DATA[i] = ((i * 2 + 1 + MAGIC) << u.DATA_WIDTH) | (i * 2 + MAGIC)
+
+        u.r.addr._ag.data.extend(
+            # (id, addr)
+            [(0, i) for i in range(TEST_LEN)])
+
         self.runSim((TEST_LEN + 20) * CLK_PERIOD)
-        self.assertValSequenceEqual(u.r.data._ag.data, testSecondData, "Read data after flush mismatch")
-        self.assertValSequenceEqual(u.flush_data.addr._ag.data, testAddr, "Flush addr mismatch")
-        self.assertValSequenceEqual(u.flush_data.data._ag.data, testInitData, "Flush data mismatch")
+
+        r_expected = [(0, i + MAGIC, int((i + 1) % BURST_LEN == 0))
+                      for i in range(TEST_LEN * BURST_LEN)]
+        self.assertValSequenceEqual(u.r.data._ag.data, r_expected)
+
+        ae = self.assertEmpty
+        ae(u.r.addr._ag.data)
+        ae(u.w.addr._ag.data)
+        ae(u.w.data._ag.data)
+        ae(u.flush_data.addr._ag.data)
+        ae(u.flush_data.data._ag.data)
+
+
+    def test_read_write_flush(self, TEST_LEN=3):
+        u = self.u
+        BURST_LEN = self.BURST_LEN
+        # def proc():
+        #    yield Timer(int(CLK_PERIOD * 0.5))
+        #    for i in [u.w, u.flush_data, u.r]:
+        #        i._ag.setEnable(False)
+        #    yield Timer(int(CLK_PERIOD * 0.3))
+        #    for i in [u.w, u.flush_data, u.r]:
+        #        i._ag.setEnable(True)
+        #
+        # self.procs.append(proc())
+
+        # u.r.addr._ag.data.extend(
+        #    # Skip write phase
+        #    [NOP for _ in range(10)] +
+        #    # Read during writing/flushing -> delays it after write
+        #    [(0, i) for i in range(10)])
+        # write_non_flushing_addr = [
+        #    (0, i, 0)
+        #    for i in range(TEST_LEN)
+        # ]
+        write_non_flushing_data = [
+            (i, mask(u.DATA_WIDTH // 8), int((i + 1) % BURST_LEN == 0))
+            for i in range(TEST_LEN * BURST_LEN)
+        ]
+        # u.w.addr._ag.data.extend(write_non_flushing_addr)
+        # u.w.data._ag.data.extend(write_non_flushing_data)
+        write_flushing_addr = [
+            (0, i, 1)
+            for i in range(TEST_LEN)
+        ]
+        write_flushing_data = [
+            (i, mask(u.DATA_WIDTH // 8), int((i + 1) % BURST_LEN == 0))
+            for i in range(TEST_LEN * BURST_LEN)
+        ]
+        u.w.addr._ag.data.extend(write_flushing_addr)
+        u.w.data._ag.data.extend(write_flushing_data)
+
+        self.runSim((TEST_LEN + 20) * CLK_PERIOD)
+
+        ae = self.assertValSequenceEqual
+        ae(u.r.data._ag.data, write_flushing_data, "Read data after flush mismatch")
+        ae(u.flush_data.addr._ag.data, write_flushing_addr, "Flush addr mismatch")
+        original_data = write_non_flushing_data
+        ae(u.flush_data.data._ag.data, original_data, "Flush data mismatch")
 
 
 RamTransactionalTCs = [
