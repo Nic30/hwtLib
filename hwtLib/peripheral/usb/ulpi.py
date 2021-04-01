bb515df727d4ea54f308a76861fbd15aacc61b9b
Nic30 <nic30@seznam.cz>
2021-04-01 11:46:17 +0200

    ULPI: more clock domain automatization friendly definition

--- hwtLib/peripheral/usb/ulpi.py
+++ hwtLib/peripheral/usb/ulpi.py
@@ -1,14 +1,188 @@
+from hwt.code import Concat
+from hwt.hdl.types.bits import Bits
 from hwt.interfaces.std import Signal, Clk, Rst
 from hwt.interfaces.tristate import TristateSig
 from hwt.synthesizer.interface import Interface
 from hwt.synthesizer.param import Param
 from ipCorePackager.constants import DIRECTION
 from ipCorePackager.intfIpMeta import IntfIpMeta
+from hwt.hdl.types.struct import HStruct
+from hwt.hdl.types.defs import BIT
+from hwtLib.peripheral.usb.utmi import utmi_function_control_t, \
+    utmi_interface_control_t, utmi_otg_control_t, utmi_interrupt_t
+
+
+class ULPI_TX_CMD():
+    NOOP = 0b00_000000
+    """
+    :cvar NOOP: Transmit USB data that does not have a PID, such as
+        chirp and resume signalling. The PHY starts
+        transmitting on the USB beginning with the next data
+        byte.
+    """
+    NOPID = 0b01_000000
+
+    """
+    :cvar PID: Transmit USB packet. data(3:0) indicates USB
+        packet identifier PID(3:0).
+    """
+
+    @staticmethod
+    def PID(pid):
+        if pid is None or isinstance(pid, int):
+            pid = Bits(4).from_py(pid)
+        else:
+            assert pid._dtype.bit_length() == 4
+        return Concat(Bits(4).from_py(0b01_00), pid)
+
+    """
+    :cvar REGW: Register write command with 6-bit immediate
+        address.
+    """
+
+    @staticmethod
+    def REGW(addr):
+        if addr is None or isinstance(addr, int):
+            addr = Bits(6).from_py(addr)
+        else:
+            assert addr._dtype.bit_length() == 6
+        return Concat(Bits(2).from_py(0b10), addr)
+
+    """
+    :cvar Extended reqister write command. 8-bit address
+        available in the next cycle.
+    """
+    EXTW = 0b10_101111
+
+    """
+    :cvar REGR: Register read command with 6-bit immediate
+        address.
+    """
+
+    @staticmethod
+    def REGR(addr):
+        if addr is None or isinstance(addr, int):
+            addr = Bits(6).from_py(addr)
+        else:
+            assert addr._dtype.bit_length() == 6
+        return Concat(Bits(2).from_py(0b11), addr)
+
+    """
+    :cvar EXTR: Extended register read command. 8-bit address
+        available in the next cycle.
+    """
+    EXTR = 0b11_101111
+
+# class ULPI_RX_CMD():
+#
+#    def build(self, LineState, vBusState, RxEvent, ID, alt_int):
+
+
+class ULPI_REG:
+    """
+    :note: all registers are 8b in size
+    """
+    Vendor_ID_Low = 0x00
+    Vendor_ID_High = 0x01
+    Product_ID_Low = 0x02
+    Product_ID_High = 0x03
+
+    Function_Control = 0x04
+    Interface_Control = 0x07
+    OTG_Control = 0x0A
+    USB_Interrupt_Enable_Rising = 0x0D
+    USB_Interrupt_Enable_Falling = 0x10
+    USB_Interrupt_Status = 0x13
+    USB_Interrupt_Latch = 0x14
+    Debug = 0x15
+    Scratch_Register = 0x16
+    # optional registers
+    Carkit_Control = 0x19
+    Carkit_Interrupt_Delay = 0x1C
+    Carkit_Interrupt_Enable = 0x1D
+    Carkit_Interrupt_Status = 0x20
+    Carkit_Interrupt_Latch = 0x21
+    Carkit_Pulse_Control = 0x22
+    Transmit_Positive_Width = 0x25
+    Transmit_Negative_Width = 0x26
+    Receive_Polarity_Recovery = 0x27
+
+    REGS_WIT_SET_AND_CLR = [
+        Function_Control,
+        Interface_Control,
+        OTG_Control,
+        USB_Interrupt_Enable_Rising,
+        USB_Interrupt_Enable_Falling,
+        Scratch_Register,
+        Carkit_Control,
+        Carkit_Interrupt_Enable,
+        Carkit_Pulse_Control,
+    ]
+
+    @classmethod
+    def set_of(addr):
+        """
+        By write to this address the pattern on the data bus
+        is OR’d with and written into the register.
+        """
+        assert addr in ULPI_REG.REGS_WIT_SET_AND_CLR, addr
+        return addr + 1
+
+    def clr_of(self, addr):
+        """
+        By write to this address the pattern on the data bus
+        is a mask. If a bit in the mask is set, then
+        the corresponding register bit will be set to zero (cleared).
+        """
+        assert addr in ULPI_REG.REGS_WIT_SET_AND_CLR, addr
+        return addr + 2
+
+
+ulpi_reg_function_control_t = utmi_function_control_t
+ulpi_reg_function_control_t_reset_default = {
+    "XcvrSelect": 1,
+    "TermSelect": 0,
+    "OpMode": 0,
+    "Reset": 0,
+    "SuspendM": 1,
+}
+
+ulpi_reg_interface_control_t = utmi_interface_control_t
+
+ulpi_reg_otg_control_t = utmi_otg_control_t
+ulpi_reg_otg_control_t_reset_defaults = {
+    "IdPullup": 0,
+    "DpPulldown": 1,
+    "DmPulldown": 1,
+    "DischrgVbus": 0,
+    "ChrgVbus": 0,
+    "DrvVbus": 0,
+    "DrvVbusExternal": 0,
+    "UseExternalVbusIndicator": 0,
+}
+
+ulpi_reg_usb_interrupt_enable_rising_t = \
+ulpi_reg_usb_interrupt_enable_falling_t = \
+ulpi_reg_usb_interrupt_status_t = \
+ulpi_reg_usb_interrupt_latch_t = utmi_interrupt_t
+ulpi_reg_usb_interrupt_status_t_reset_default = {
+    "HostDisconnect": 0,
+    "VbusValid": 0,
+    "SessValid": 0,
+    "SessEnd": 0,
+    "IdGnd": 0,
+}
+
+ulpi_reg_debug_t = HStruct(
+    (BIT, "LineState0"),
+    (BIT, "LineState1"),
+    (Bits(6), None),
+)
 
 
 class Ulpi(Interface):
     """
-    Interface for USB2.0 PHY
+    ULPI (UTMI+ Low Pin Interface for USB2.0 PHY)
 
     * https://www.sparkfun.com/datasheets/Components/SMD/ULPI_v1_1.pdf
 
@@ -36,6 +210,9 @@ class Ulpi(Interface):
         in the following clock cycle. When the PHY is sending data to the Link,
         nxt indicates when a new byte is available for the Link to consume.
 
+
+    :note: PHY is the master
+
     .. hwt-autodoc::
     """
 
@@ -47,13 +224,12 @@ class Ulpi(Interface):
         self.DATA_WIDTH = Param(8)
 
     def _declr(self):
-        self.clk = Clk()
-        self.rst = Rst()
-        with self._associated(clk=self.clk, rst=self.rst):
-            self.data = TristateSig()
-            self.dir = Signal()
-            self.stop = Signal(masterDir=DIRECTION.IN)
-            self.next = Signal()
+        # the "t" has to be driven from outside of PHY (from Link usually implemented in FPGA)
+        self.data = TristateSig(masterDir=DIRECTION.IN)
+        self.data.DATA_WIDTH = self.DATA_WIDTH
+        self.dir = Signal()
+        self.stp = Signal(masterDir=DIRECTION.IN)
+        self.nxt = Signal()
 
     def _getIpCoreIntfClass(self):
         return IP_Ulpi
@@ -68,11 +244,11 @@ class IP_Ulpi(IntfIpMeta):
         self.vendor = "xilinx.com"
         self.library = "interface"
         self.map = {
-            'clk': "CLK",
-            'rst': "RST",
+            #'clk': "CLK", # [todo] need to reference associated clk/rst
+            #'rst': "RST",
             'dir': 'DIR',
-            "next": 'NEXT',
-            'stop': "STOP",
+            "nxt": 'NEXT',
+            'stp': "STOP",
             'data': {
                 'i': 'DATA_I',
                 'o': 'DATA_O',
