e0688f2ea8b31d5ddc1a0508c4c8430fe9b003c1
Nic30 <nic30@seznam.cz>
2021-05-21 14:55:42 +0200

    types.net.tcp

---
+++ hwtLib/types/net/tcp.py
@@ -0,0 +1,29 @@
+from hwt.hdl.types.bits import Bits
+from hwt.hdl.types.defs import BIT
+from hwt.hdl.types.struct import HStruct
+from hwtLib.types.ctypes import uint16_t, uint32_t, uint8_t
+from hwtLib.types.net.ip import l4port_t
+
+TCP_flags_t = HStruct(
+    (BIT, "cwr"),
+    (BIT, "ece"),
+    (BIT, "urg"),
+    (BIT, "ack"),
+    (BIT, "psh"),
+    (BIT, "rst"),
+    (BIT, "syn"),
+    (BIT, "fin"),
+)
+
+TCP_header_t = HStruct(
+    (l4port_t, "srcp"), (l4port_t, "dstp"),
+    (uint32_t, "seq"),
+    (uint32_t, "ack_seq"),
+    (Bits(4), "doff"),
+    (Bits(4), "reserved1"),
+    (uint8_t, "flags")
+    (uint16_t, "window"), (Bits(16), "checksum"),
+    (uint16_t, "urg_ptr"),
+    name="TCP_header_t"
+)
+
