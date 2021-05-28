2d7168430c280fc3c9eca9697fbfd08884053f74
Nic30 <nic30@seznam.cz>
2021-05-28 13:43:45 +0200

    CodeBlock: additional examples/tests

--- hwtLib/examples/statements/codeBlockStm.py
+++ hwtLib/examples/statements/codeBlockStm.py
@@ -3,6 +3,7 @@
 
 from hwt.code import If, CodeBlock
 from hwt.interfaces.std import Signal
+from hwt.interfaces.utils import addClkRstn
 from hwt.synthesizer.unit import Unit
 
 
@@ -47,13 +48,44 @@ class BlockStm_complete_override2(BlockStm_complete_override0):
             self.c(self.a),
         )
 
+class BlockStm_nop_val_optimized_out(BlockStm_complete_override0):
+
+    def _declr(self):
+        addClkRstn(self)
+        BlockStm_complete_override0._declr(self)
+
+    def _impl(self):
+        r = self._reg("r")
+        CodeBlock(
+            If(self.b,
+               r(self.a),
+            ),
+            self.c(self.a),
+        )
+
+class BlockStm_nop_val(BlockStm_nop_val_optimized_out):
+
+    def _declr(self):
+        BlockStm_nop_val_optimized_out._declr(self)
+        self.c1 = Signal()._m()
+
+    def _impl(self):
+        r = self._reg("r")
+        CodeBlock(
+            If(self.b,
+               r(self.a),
+            ),
+            self.c(self.a),
+        )
+        self.c1(r)
+
 
 if __name__ == "__main__":
     from hwt.synthesizer.utils import to_rtl_str
     # u = BlockStm_complete_override0()
     # print(to_rtl_str(u))
-    u = BlockStm_complete_override1()
+    u = BlockStm_nop_val()
     print(to_rtl_str(u))
-    #u = BlockStm_complete_override2()
-    #print(to_rtl_str(u))
+    # u = BlockStm_complete_override2()
+    # print(to_rtl_str(u))
 
--- hwtLib/examples/statements/codeBlockStm_test.py
+++ hwtLib/examples/statements/codeBlockStm_test.py
@@ -1,8 +1,9 @@
 from hwt.simulator.simTestCase import SimTestCase
 from hwtLib.examples.base_serialization_TC import BaseSerializationTC
-from hwtSimApi.constants import CLK_PERIOD
 from hwtLib.examples.statements.codeBlockStm import BlockStm_complete_override0, \
-    BlockStm_complete_override1, BlockStm_complete_override2
+    BlockStm_complete_override1, BlockStm_complete_override2, \
+    BlockStm_nop_val_optimized_out, BlockStm_nop_val
+from hwtSimApi.constants import CLK_PERIOD
 
 
 class CodeBlokStmTC(SimTestCase, BaseSerializationTC):
@@ -65,6 +66,50 @@ class CodeBlokStmTC(SimTestCase, BaseSerializationTC):
         self.runSim(len(c) * CLK_PERIOD)
 
         self.assertValSequenceEqual(u.c._ag.data, c)
+
+    def test_BlockStm_nop_val_optimized_out(self):
+        u = BlockStm_nop_val_optimized_out()
+        self.compileSimAndStart(u)
+
+        a = u.a._ag.data
+        b = u.b._ag.data
+        c = []
+        for _a in [0, 1, None]:
+            for _b in [0, 1, None]:
+                _c = _a
+                a.append(_a)
+                b.append(_b)
+                c.append(_c)
+
+        self.runSim((len(c) + 1) * CLK_PERIOD)
+
+        self.assertValSequenceEqual(u.c._ag.data, c)
+
+    def test_BlockStm_nop(self):
+        u = BlockStm_nop_val()
+        self.compileSimAndStart(u)
+
+        a = u.a._ag.data
+        b = u.b._ag.data
+        c = []
+        c1 = []
+        _c1 = None
+        for _a in [1, 1, 0, 1, 0, 1, 1, None]:
+            for _b in [0, 1, None]:
+                _c = _a
+                a.append(_a)
+                b.append(_b)
+                c.append(_c)
+                c1.append(_c1)
+                if _b:
+                    _c1 = _a
+                elif _b is None:
+                    _c1 = None
+
+        self.runSim((len(c) + 1) * CLK_PERIOD)
+
+        self.assertValSequenceEqual(u.c._ag.data, c)
+        self.assertValSequenceEqual(u.c1._ag.data, c1)
 
 
 if __name__ == "__main__":
