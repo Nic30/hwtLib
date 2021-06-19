f881cad456df7917028bfbae767a9fbeb357261d
Nic30 <nic30@seznam.cz>
2021-06-19 15:32:51 +0200

    update verilog TmpVarExample2

--- hwtLib/tests/serialization/TmpVarExample2.v
+++ hwtLib/tests/serialization/TmpVarExample2.v
@@ -4,9 +4,11 @@ module TmpVarExample2 (
 );
     always @(a) begin: assig_process_b
         reg[1:0] tmp_concat_1;
+        reg[1:0] tmp_index_0;
         reg[1:0] tmp_concat_0;
         tmp_concat_1 = {a[31:16] == 16'h0001, a[15:0] == 16'h0001};
-        tmp_concat_0 = {(tmp_concat_1 + 2'b01)[0], (tmp_concat_1 + 2'b01)[1]};
+        tmp_index_0 = tmp_concat_1 + 2'b01;
+        tmp_concat_0 = {tmp_index_0[0], tmp_index_0[1]};
         if (tmp_concat_0[0] == 1'b0 & tmp_concat_0[1] == 1'b0)
             b = 32'h00000000;
         else
