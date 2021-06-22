36d11eab382ae2a8df924d41a6b2248ceca26a90
Nic30 <nic30@seznam.cz>
2021-06-22 09:03:25 +0200

    Usb2SieDeviceTx: simplify the wait for crc16 send

--- hwtLib/peripheral/usb/usb2/sie_tx.py
+++ hwtLib/peripheral/usb/usb2/sie_tx.py
@@ -99,10 +99,10 @@ class Usb2SieDeviceTx(Unit):
         self.deparser_hs = deparser_hs
         self.deparser_data = deparser_data
 
-        tx_cmd: Usb2SieDeviceTxInput = AxiSBuilder(self, self.tx_cmd).buff(1).end
+        tx_cmd: Usb2SieDeviceTxInput = AxiSBuilder(self, self.tx_cmd).buff(items=1).end
         pid_is_hs = rename_signal(self, tx_cmd.valid & ~tx_cmd.chirp & USB_PID.is_hs(tx_cmd.pid), "pid_is_hs")
         pid_has_data = rename_signal(self, tx_cmd.valid & ~tx_cmd.chirp & USB_PID.is_data(tx_cmd.pid), "pid_has_data")
-        pid_has_data_non_zero_len = rename_signal(self, pid_has_data & (tx_cmd.keep != 0), "pid_has_data_non_zero_len")
+        # pid_has_data_non_zero_len = rename_signal(self, pid_has_data & (tx_cmd.keep != 0), "pid_has_data_non_zero_len")
         pid_has_data_zero_len = rename_signal(self, pid_has_data & tx_cmd.keep._eq(0) & tx_cmd.last, "pid_has_data_zero_len")
 
         for d in [deparser_hs, deparser_data]:
@@ -111,38 +111,37 @@ class Usb2SieDeviceTx(Unit):
         is_sof_of_command = self._reg("is_sof_of_command", def_val=1)
         If(~self.enable | (tx_cmd.ready & tx_cmd.valid & tx_cmd.last),
            is_sof_of_command(1)
-        ).Elif(tx_cmd.ready & tx_cmd.valid,
+        ).Elif(tx_cmd.ready & tx_cmd.valid & ~tx_cmd.last,
            is_sof_of_command(0),
         )
+
+        crc16_valid = self._reg("crc16_valid", def_val=0)
         # select a frame depending on type of pacekt defined by PID and presence of tx data
         StreamNode(
             [],
             [deparser_hs.dataIn.pid, deparser_data.dataIn.pid],
             extraConds={
-                deparser_hs.dataIn.pid:pid_is_hs & is_sof_of_command,
-                deparser_data.dataIn.pid: pid_has_data & is_sof_of_command,
+                deparser_hs.dataIn.pid:pid_is_hs,
+                deparser_data.dataIn.pid: pid_has_data,
             },
             skipWhen={
                 deparser_hs.dataIn.pid: pid_has_data,
                 deparser_data.dataIn.pid: pid_is_hs,
             }
-        ).sync(tx_cmd.valid & self.enable)
+        ).sync(tx_cmd.valid & self.enable & is_sof_of_command & ~crc16_valid)
 
-        crc16_valid = self._reg("crc16_valid", def_val=0)
         payload_in: AxiStream = deparser_data.dataIn.data
         StreamNode(
             [], [payload_in],
-        ).sync(tx_cmd.valid & ~crc16_valid)
+        ).sync(pid_has_data & tx_cmd.valid & ~crc16_valid)
 
         # tx_cmd consume
         StreamNode(
             [tx_cmd], [],
-            extraConds={
-                tx_cmd: (pid_is_hs & deparser_hs.dataIn.pid.rd) |
-                        (pid_has_data_zero_len & payload_in.ready & ~crc16_valid) |
-                        (pid_has_data_non_zero_len & payload_in.ready & ~crc16_valid)
-            }
-        ).sync()
+        ).sync(~crc16_valid & (
+                        pid_is_hs & deparser_hs.dataIn.pid.rd |
+                        (pid_has_data & payload_in.ready)
+                    ))
         payload_in.data(tx_cmd.data)
         payload_in.last(tx_cmd.last)
         payload_in.keep(tx_cmd.keep)
@@ -158,14 +157,14 @@ class Usb2SieDeviceTx(Unit):
         crc16.dataIn.vld(payload_in.valid & payload_in.ready & (payload_in.keep != 0))
 
         end_of_payload = rename_signal(self, payload_in.valid & payload_in.ready & payload_in.last, "end_of_payload")
-        If(end_of_payload | pid_has_data_zero_len,
-           crc16_valid(~deparser_data.dataIn.crc16.rd),
+        If(~crc16_valid & pid_has_data & end_of_payload,
+           crc16_valid(1),
         ).Elif(deparser_data.dataIn.crc16.rd,
            crc16_valid(0)
         )
         # [TODO] check if reflection is done twice or not
         deparser_data.dataIn.crc16.data(Concat(*(b for b in crc16.dataOut)))
-        deparser_data.dataIn.crc16.vld(crc16_valid)
+        deparser_data.dataIn.crc16.vld(crc16_valid | pid_has_data_zero_len)
 
         axis_tx = AxiStream()
         axis_tx.DATA_WIDTH = tx_cmd.DATA_WIDTH
