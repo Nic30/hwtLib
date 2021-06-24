5359c8481bd50e288127aae30b31f234fc5f63a4
Nic30 <nic30@seznam.cz>
2021-06-24 08:46:30 +0200

    XilinxVivadoPlatform: add part as parameter

--- hwtLib/xilinx/platform.py
+++ hwtLib/xilinx/platform.py
@@ -1,5 +1,10 @@
+from typing import Optional
+
 from hwt.synthesizer.dummyPlatform import DummyPlatform
 
 
 class XilinxVivadoPlatform(DummyPlatform):
-    pass
+
+    def __init__(self, part: Optional[str]=None):
+        self.part = part
+        super(XilinxVivadoPlatform, self).__init__()
