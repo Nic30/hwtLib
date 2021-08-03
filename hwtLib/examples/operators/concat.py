fdefcd9052e0568d8f838e4e9076e8aaff5dc8ec
Nic30 <nic30@seznam.cz>
2021-08-03 15:20:57 +0200

    resolve_final_parts_from_splitpoints_and_parts: extra tests
    * an optimization pass which cuts the signals to a independent chunks
      to reduce false sharing

--- hwtLib/examples/operators/concat.py
+++ hwtLib/examples/operators/concat.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from hwt.code import Concat
-from hwt.interfaces.std import Signal, VectSignal
+from math import ceil
+
+from hwt.code import Concat, If, Switch
+from hwt.hdl.types.bits import Bits
+from hwt.interfaces.std import Signal, VectSignal, BramPort_withoutClk
+from hwt.interfaces.utils import addClkRstn
+from hwt.synthesizer.hObjList import HObjList
 from hwt.synthesizer.unit import Unit
+from hwtLib.types.net.arp import arp_ipv4_t
 
 
 class SimpleConcat(Unit):
     """
     .. hwt-autodoc::
     """
+
     def _declr(self):
         self.a0 = Signal()
         self.a1 = Signal()
@@ -22,6 +29,113 @@ class SimpleConcat(Unit):
         self.a_out(Concat(self.a3, self.a2, self.a1, self.a0))
 
 
+class ConcatAssign(Unit):
+    """
+    .. hwt-autodoc::
+    """
+
+    def _declr(self):
+        self.a0 = Signal()._m()
+        self.a1 = Signal()._m()
+        self.a2 = Signal()._m()
+        self.a3 = Signal()._m()
+
+        self.a_in = VectSignal(4)
+
+    def _impl(self):
+        # verilog like {a3, a2, a1, a0} = a_in
+        Concat(self.a3, self.a2, self.a1, self.a0)(self.a_in)
+
+
+class ConcatIndexAssignMix0(Unit):
+    """
+    .. hwt-autodoc::
+    """
+
+    def _declr(self):
+        self.a = HObjList([VectSignal(2), VectSignal(2)])
+        self.b = HObjList([Signal()._m() for _ in range(4)])
+
+    def _impl(self) -> None:
+        a = Concat(*reversed(self.a))
+        for i, b in enumerate(self.b):
+            b(a[i])
+
+
+class ConcatIndexAssignMix1(Unit):
+    """
+    .. hwt-autodoc::
+    """
+
+    def _declr(self):
+        self.a = HObjList([Signal() for _ in range(4)])
+        self.b = HObjList([VectSignal(2)._m(), VectSignal(2)._m()])
+
+    def _impl(self) -> None:
+        b = Concat(*reversed(self.b))
+        for i, a in enumerate(self.a):
+            b[i](a)
+
+
+class ConcatIndexAssignMix2(Unit):
+    """
+    .. hwt-autodoc::
+    """
+
+    def _declr(self):
+        self.a = HObjList([VectSignal(2), VectSignal(4), VectSignal(2)])
+        self.b = HObjList([VectSignal(4)._m(), VectSignal(4)._m()])
+
+    def _impl(self) -> None:
+        b = Concat(*reversed(self.b))
+        a0, a1, a2 = self.a
+        b[:6](a2)
+        b[6:2](a1)
+        b[2:](a0)
+
+
+class ConcatIndexAssignMix3(Unit):
+    """
+    .. hwt-autodoc::
+    """
+
+    def _declr(self):
+        addClkRstn(self)
+        self.port = BramPort_withoutClk()
+        self.port.DATA_WIDTH = 24
+
+    def _impl(self) -> None:
+        port = self.port
+        d = self._reg("d", arp_ipv4_t, def_val={f.name: i for i, f in enumerate(arp_ipv4_t.fields)})
+        d_flat = d._reinterpret_cast(Bits(d._dtype.bit_length()))
+        rd_cases = []
+        wr_cases = []
+        d_w = d_flat._dtype.bit_length()
+        for i in range(ceil(d_w / 24)):
+            start = i * 24
+            end = (i + 1) * 24
+            if end <= d_w:
+                out_src = d_flat[end:start]
+                in_dst_assig = out_src(port.din)
+            else:
+                out_src = Concat(Bits(end - d_w).from_py(0), d_flat[d_w:start])
+                in_dst_assig = d_flat[d_w:start](port.din[d_w - start:])
+
+            rd_cases.append((i, port.dout(out_src)))
+            wr_cases.append((i, in_dst_assig))
+
+        If(self.clk._onRisingEdge() & port.en,
+           Switch(port.addr)\
+            .add_cases(rd_cases)\
+            .Default(port.dout(None)),
+        )
+        If(port.en & port.we,
+            Switch(port.addr)\
+             .add_cases(wr_cases)
+        )
+
+
 if __name__ == "__main__":
     from hwt.synthesizer.utils import to_rtl_str
-    print(to_rtl_str(SimpleConcat))
+    u = ConcatIndexAssignMix3()
+    print(to_rtl_str(u))
--- hwtLib/examples/operators/concat_test.py
+++ hwtLib/examples/operators/concat_test.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from math import ceil
+from typing import Dict
 import unittest
 
-from hwt.hdl.constants import Time
+from hwt.code import Concat
+from hwt.hdl.constants import WRITE, READ, NOP
+from hwt.hdl.types.bits import Bits
 from hwt.simulator.simTestCase import SimTestCase
-from hwtLib.examples.operators.concat import SimpleConcat
-from pyMathBitPrecise.bit_utils import get_bit
+from hwtLib.examples.operators.concat import SimpleConcat, ConcatAssign, \
+    ConcatIndexAssignMix0, ConcatIndexAssignMix1, ConcatIndexAssignMix2, \
+    ConcatIndexAssignMix3
+from hwtLib.types.net.arp import arp_ipv4_t
+from hwtSimApi.constants import CLK_PERIOD
+from pyMathBitPrecise.bit_utils import get_bit, mask
 
 
 def addValues(unit, data):
@@ -24,19 +32,113 @@ def addValues(unit, data):
 
 class ConcatTC(SimTestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.u = SimpleConcat()
-        cls.compileSim(cls.u)
+    def tearDown(self):
+        self.rmSim()
+        SimTestCase.tearDown(self)
 
     def test_join(self):
-        u = self.u
+        u = SimpleConcat()
+        self.compileSimAndStart(u)
 
         # addValues(u, [0, 1, 2, 4, 8, (1 << 4) - 1, None, 3, 2, 1])
         addValues(u, [2, 4, (1 << 4) - 1, None, 3, 2, 1])
-        self.runSim(70 * Time.ns)
+        self.runSim(7 * CLK_PERIOD)
         self.assertValSequenceEqual(u.a_out._ag.data,
                                     [2, 4, 15, None, 3, 2, 1])
+
+    def test_assign(self):
+        u = ConcatAssign()
+        self.compileSimAndStart(u)
+        N = 4
+        a = [[] for _ in range(N)]
+        for i in range(N):
+            for i2, bit_ref_vals in enumerate(a):
+                bit_ref_vals.append(int(i == i2))
+            u.a_in._ag.data.append(1 << i)
+
+        self.runSim(N * CLK_PERIOD)
+        for i, bit_ref_vals in enumerate(a):
+            data = getattr(u, f'a{i:d}')._ag.data
+            self.assertValSequenceEqual(data, bit_ref_vals)
+
+    def test_ConcatIndexAssignMix0(self):
+        u = ConcatIndexAssignMix0()
+        self.compileSimAndStart(u)
+        N = 4
+        a = [[] for _ in range(N)]
+        for i in range(N):
+            for i2, bit_ref_vals in enumerate(a):
+                bit_ref_vals.append(int(i == i2))
+            v = 1 << i
+            u.a[0]._ag.data.append(v & 0x3)
+            u.a[1]._ag.data.append((v >> 2) & 0x3)
+
+        self.runSim(N * CLK_PERIOD)
+        for i, bit_ref_vals in enumerate(a):
+            data = u.b[i]._ag.data
+            self.assertValSequenceEqual(data, bit_ref_vals)
+
+    def test_ConcatIndexAssignMix1(self):
+        u = ConcatIndexAssignMix1()
+        self.compileSimAndStart(u)
+        N = 4
+        b = [[] for _ in range(2)]
+        for i in range(N):
+            for i2, a in enumerate(u.a):
+                a._ag.data.append(int(i == i2))
+            v = 1 << i
+            b[0].append(v & 0x3)
+            b[1].append((v >> 2) & 0x3)
+
+        self.runSim(N * CLK_PERIOD)
+        for i, ref_vals in enumerate(b):
+            self.assertValSequenceEqual(u.b[i]._ag.data, ref_vals)
+
+    def test_ConcatIndexAssignMix2(self):
+        u = ConcatIndexAssignMix2()
+        self.compileSimAndStart(u)
+        N = 8
+        b = [[] for _ in range(2)]
+        for i in range(N):
+            offset = 0
+            v = 1 << i
+            for a in u.a:
+                w = a._dtype.bit_length()
+                a._ag.data.append((v >> offset) & mask(w))
+                offset += w
+
+            b[0].append(v & 0xf)
+            b[1].append((v >> 4) & 0xf)
+
+        self.runSim(N * CLK_PERIOD)
+        for i, ref_vals in enumerate(b):
+            self.assertValSequenceEqual(u.b[i]._ag.data, ref_vals)
+
+    def _ConcatIndexAssignMix3_py_val_to_words(self, v:Dict[str, int], W: int, WORD_CNT: int):
+        d = arp_ipv4_t.from_py(v)
+        padding_w = WORD_CNT * 24 - W
+        d_words = Concat(Bits(padding_w).from_py(0), d._reinterpret_cast(Bits(W)))\
+            ._reinterpret_cast(Bits(24)[WORD_CNT])
+        return d_words
+
+    def test_ConcatIndexAssignMix3(self):
+        u = ConcatIndexAssignMix3()
+        self.compileSimAndStart(u)
+        W = arp_ipv4_t.bit_length()
+        WORD_CNT = ceil(W / 24)
+        D1 = self._ConcatIndexAssignMix3_py_val_to_words(
+            {f.name: i + 1 for i, f in enumerate(arp_ipv4_t.fields)}, W, WORD_CNT)
+        u.port._ag.requests.extend([
+            NOP, NOP,
+            *((READ, i) for i in range(WORD_CNT)),
+            *((WRITE, i, v) for i, v in enumerate(D1)),
+            *((READ, i) for i in range(WORD_CNT)),
+        ])
+        self.runSim((len(u.port._ag.requests) + 4) * CLK_PERIOD)
+        D0 = self._ConcatIndexAssignMix3_py_val_to_words(
+            {f.name: i for i, f in enumerate(arp_ipv4_t.fields)}, W, WORD_CNT)
+        r_data = list(u.port._ag.r_data)
+        self.assertValSequenceEqual(r_data[0:WORD_CNT] + r_data[2 * WORD_CNT:], [int(d) for d in (list(D0) + list(D1))])
 
 
 if __name__ == "__main__":
