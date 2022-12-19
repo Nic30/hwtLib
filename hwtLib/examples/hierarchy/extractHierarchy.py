2834ae86f49174bc00bfc64c977df9540181784f
Nic30 <nic30@seznam.cz>
2022-12-19 16:53:53 +0100

    ExtractedUnit: fix transfer of an expression

--- hwtLib/examples/hierarchy/extractHierarchy.py
+++ hwtLib/examples/hierarchy/extractHierarchy.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from collections import deque
-from typing import Union, Type, Optional, Sequence
+from itertools import chain
+from typing import Union, Optional, Sequence, Dict, Deque, List
 
 from hwt.hdl.operator import Operator
 from hwt.hdl.operatorDefs import EVENT_OPS
@@ -12,7 +13,7 @@ from hwt.hdl.value import HValue
 from hwt.interfaces.std import Clk, Rst, Rst_n, Signal
 from hwt.pyUtils.uniqList import UniqList
 from hwt.synthesizer.rtlLevel.netlist import RtlNetlist
-from hwt.synthesizer.rtlLevel.rtlSignal import RtlSignal, OperatorCaheKeyType
+from hwt.synthesizer.rtlLevel.rtlSignal import RtlSignal
 from hwt.synthesizer.rtlLevel.rtlSyncSignal import RtlSyncSignal
 from hwt.synthesizer.rtlLevel.signalUtils.exceptions import SignalDriverErr
 from hwt.synthesizer.unit import Unit
@@ -90,7 +91,7 @@ class ExtractedUnit(Unit):
 
             if iCloned is None:
                 iCloned = Signal(i._dtype)
-            name = AbstractComponentBuilder(self, None, "")._findSuitableName(i.name, firstWithoutCntr=True)
+            name = AbstractComponentBuilder(self, None, "")._findSuitableName(i.name, firstWithoutCntrSuffix=True)
             setattr(self, name, iCloned)
             assert i not in ioMap
             ioMap[i] = iCloned
@@ -98,16 +99,72 @@ class ExtractedUnit(Unit):
         for o in self._externOutputs:
             o: RtlSignal
             oCloned = Signal(o._dtype)._m()
-            name = AbstractComponentBuilder(self, None, "")._findSuitableName(o.name, firstWithoutCntr=True)
+            name = AbstractComponentBuilder(self, None, "")._findSuitableName(o.name, firstWithoutCntrSuffix=True)
             setattr(self, name, oCloned)
             assert o not in ioMap
             ioMap[o] = oCloned
 
-    def _moveOrCopyCircuitFromParent(self):
+    def _handleUpdateForOutput(self, o: RtlSignal,
+                               translation: Dict[RtlSignal, RtlSignal],
+                               interOuts: Dict[RtlSignal, RtlSignal],
+                               outerIoMap: Dict[RtlSignal, RtlSignal],
+                               toTranslate: Deque[Union[Operator, HdlStatement, HdlPortItem]],
+                               ):
+        priv = self._externObjsToExtract
+
+        translation[o] = o
+
+        outerIo = outerIoMap.get(o, None)
+        # drop operand cache items for expressions which will not be moved to this netlist
+        # Must replace o with outerIo in every use of o outside of this unit
+        assert o.ctx is self._ctx, o
+        for ep in tuple(o.endpoints):
+            if ep in priv:
+                toTranslate.append(ep)
+            else:
+                assert outerIo is not None, (o, ep)
+                if isinstance(ep, HdlStatement):
+                    ep._replace_input((o, outerIo))
+            
+                elif isinstance(ep, HdlPortItem):
+                    raise NotImplementedError(ep)
+            
+                else:
+                    assert isinstance(ep, Operator), ep
+                    if ep in priv:
+                        assert o not in ep.perands, (o, ep)
+                        assert ep.result.ctx is self._ctx, ep
+                    else:
+                        # replace operand of the operator node
+                        ep._replace_input(o, outerIo)
+        
+        toDrop = []
+        for k, v in o._usedOps.items():
+            if isinstance(v, HValue):
+                continue
+            v: RtlSignal
+            if v.ctx is self._ctx:
+                continue
+            try:
+                d = v.singleDriver()
+            except SignalDriverErr:
+                raise NotImplementedError()
+            # if d in priv:
+            #    # will be moved later, we can keep it
+            #    continue
+            toDrop.append(k)
+
+        for kToDrop in toDrop:
+            o._usedOps.pop(kToDrop)
+            o._usedOpsAlias.pop(kToDrop).remove(kToDrop)
+        
+        if outerIo is not None:
+            interOuts[o](o)  # drive output of this unit
+
+    def _moveOrCopyCircuitFromParent(self, translation: Dict[RtlSignal, RtlSignal], interOuts: Dict[RtlSignal, RtlSignal], outerIoMap: Dict[RtlSignal, RtlSignal]):
         # BFS move or copy circuit to this unit from parent
         priv = self._externObjsToExtract
-        translation = {i: self._ioMap[i]._sig for i in self._externInputs}
-        toTranslate = deque()
+        toTranslate: Deque[Union[Operator, HdlStatement, HdlPortItem]] = deque()
         for i in self._externInputs:
             i: RtlSignal
             toTranslate.extend(ep for ep in i.endpoints if ep in priv)
@@ -122,7 +179,7 @@ class ExtractedUnit(Unit):
 
             if isinstance(obj, HdlStatement):
                 stm: HdlStatement = obj
-                newInputs = []
+                newInputs: Optional[List[Union[RtlSignal, HValue]]] = []
                 for i in stm._inputs:
                     _i = translation.get(i, None)
                     if _i is None:
@@ -144,100 +201,106 @@ class ExtractedUnit(Unit):
                         stm._replace_input((i, newI))
                         
                 for o in stm._outputs:
+                    # move signal to a specified netlist
                     o.ctx.signals.remove(o)
                     o.ctx = self._ctx
                     o.ctx.signals.add(o)
-                    translation[o] = o
-                    toTranslate.extend(ep for ep in o.endpoints if ep in priv)
-        
+                    self._handleUpdateForOutput(o, translation, interOuts, outerIoMap, toTranslate)
+
                 ctx.statements.remove(stm)
                 self._ctx.statements.add(stm)
                 translation[stm] = stm
                 stm._clean_signal_meta()
 
             elif isinstance(obj, Operator):
-                # shouldCopy = obj.operator in EVENT_OPS
-                newOps = []
-                for op in obj.operands:
-                    if isinstance(op, RtlSignal):
-                        _op = translation.get(op)
-                        if _op is None:
-                            # some input not available yet, must postpone translation
-                            newOps = None
-                            break
+                shouldCopy = obj not in priv or obj.operator in EVENT_OPS
+                if shouldCopy:
+                    newOps = []
+                    for op in obj.operands:
+                        if isinstance(op, RtlSignal):
+                            _op = translation.get(op)
+                            if _op is None:
+                                # some input not available yet, must postpone translation
+                                newOps = None
+                                break
+                            else:
+                                assert _op.ctx is self._ctx
+                                newOps.append(_op)
                         else:
-                            assert _op.ctx is self._ctx
-                            newOps.append(_op)
-                    else:
-                        assert isinstance(op, HValue), op
-                        newOps.append(op)
+                            assert isinstance(op, HValue), op
+                            newOps.append(op)
+    
+                    if newOps is None:
+                        toTranslate.append(obj)
+                        continue
+                    # create a new copy of this object in this unit
+                    newSig = obj.operator._evalFn(*newOps)
+                    assert newSig.ctx is self._ctx, newSig
+                    assert obj.result.ctx is not self._ctx, obj.result
+                    
+                else:
+                    # move this object to his unit, move is done by translation of operand
+                    # and by move is result signal to the netlist of this unit
+                    # move signal to a specified netlist
+                    o = obj.result
+                    if o.ctx is not self._ctx:
+                        o.ctx.signals.remove(o)
+                        o.ctx = self._ctx
+                        o.ctx.signals.add(o)
+                    
+                    translated = True
+                    for op in tuple(obj.operands):
+                        if isinstance(op, RtlSignal):
+                            if op.ctx is self._ctx:
+                                continue
+                            _op = translation.get(op)
+                            if _op is None:
+                                # some input not available yet, must postpone translation
+                                translated = False
+                                break
+                            else:
+                                assert _op.ctx is self._ctx
+                                obj._replace_input(op, _op)
 
-                if newOps is None:
-                    toTranslate.append(obj)
-                    continue    
+                    if not translated:
+                        toTranslate.append(obj)
 
-                # if shouldCopy:
-                # create a new copy of this object in this unit
-                newSig = obj.operator._evalFn(*newOps)
-                # parentCtx = obj.result.ctx
-                # parentCtx.signals.remove(obj.result)
-                # self._ctx.signals.add(obj.result)
-                # obj.result.ctx = self._ctx
-                
+                    self._handleUpdateForOutput(o, translation, interOuts, outerIoMap, toTranslate)
+                    newSig = o
+
                 translation[obj.result] = newSig
                 translation[obj] = newSig.singleDriver()
                 toTranslate.extend(ep for ep in obj.result.endpoints if ep in priv)
         
-                # else:
-                #    # move this object to his unit, move is done by translation of operand
-                #    # and by move is result signal to the netlist of this unit
-                #    raise NotImplementedError(obj)
+                outerIo = outerIoMap.get(obj.result, None)
+                if outerIo is not None:
+                    outerIo(newSig)
                 
             else:
                 raise NotImplementedError(obj.__class__, obj)
 
-        return translation
-
     def _impl(self) -> None:
         # transfer circuit to subunit
         # re-connect signals which should be connected to IO of new subunit
-        translation = self._moveOrCopyCircuitFromParent()
-        for o in self._externOutputs:
-            self._ioMap[o](translation[o])
-    
-    def _signalsForSubUnitEntity(self, context: RtlNetlist, prefix: str):
-        super(ExtractedUnit, self)._signalsForSubUnitEntity(context, prefix)
+        translation = {i: self._ioMap[i]._sig for i in self._externInputs}
+        interOuts = {i: self._ioMap[i]._sig for i in self._externOutputs}
+        self._cleanAsSubunit()
+        super(ExtractedUnit, self)._signalsForSubUnitEntity(self._parent._ctx, "sig_" + self._name)
+        
+        outerIo = {i: self._ioMap[i]._sig for i in chain(self._externInputs, self._externOutputs)}
+        self._moveOrCopyCircuitFromParent(translation, interOuts, outerIo)
+
         # connect external input signal to a input of this unit
         for i in self._externInputs:
             self._ioMap[i](i)
+        self._outerIo = outerIo
         
-        priv = self._externObjsToExtract
-        # for each output to output of this unit
-        for o in self._externOutputs:
-            assert o.ctx is self._ctx, o
-
-            for ep in tuple(o.endpoints):
-                if ep in priv:
-                    continue
-
-                replacement = self._ioMap[o]._sig
-                assert replacement.ctx is not self._ctx
-                if isinstance(ep, HdlStatement):
-                    ep._replace_input((o, replacement))
-
-                elif isinstance(ep, HdlPortItem):
-                    raise NotImplementedError(ep)
-
-                else:
-                    assert isinstance(ep, Operator), ep
-                    # o.endpoints.remove(ep)
-                    if ep in priv:
-                        assert o not in ep.perands, (o, ep)
-                        assert ep.result.ctx is self._ctx, ep
-                    else:
-                        # replace operand of the operator node
-                        ep._replace_input(o, replacement)
-
+    def _signalsForSubUnitEntity(self, context: RtlNetlist, prefix: str):
+        assert context is self._parent._ctx, self
+        assert prefix == "sig_" + self._name, (prefix, self._name) 
+        for i in chain(self._externInputs, self._externOutputs):
+            self._ioMap[i]._sig = self._outerIo[i]
+    
 
 def extractNetlistPartToSubunit(
                         inputs: UniqList[RtlSignal],
@@ -284,5 +347,4 @@ def extractRegsToSubunit(regs: Sequence[RtlSyncSignal]) -> ExtractedUnit:
             outputs.extend(stm._outputs)
     
     return extractNetlistPartToSubunit(inputs, outputs, priv)
-
     
