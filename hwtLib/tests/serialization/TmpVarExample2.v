2948cd3f33b9843b2c5c10142248f3ea54d000b9
Nic30 <nic30@seznam.cz>
2024-04-10 10:20:02 +0200

    test(Serializer_tmpVar_TC): rm redundant vars from TmpVarExample2.v
    * variables were created from second concat
      but now the index on concat is directly resolved
      to input of concat

--- hwtLib/tests/serialization/TmpVarExample2.v
+++ hwtLib/tests/serialization/TmpVarExample2.v
@@ -4,14 +4,10 @@ module TmpVarExample2 (
 );
     always @(a) begin: assig_process_b
         reg[1:0] tmp_concat_0;
-        reg[1:0] tmp_index_1;
-        reg tmp_index_0;
-        reg tmp_index_2;
+        reg[1:0] tmp_index_0;
         tmp_concat_0 = {a[31:16] == 16'h0001, a[15:0] == 16'h0001};
-        tmp_index_1 = tmp_concat_0 + 2'b01;
-        tmp_index_0 = tmp_index_1[1];
-        tmp_index_2 = tmp_index_1[0];
-        if (tmp_index_0 == 1'b0 & tmp_index_2 == 1'b0)
+        tmp_index_0 = tmp_concat_0 + 2'b01;
+        if (tmp_index_0[1] == 1'b0 & tmp_index_0[0] == 1'b0)
             b = 32'h00000000;
         else
             b = 32'h00000001;
