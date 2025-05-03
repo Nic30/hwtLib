bdcac447e0bfa9fee347e8fbc43d065a2e8db739
Nic30 <nic30@seznam.cz>
2025-05-03 15:04:31 +0200

    feat(BaseSerializationTC):assert_same_as_file option to dump both on err

--- hwtLib/examples/base_serialization_TC.py
+++ hwtLib/examples/base_serialization_TC.py
@@ -9,7 +9,6 @@ from hwt.serializer.vhdl import Vhdl2008Serializer
 from hwt.simulator.simTestCase import SimTestCase
 from hwt.synth import to_rtl_str
 
-
 rmWhitespaces = re.compile(r'\s+', re.MULTILINE)
 
 
@@ -42,15 +41,25 @@ class BaseSerializationTC(SimTestCase):
         s = to_rtl_str(m, serializer_cls=ser)
         self.assert_same_as_file(s, file_name)
 
-    def assert_same_as_file(self, s:str, file_name: str):
+    def assert_same_as_file(self, s:str, file_name: str, printOnError=False):
         assert self.__FILE__ is not None, "This should be set on child class"
         THIS_DIR = os.path.dirname(os.path.realpath(self.__FILE__))
         fn = os.path.join(THIS_DIR, file_name)
         # print(s)
         # with open(fn, "w") as f:
         #     f.write(s)
-        with open(fn) as f:
-            ref_s = f.read()
-        # print(ref_s)
-        # print(s)
-        self.strStructureCmp(s, ref_s)
+        try:
+            with open(fn) as f:
+                ref_s = f.read()
+        except FileNotFoundError as e:
+            if printOnError:
+                print(s)
+            raise e
+        # self.strStructureCmp(s, ref_s)
+        try:
+            self.strStructureCmp(s, ref_s)
+        except:
+            if printOnError:
+                print(ref_s)
+                print(s)
+            raise
