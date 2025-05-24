ee113d57cd1590573d297f6ce4b674994ff4c804
Nic30 <nic30@seznam.cz>
2025-05-24 16:52:12 +0200

    refactor: extract mask utils to pyMatchBitPrecise

--- hwtLib/amba/axi_comp/cache/utils.py
+++ hwtLib/amba/axi_comp/cache/utils.py
@@ -1,41 +1,7 @@
-from math import ceil
-
-from hwt.code import Concat
-from hwt.hdl.types.bits import HBits
 from hwt.math import log2ceil
 from hwt.pyUtils.typingFuture import override
 from hwtLib.commonHwIO.addr_data import HwIOAddrData
 from hwtLib.mem.cam import CamMultiPort
-from pyMathBitPrecise.bit_utils import apply_set_and_clear
-
-
-def expand_byte_mask_to_bit_mask(m):
-    res = []
-    for b in m:
-        B = []
-        for _ in range(8):
-            B.append(b)
-
-        res.append(Concat(*B))
-
-    return Concat(*reversed(res))
-
-
-def apply_write_with_mask(current_data, new_data, write_mask):
-    m = expand_byte_mask_to_bit_mask(write_mask)
-    return apply_set_and_clear(current_data, new_data & m, m)
-
-
-def extend_to_width_multiple_of_8(sig):
-    """
-    make width of signal modulo 8 equal to 0
-    """
-    w = sig._dtype.bit_length()
-    cosest_multiple_of_8 = ceil((w // 8) / 8) * 8
-    if cosest_multiple_of_8 == w:
-        return sig
-    else:
-        return Concat(HBits(cosest_multiple_of_8 - w).from_py(0), sig)
 
 
 class CamWithReadPort(CamMultiPort):
